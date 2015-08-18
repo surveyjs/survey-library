67fbc33c45162b38217b8dde12098d00bb0b52a7
AndrewTelnov <andrew.telnov@gmail.com>
2015-08-18 11:17:39 +0300

    Added dropdown

---
+++ src/dxSurvey/sources/question_dropdown.ts
@@ -0,0 +1,14 @@
+﻿// <reference path="question.ts" />
+/// <reference path="questionfactory.ts" />
+module dxSurvey {
+    export class QuestionDropdown extends Question {
+        choices: Array<string> = new Array<string>();
+        constructor(public name: string) {
+            super(name);
+        }
+        public getType(): string {
+            return "dropdown";
+        }
+    }
+    QuestionFactory.Instance.registerQuestion("dropdown", (name) => { return new QuestionDropdown(name); });
+}

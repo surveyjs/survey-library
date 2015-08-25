f833363e70ce684256c6036edd69bf9262daadcb
AndrewTelnov <andrew.telnov@gmail.com>
2015-08-25 17:42:38 +0300

    The classes to make Json serialization are written.

---
+++ src/dxSurvey/sources/jsonobject.ts
@@ -0,0 +1,170 @@
+﻿/// <reference path="base.ts" />
+module dxSurvey {
+    export class JsonObjectProperty {
+        public onGetValue: (obj: any) => any = null;
+        public onCreateObj: (jsonValue: any) => any = null;
+
+        constructor(public name: string) {
+        }
+        public get hasToUseGetValue() { return this.onGetValue; }
+        public getValue(obj: any): any {
+            if (this.onGetValue) return this.onGetValue(obj);
+            return null;
+        }
+        public createObj(jsonValue: any): any {
+            if (this.onCreateObj) return this.createObj(jsonValue);
+            return null;
+        }
+    }
+    class JsonMetadataClass {
+        properties: Array<JsonObjectProperty> = null;
+        constructor(public name: string, propertiesNames: Array<string>, public creator: () => any = null, public parentName: string = null) {
+            this.properties = new Array<JsonObjectProperty>();
+            for (var i = 0; i < propertiesNames.length; i++) {
+                this.properties.push(new JsonObjectProperty(propertiesNames[i]));
+            }
+        }
+        public find(name: string): JsonObjectProperty {
+            for (var i = 0; i < this.properties.length; i++) {
+                if (this.properties[i].name == name) return this.properties[i];
+            }
+            return null;
+        }
+
+    }
+    export class JsonMetadata {
+        private classes: HashTable<JsonMetadataClass> = {};
+        private classProperties: HashTable<Array<JsonObjectProperty>> = {};
+        public addClass(name: string, propertiesNames: Array<string>, creator: () => any = null, parentName: string = null): JsonMetadataClass {
+            var metaDataClass = new JsonMetadataClass(name, propertiesNames, creator, parentName);
+            this.classes[name] = metaDataClass;
+            return metaDataClass;
+        }
+        public setonGetValue(name: string, propertyName: string, onGetValue: (obj: any) => any) {
+            var property = this.findProperty(name, propertyName);
+            if (!property) return;
+            property.onGetValue = onGetValue;
+        }
+        public setCreator(name: string, creator: () => any) {
+            var metaDataClass = this.classes[name];
+            if (!metaDataClass) return;
+            metaDataClass.creator = creator;
+        }
+        public getProperties(name: string): Array<JsonObjectProperty> {
+            var properties = this.classProperties[name];
+            if (!properties) {
+                properties = new Array<JsonObjectProperty>();
+                this.fillProperties(name, properties);
+                this.classProperties[name] = properties;
+            }
+            return properties;
+        }
+        public createClass(name: string): any {
+            var metaDataClass = this.classes[name];
+            if (!metaDataClass) return null;
+            return metaDataClass.creator();
+        }
+        private findProperty(name: string, propertyName: string): JsonObjectProperty {
+            var metaDataClass = this.classes[name];
+            return metaDataClass ? metaDataClass.find(propertyName) : null;
+        }
+        private fillProperties(name: string, list: Array<JsonObjectProperty>) {
+            var metaDataClass = this.classes[name];
+            if (!metaDataClass) return;
+            if (metaDataClass.parentName) {
+                this.fillProperties(metaDataClass.parentName, list);
+            }
+            for (var i = 0; i < metaDataClass.properties.length; i++) {
+                this.addProperty(metaDataClass.properties[i], list, list.length);
+            }
+        }
+        private addProperty(property: JsonObjectProperty, list: Array<JsonObjectProperty>, endIndex: number) {
+            var index = -1;
+            for (var i = 0; i < endIndex; i++) {
+                if (list[i].name == property.name) {
+                    index = i;
+                    break;
+                }
+            } 
+            if (index < 0) {
+                list.push(property)
+            } else {
+                list[index] = property;
+            }
+        }
+    }
+    export class JsonObject {
+        private static metaDataValue = new JsonMetadata();
+        public static get metaData() { return JsonObject.metaDataValue; }
+        public toJsonObject(obj: any): any {
+            if (!obj.getType) return obj;
+            var result = {};
+            var properties = JsonObject.metaData.getProperties(obj.getType());
+            for (var i: number = 0; i < properties.length; i++) {
+                this.valueToJson(obj, result, properties[i]);
+            }
+            return result;
+        }
+        public toObject(jsonObj: any, obj: any) {
+            if (!jsonObj) return;
+            for (var key in jsonObj) {
+                this.valueToObj(jsonObj[key], obj, key);
+            }
+        }
+        protected valueToJson(obj: any, result: any, property: JsonObjectProperty) {
+            var value = null;
+            if (property.hasToUseGetValue) {
+                value = property.getValue(obj);
+            } else {
+                value = obj[property.name];
+            }
+            if (!value) return;
+            if (this.isValueArray(value)) {
+                var arrValue = [];
+                for (var i = 0; i < value.length; i++) {
+                    arrValue.push(this.toJsonObject(value[i]));
+                }
+                value = arrValue.length > 0 ? arrValue : null;
+            } else {
+                value = this.toJsonObject(value);
+            }
+            if (value) {
+                result[property.name] = value;
+            }
+        }
+        protected valueToObj(value: any, obj: any, key: any) {
+            if (this.isValueArray(value)) {
+                this.valueToArray(value, obj, key);
+            } else {
+                var newObj = this.createNewObj(value);
+                if (newObj) {
+                    obj[key] = newObj;
+                    this.toObject(value, newObj);
+                } else {
+                    obj[key] = value;
+                }
+            }
+        }
+        isValueArray(value: any): boolean { return value.constructor.toString().indexOf("Array") > -1; }
+        createNewObj(value: any): any {
+            //TODO remove 'type' make it constant or deal wiht it somehow.
+            var className = value['type'];
+            if (!className) return null;
+            delete value['type'];
+            return JsonObject.metaData.createClass(className);
+        }
+        valueToArray(value: Array<any>, obj: any, key: any) {
+            var arrValue = [];
+            for (var i = 0; i < value.length; i++) {
+                var newValue = this.createNewObj(value[i]);
+                if (newValue) {
+                    this.toObject(value[i], newValue);
+                } else {
+                    newValue = value[i];
+                }
+                arrValue.push(newValue);
+            }
+            obj[key] = arrValue;
+        } 
+    }
+}
---
+++ src/dxSurvey/tests/jsonobjecttests.ts
@@ -0,0 +1,119 @@
+﻿/// <reference path="../sources/jsonobject.ts" />
+module dxSurvey.Tests {
+    class Car {
+        public name: string;
+        public getType(): string { return "car"; }
+    }
+    class SportCar extends Car {
+        public maxSpeed: number;
+        public getType(): string { return "sport"; }
+    }
+    class Truck extends Car { 
+        public maxWeight: number;
+        public getType(): string { return "truck"; }
+    }
+     
+    class Dealer {
+        public name: string;
+        public unserializedName: string;
+        public cars = new Array<Car>();
+        public stringArray: Array<string> = [];
+        public defaultValue: string;
+        public car: Car;
+        public getType(): string { return "dealer"; }
+    }
+
+    JsonObject.metaData.addClass("dealer", ["name", "dummyname", "cars", "stringArray", "defaultValue", "car"]);
+    JsonObject.metaData.setonGetValue("dealer", "defaultValue", function (obj: any): any {
+        if (obj.defaultValue == "default") return null;
+        return obj.defaultValue;
+    });
+
+    JsonObject.metaData.addClass("car", ["name", "type"]);
+    JsonObject.metaData.setonGetValue("car", "type", function (obj: any) { return obj.getType(); });
+    JsonObject.metaData.addClass("truck", ["maxWeight"], function () { return new Truck(); }, "car");
+    JsonObject.metaData.addClass("sport", ["maxSpeed"], null, "car");
+    JsonObject.metaData.setCreator("sport", function () { return new SportCar(); });
+
+    QUnit.module("JsonObject");
+
+    QUnit.test("Metadata for non inherited class", function (assert) {
+        assert.equal(JsonObject.metaData.getProperties("dealer").length, 6, "Flat properties list");
+        assert.equal(JsonObject.metaData.getProperties("dealer")[0].name, "name", "Name property");
+    });
+    QUnit.test("Metadata add at the beginning parent class properties ", function (assert) {
+        assert.equal(JsonObject.metaData.getProperties("truck").length, 3, "1 + 2 parent propreties");
+        assert.equal(JsonObject.metaData.getProperties("truck")[0].name, "name", "parent properties first");
+        assert.notEqual(JsonObject.metaData.getProperties("truck")[1].onGetValue, null, "has onGetValue callback");
+    });
+    QUnit.test("One object - one property serialization", function (assert) {
+        var dealer = new Dealer();
+        dealer.name = "small";
+        dealer.unserializedName = "none";
+        var jsObj = new dxSurvey.JsonObject().toJsonObject(dealer);
+        assert.equal(JSON.stringify(jsObj), "{\"name\":\"small\"}", "serialize just one property");
+    });
+    QUnit.test("String array serialization", function (assert) {
+        var dealer = new Dealer();
+        dealer.stringArray = ["one", "two"];
+        var jsObj = new dxSurvey.JsonObject().toJsonObject(dealer);
+        assert.equal(JSON.stringify(jsObj), "{\"stringArray\":[\"one\",\"two\"]}", "serialize array");
+    });
+    QUnit.test("Default value serialization", function (assert) {
+        var dealer = new Dealer();
+        dealer.defaultValue = "default";
+        var jsObj = new dxSurvey.JsonObject().toJsonObject(dealer);
+        assert.equal(JSON.stringify(jsObj), "{}", "default value of this property is not serialized");
+        dealer.defaultValue = "nondefault";
+        var jsObj = new dxSurvey.JsonObject().toJsonObject(dealer);
+        assert.equal(JSON.stringify(jsObj), "{\"defaultValue\":\"nondefault\"}", "serialize non default property as any other");
+    });
+    QUnit.test("Serialize object with it's type", function (assert) {
+        var dealer = new Dealer();
+        var truck = new Truck();
+        truck.maxWeight = 10000;
+        dealer.car = truck;
+        var jsObj = new dxSurvey.JsonObject().toJsonObject(dealer);
+        assert.equal(JSON.stringify(jsObj), "{\"car\":{\"type\":\"truck\",\"maxWeight\":10000}}", "serialize object with it's type");
+    });
+    QUnit.test("Serialize arrays with serializable objects", function (assert) {
+        var dealer = new Dealer();
+        var truck = new Truck();
+        truck.maxWeight = 10000;
+        var sport = new SportCar();
+        sport.maxSpeed = 320;
+        dealer.cars = [sport, truck];
+        var jsObj = new dxSurvey.JsonObject().toJsonObject(dealer);
+        assert.equal(JSON.stringify(jsObj), "{\"cars\":[{\"type\":\"sport\",\"maxSpeed\":320},{\"type\":\"truck\",\"maxWeight\":10000}]}", "serialize object with it's type");
+    });
+    QUnit.test("One object - one property deserialization", function (assert) {
+        var dealer = new Dealer();
+        new dxSurvey.JsonObject().toObject({ "name": "small" }, dealer);
+        assert.equal(dealer.name, "small", "deserialize just one property");
+    });
+    QUnit.test("String array deserialization", function (assert) {
+        var dealer = new Dealer();
+        new dxSurvey.JsonObject().toObject({ "stringArray": ["one", "two"] }, dealer);
+        assert.deepEqual(dealer.stringArray, ["one", "two"], "deserialize array");
+    });
+    QUnit.test("Deserialize object with it's type", function (assert) {
+        var dealer = new Dealer();
+        new dxSurvey.JsonObject().toObject({ "car": { "type": "truck", "maxWeight": 10000 } }, dealer);
+        var truck: any = dealer.car;
+        assert.equal(truck.maxWeight, 10000, "deserialize object with it's type");
+        assert.equal(truck.getType(), "truck", "the live object");
+        assert.notEqual(truck.type, "truck", "type is removed");
+    });
+    QUnit.test("Deserialize arrays with serializable objects", function (assert) {
+        var dealer = new Dealer();
+        new dxSurvey.JsonObject().toObject({ "cars": [{ "type": "sport", "maxSpeed": 320 }, { "type": "truck", "maxWeight": 10000 }] }, dealer);
+        assert.equal(dealer.cars.length, 2, "two objects in array should be deserialized");
+        var sport: any = dealer.cars[0];
+        var truck: any = dealer.cars[1];
+        assert.equal(sport.maxSpeed, 320, "deserialize the first object");
+        assert.equal(sport.getType(), "sport", "deserialize the first object");
+        assert.equal(truck.maxWeight, 10000, "deserialize the second object");
+        assert.equal(truck.getType(), "truck", "deserialize the second object");
+    });
+
+}

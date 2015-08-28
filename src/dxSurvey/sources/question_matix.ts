a1d0ddaaf9374504415e0c431a91e3011243ff90
AndrewTelnov <andrew.telnov@gmail.com>
2015-08-28 17:52:52 +0300

    Rating question is added.

---
+++ src/dxSurvey/sources/question_matix.ts
@@ -0,0 +1,17 @@
+﻿// <reference path="question.ts" />
+/// <reference path="questionfactory.ts" />
+/// <reference path="jsonobject.ts" />
+module dxSurvey {
+    export class QuestionMatrix extends Question {
+        public columns: string[] = [];
+        public rows: string[] = [];
+        constructor(public name: string) {
+            super(name);
+        }
+        public getType(): string {
+            return "matrix";
+        }
+    }
+    JsonObject.metaData.addClass("matrix", ["columns", "rows"], function () { return new QuestionMatrix(""); }, "question");
+    QuestionFactory.Instance.registerQuestion("matrix", (name) => { return new QuestionMatrix(name); });
+}
---
+++ src/dxSurvey/sources/question_rating.ts
@@ -0,0 +1,34 @@
+﻿// <reference path="question.ts" />
+/// <reference path="questionfactory.ts" />
+/// <reference path="jsonobject.ts" />
+module dxSurvey {
+    export class QuestionRating extends Question {
+        static defaultRateValues: string[] = ["1", "2", "3", "4", "5"];
+        public rateValues: string[] = [];
+        public mininumRateDescription: string = null;
+        public maximumRateDescription: string = null;
+
+        koVisibleRateValues: any;
+
+        constructor(public name: string) {
+            super(name);
+            if (this.isKO) {
+                var self = this;
+                this.koVisibleRateValues = ko.computed(function () {
+                    return self.visibleRateValues;
+                });
+            }
+        }
+        get visibleRateValues(): string[] {
+            if (this.rateValues.length > 0) return this.rateValues;
+            return QuestionRating.defaultRateValues;
+        }
+        public getType(): string {
+            return "rating";
+        }
+        public supportComment(): boolean { return true; }
+        public supportOther(): boolean { return true; }
+    }
+    JsonObject.metaData.addClass("rating", ["rateValues", "mininumRateDescription", "maximumRateDescription"], function () { return new QuestionRating(""); }, "question");
+    QuestionFactory.Instance.registerQuestion("rating", (name) => { return new QuestionRating(name); });
+}

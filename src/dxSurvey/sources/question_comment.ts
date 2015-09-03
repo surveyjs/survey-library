6f72f07d69ee35aae35335ea8e14d8664ae0e478
AndrewTelnov <andrew.telnov@gmail.com>
2015-09-03 09:23:14 +0300

    Add comment question.

---
+++ src/dxSurvey/sources/question_comment.ts
@@ -0,0 +1,20 @@
+﻿// <reference path="question.ts" />
+/// <reference path="questionfactory.ts" />
+/// <reference path="jsonobject.ts" />
+module dxSurvey {
+    export class QuestionComment extends Question {
+        public rows: number = 3;
+        constructor(public name: string) {
+            super(name);
+        }
+        public getType(): string {
+            return "comment";
+        }
+        isEmpty(): boolean {
+            return super.isEmpty() || this.value == "";
+        }
+    }
+    JsonObject.metaData.addClass("comment", ["rows"], function () { return new QuestionComment(""); }, "question");
+    JsonObject.metaData.setPropertyValues("comment", "rows", null, 3);
+    QuestionFactory.Instance.registerQuestion("comment", (name) => { return new QuestionComment(name); });
+}

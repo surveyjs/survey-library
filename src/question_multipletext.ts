6641143ba18030c0ad331bf6853cf7126f992f7d
AndrewTelnov <andrew.telnov@gmail.com>
2015-09-14 12:13:24 +0300

    Add multipletext question

---
+++ src/question_multipletext.ts
@@ -0,0 +1,98 @@
+﻿// <reference path="question.ts" />
+/// <reference path="questionfactory.ts" />
+/// <reference path="jsonobject.ts" />
+module dxSurvey {
+    interface IMultipleTextData {
+        getMultipleTextValue(name: string): any;
+        setMultipleTextValue(name: string, value: any);
+    }
+
+    export class MultipleTextItem extends Base {
+        private data: IMultipleTextData;
+        private titleValue: string;
+        koValue: any;
+
+        constructor(public name: any = null, title: string = null) {
+            super();
+            this.title = title;
+            if (this.isKO) {
+                this.koValue = ko.observable(this.value);
+                var self = this;
+                this.koValue.subscribe(function (newValue) {
+                    self.value = newValue;
+                });
+            }
+        }
+        public getType(): string {
+            return "multipletextitem";
+        }
+        setData(data: IMultipleTextData) {
+            this.data = data;
+        }
+        public get title() { return this.titleValue ? this.titleValue : this.name;  }
+        public set title(newText: string) { this.titleValue = newText; }
+        public get value() {
+            return this.data ? this.data.getMultipleTextValue(this.name) : null;
+        }
+        public set value(value: any) {
+            if (this.data != null) {
+                this.data.setMultipleTextValue(this.name, value);
+            }
+        }
+        onValueChanged(newValue: any) {
+            if (this.isKO) {
+                this.koValue(newValue);
+            }
+        }
+    }
+
+    export class QuestionMultipleText extends Question implements IMultipleTextData {
+        public items: Array<MultipleTextItem> = new Array<MultipleTextItem>();
+        constructor(public name: string) {
+            super(name);
+            var self = this;
+            this.items.push = function (value) {
+                value.setData(self);
+                return Array.prototype.push.call(this, value);
+            };
+        }
+        public getType(): string {
+            return "multipletext";
+        }
+        private isMultipleItemValueChanging = false;
+        protected onValueChanged() {
+            super.onValueChanged();
+            if (!this.isMultipleItemValueChanging) {
+                for (var i = 0; i < this.items.length; i++) {
+                    var itemValue = null;
+                    if (this.value && (this.items[i].name in this.value)) {
+                        itemValue = this.value[this.items[i].name];
+                    }
+                    this.items[i].onValueChanged(itemValue);
+                }
+            }
+        }
+        //IMultipleTextData
+        getMultipleTextValue(name: string) {
+            if (!this.value) return null;
+            return this.value[name];
+        }
+        setMultipleTextValue(name: string, value: any) {
+            this.isMultipleItemValueChanging = true;
+            if (!this.value) {
+                this.value = {};
+            }
+            this.value[name] = value;
+            this.isMultipleItemValueChanging = false;
+        }
+
+    }
+    JsonObject.metaData.addClass("multipletextitem", ["name", "title"], function () { return new MultipleTextItem(""); });
+    JsonObject.metaData.setPropertyValues("multipletextitem", "title", null, null,
+        function (obj: any) { return obj.titleValue; });
+
+    JsonObject.metaData.addClass("multipletext", ["items"], function () { return new QuestionMultipleText(""); }, "question");
+    JsonObject.metaData.setPropertyValues("multipletext", "items", "multipletextitem");
+
+    QuestionFactory.Instance.registerQuestion("multipletext", (name) => { return new QuestionMultipleText(name); });
+}

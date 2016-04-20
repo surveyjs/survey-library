606b7a5f2f6111f610a8650a60a8865cbbe2da69
Topi Porkka <topi.porkka@keskisuomalainen.fi>
2016-04-20 08:37:59 +0300

    Finnish localization

---
+++ src/localization/finnish.ts
@@ -0,0 +1,20 @@
+/// <reference path="..//surveyStrings.ts" />
+module Survey {
+  var finnishSurveyStrings = {
+      pagePrevText: "Edellinen",
+      pageNextText: "Seuraava",
+      completeText: "Valmis",
+      otherItemText: "Muu (kuvaile)",
+      progressText: "Sivu {0}/{1}",
+      optionsCaption: "Valitse...",
+      requiredError: "Ole hyvä ja vastaa kysymykseen.",
+      numericError: "Arvon tulee olla numeerinen.",
+      textMinLength: "Ole hyvä ja syötä vähintään {0} merkkiä.",
+      minSelectError: "Ole hyvä ja valitse vähintään {0} vaihtoehtoa.",
+      maxSelectError: "Ole hyvä ja valitse enintään {0} vaihtoehtoa.",
+      numericMinMax: "Syötä numero joka on enemmän tai yhtä suuri kuin {1} ja vähemmän tai yhtä suuri kuin {2}",
+      numericMin: "Syötä numero joka on enemmän tai yhtä suuri kuin {1}",
+      numericMax: "Syötä numero joka on vähemmän tai yhtä suuri kuin {1}"
+  }
+  surveyLocalization.locales["fi"] = finnishSurveyStrings;
+}

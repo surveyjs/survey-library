a6badc91fb86ed245e224ccd992b45b0df17237a
AndrewTelnov <andrew.telnov@gmail.com>
2016-06-16 08:45:40 +0300

    French localization is added, on behalf https://github.com/Frank13

---
+++ src/localization/french.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="..//surveyStrings.ts" />
+//Created on behalf https://github.com/Frank13
+module Survey {
+    var frenchSurveyStrings = {
+        pagePrevText: "Pr\u00e9c\u00e9dent",
+        pageNextText: "Suivant",
+        completeText: "Terminer",
+        otherItemText: "Autre (pr\u00e9ciser)",
+        progressText: "Page {0} sur {1}",
+        emptySurvey: "Il n'y a ni page visible ni question visible dansce questionnaire",
+        completingSurvey: "Merci d'avoir r\u00e9pondu au questionnaire!",
+        loadingSurvey: "Le questionnaire est en cours de chargement...",
+        optionsCaption: "Choisissez...",
+        requiredError: "La r\u00e9ponse \u00e0 cette question estobligatoire.",
+        numericError: "La r\u00e9ponse doit \u00eatre un nombre.",
+        textMinLength: "Merci d'entrer au moins {0} symboles.",
+        minSelectError: "Merci de s\u00e9lectionner au moins {0}r\u00e9ponses.",
+        maxSelectError: "Merci de s\u00e9lectionner au plus {0}r\u00e9ponses.",
+        numericMinMax: "Votre r\u00e9ponse '{0}' doit \u00eatresup\u00e9rieure ou \u00e9gale \u00e0 {1} et inf\u00e9rieure ou\u00e9gale \u00e0 {2}",
+        numericMin: "Votre r\u00e9ponse '{0}' doit \u00eatresup\u00e9rieure ou \u00e9gale \u00e0 {1}",
+        numericMax: "Votre r\u00e9ponse '{0}' doit \u00eatreinf\u00e9rieure ou \u00e9gale \u00e0 {1}",
+        invalidEmail: "Merci d'entrer une adresse mail valide.",
+        exceedMaxSize: "La taille du fichier ne doit pas exc\u00e9der {0}.",
+        otherRequiredError: "Merci de pr\u00e9ciser le champ 'Autre'."
+    }
+    surveyLocalization.locales["fr"] = frenchSurveyStrings;
+}

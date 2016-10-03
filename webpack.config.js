619ee8ab6a7cdf4339931ca4524b43d9aff01061
dmitrykurmanov <kurmanov.work@gmail.com>
2016-10-03 14:54:41 +0300

    added "library" config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,23 @@ module.exports = (options) => {
         },
         output: {
             path: path.resolve(__dirname, options.outputDir), // this is not important because we use gulp dest
-            filename: '[name].js'
+            filename: '[name].js',
+            library: 'Survey',
+            libraryTarget: 'umd'
+        },
+        externals: {
+            'react': {
+                root: 'React',
+                commonjs2: 'react',
+                commonjs: 'react',
+                amd: 'react'
+            },
+            'react-dom': {
+                root: 'ReactDOM',
+                commonjs2: 'react-dom',
+                commonjs: 'react-dom',
+                amd: 'react-dom'
+            }
         },
         module: {
             loaders: [

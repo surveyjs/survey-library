37436eda5dd19ac9bf3ff03ae95bd6f6ce0864df
dmitrykurmanov <kurmanov.work@gmail.com>
2016-10-04 16:34:35 +0300

    implemented external modules (es2015) changed tests files

--- tests/surveyvalidatortests.ts
+++ tests/surveyvalidatortests.ts
@@ -1,24 +1,23 @@
-﻿/// <reference path="../src/base.ts" />
-/// <reference path="../src/validator.ts" />
-module Survey.Tests {
-    QUnit.module("Validators");
+﻿import {NumericValidator, EmailValidator} from "../src/validator";
 
-    QUnit.test("Numeric validator", function (assert) {
-        var validator = new NumericValidator();
-        assert.notEqual(validator.validate("s5").error, null, "Could not convert to numeric");
-        assert.equal(validator.validate(5), null, "There is no limits here");
-        assert.equal(validator.validate("5").value, 5, "Convert to numeric");
-        assert.equal(validator.validate("5").error, null, "There is no error");
-        validator.minValue = 10;
-        assert.notEqual(validator.validate(5).error, null, "There is should be an error. The value is low.");
-        validator.maxValue = 20;
-        assert.notEqual(validator.validate(25).error, null, "There is should be an error. The value is high.");
-        assert.equal(validator.validate("15").error, null, "There value is between minValue and maxValue");
-        assert.equal(validator.validate(15), null, "everything is fine - return null");
-    });
-    QUnit.test("Email validator", function (assert) {
-        var validator = new EmailValidator();
-        assert.equal(validator.validate("my@mail.com"), null, "Could convert the correct e-mail");
-        assert.notEqual(validator.validate("@mail.com").error, null, "Could convert the incorrect correct e-mail");
-    });
-}
+QUnit.module("Validators");
+
+QUnit.test("Numeric validator", function (assert) {
+    var validator = new NumericValidator();
+    assert.notEqual(validator.validate("s5").error, null, "Could not convert to numeric");
+    assert.equal(validator.validate(5), null, "There is no limits here");
+    assert.equal(validator.validate("5").value, 5, "Convert to numeric");
+    assert.equal(validator.validate("5").error, null, "There is no error");
+    validator.minValue = 10;
+    assert.notEqual(validator.validate(5).error, null, "There is should be an error. The value is low.");
+    validator.maxValue = 20;
+    assert.notEqual(validator.validate(25).error, null, "There is should be an error. The value is high.");
+    assert.equal(validator.validate("15").error, null, "There value is between minValue and maxValue");
+    assert.equal(validator.validate(15), null, "everything is fine - return null");
+});
+
+QUnit.test("Email validator", function (assert) {
+    var validator = new EmailValidator();
+    assert.equal(validator.validate("my@mail.com"), null, "Could convert the correct e-mail");
+    assert.notEqual(validator.validate("@mail.com").error, null, "Could convert the incorrect correct e-mail");
+});

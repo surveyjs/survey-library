79eb3cbd7568046130c8ed498906980fe63808fb
AndrewTelnov <andrew.telnov@gmail.com>
2016-10-28 11:54:10 +0300

    Progress bar is not rendering in the react: https://github.com/andrewtelnov/surveyjs/issues/98

---
+++ src/plugins/questiondate/react/reactquestiondate.tsx
@@ -0,0 +1,47 @@
+﻿import * as React from 'react';
+import QuestionDateModel from "../question_date";
+import {ReactQuestionFactory} from "../../../react/reactquestionfactory";
+
+
+export default class SurveyQuestionDate extends React.Component<any, any> {
+    private question: QuestionDateModel;
+    protected css: any;
+    constructor(props: any) {
+        super(props);
+        this.question = props.question;
+        this.css = props.css;
+        this.state = { value: this.question.value };
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+    handleOnChange(event) {
+        this.question.value = event.target.value;
+        this.setState({ value: this.question.value });
+    }
+    componentWillReceiveProps(nextProps: any) {
+        this.question = nextProps.question;
+        this.css = nextProps.css;
+    }
+    componentDidMount() {
+        var scriptText = "$(function () { $('#" + this.getDateId() + "').datepicker(); });";
+        var rootId = this.getDivId();
+        var scriptEl = document.createElement("script");
+        scriptEl.type = "text\/javascript";
+        scriptEl.text = scriptText;
+        document.getElementById(rootId).appendChild(scriptEl);
+    }
+    render(): JSX.Element {
+        if (!this.question) return null;
+        var inputId = "date_" + this.question.name;
+        return (
+            <div id={this.getDivId()}>
+                <input className={this.css} id={this.getDateId()} type="text" value={this.question.value || ''} onChange={this.handleOnChange} />
+            </div>
+        );
+    }
+    private getDateId(): string { return "date_" + this.question.name; }
+    private getDivId(): string { return "rootDate_" + this.question.name; }
+}
+
+ReactQuestionFactory.Instance.registerQuestion("date", (props) => {
+    return React.createElement(SurveyQuestionDate, props);
+});

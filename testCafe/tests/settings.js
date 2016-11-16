d4683e7ad137307b28e1b283fabca800f72d6270
dmitrykurmanov <kurmanov.work@gmail.com>
2016-11-16 14:44:31 +0300

    fixed path for testcafe tests

--- testCafe/tests/settings.js
+++ testCafe/tests/settings.js
@@ -1,2 +1,2 @@
 export const frameworks = ['knockout/standard', 'knockout/bootstrap', 'react/standard', 'react/bootstrap'];
-export const url = 'http://127.0.0.1:8080/';
+export const url = 'http://127.0.0.1:8080/testCafe/';

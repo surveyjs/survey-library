c98f5c67bb78487d7000ee6b1086127d7bc4cb39
AndrewTelnov <andrew.telnov@gmail.com>
2016-11-25 11:47:11 +0300

    Add danish localization into webpack chunks

--- src/entries/chunks/localization.ts
+++ src/entries/chunks/localization.ts
@@ -1,6 +1,7 @@
-import '../../localization/russian';
-import '../../localization/french';
-import '../../localization/finnish';
-import '../../localization/german';
+import '../../localization/danish';
 import '../../localization/dutch';
+import '../../localization/finnish';
+import '../../localization/french';
+import '../../localization/german';
+import '../../localization/russian';
 import '../../localization/turkish';

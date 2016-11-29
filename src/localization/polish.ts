12da979c1b2929dde4e7afa18502d22c1439aaa2
klvbdmh <klvbdmh@users.noreply.github.com>
2016-11-29 17:35:48 +0100

    Create polish.ts
    added Polish translation file

---
+++ src/localization/polish.ts
@@ -0,0 +1,26 @@
+import {surveyLocalization} from "../surveyStrings";
+
+export var polishSurveyStrings = {
+    pagePrevText: "Wstecz",
+    pageNextText: "Dalej",
+    completeText: "Gotowe",
+    progressText: "Strona {0} z {1}",
+    emptySurvey: "Nie ma widocznych pytań.",
+    completingSurvey: "Dziękujemy za wypełnienie ankiety!",
+    loadingSurvey: "Trwa wczytywanie ankiety...",
+    otherItemText: "Inna odpowiedź...",
+    optionsCaption: "Wybierz...",
+    requiredError: "Proszę odpowiedzieć na to pytanie.",
+    numericError: "W tym polu można wpisać tylko liczby.",
+    textMinLength: "Proszę wpisać co najmniej {0} znaków.",
+    minSelectError: "Proszę wybrać co najmniej {0} pozycji.",
+    maxSelectError: "Proszę wybrać nie więcej niż {0} pozycji.",
+    numericMinMax: "'Odpowiedź {0}' powinna być większa lub równa {1} oraz mniejsza lub równa {2}",
+    numericMin: "Odpowiedź '{0}' powinna być większa lub równa {1}",
+    numericMax: "Odpowiedź '{0}' powinna być mniejsza lub równa {1}",
+    invalidEmail: "Proszę podać prawidłowy adres email.",
+    exceedMaxSize: "Rozmiar przesłanego pliku nie może przekraczać {0}.",
+    otherRequiredError: "Proszę podać inną odpowiedź."
+};
+
+surveyLocalization.locales["pl"] = polishSurveyStrings;

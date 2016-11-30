3eca0c986a9cb020ac78c4ecc1da9fe172b9ea44
AndrewTelnov <andrew.telnov@gmail.com>
2016-11-30 09:51:11 +0300

    Add polish localization.

--- src/entries/chunks/localization.ts
+++ src/entries/chunks/localization.ts
@@ -3,5 +3,6 @@ import '../../localization/dutch';
 import '../../localization/finnish';
 import '../../localization/french';
 import '../../localization/german';
+import '../../localization/polish';
 import '../../localization/russian';
 import '../../localization/turkish';

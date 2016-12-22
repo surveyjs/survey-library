338d13eb592db8359c5f0acdfb01bb5cfbe8cca9
dmitrykurmanov <kurmanov.work@gmail.com>
2016-12-22 16:14:20 +0300

    added surveyWindow for angular and jQuery

--- src/angular/SurveyNG.tsx
+++ src/angular/SurveyNG.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Survey} from "../react/reactSurvey";
+import {SurveyWindow} from "../react/reactSurveyWindow";
 
 export class SurveyNG {
     public static render(elementId, props){
         ReactDOM.render(<Survey {...props} />, document.getElementById(elementId));
+    }
+}
+
+export class SurveyWindowNG {
+    public static render(elementId, props){
+        ReactDOM.render(<SurveyWindow {...props} />, document.getElementById(elementId));
     }
 }
--- src/entries/angular.ts
+++ src/entries/angular.ts
@@ -1,3 +1,4 @@
 export * from "./react";
 export {__assign} from "../assign";
 export {SurveyNG} from "../angular/SurveyNG";
+export {SurveyWindowNG} from "../angular/SurveyNG";

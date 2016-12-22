1b54f67167158a90ab39e2832f80c9516623114c
AndrewTelnov <andrew.telnov@gmail.com>
2016-12-22 19:24:59 +0300

    Update to 0.10.3 version.

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ var gulp = require('gulp'),
 
 var Server = require("karma").Server;
 
-var libraryVersion = "0.10.2";
+var libraryVersion = "0.10.3";
 
 var paths = {
     webroot: "./" + project.webroot + "/",

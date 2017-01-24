fc043880a145d460f62acd6993827c028e7f948d
dmitrykurmanov <kurmanov.work@gmail.com>
2017-01-24 10:50:03 +0300

    webpack 2.0 + build system refactoring
    -- webpack2 ts loader

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function(options) {
+    var config = {
+        entry: {},
+        resolve: {
+            extensions: ['.ts', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'ts-loader',
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        output: {
+            filename: './packages/survey-' + options.platform + '/dist/[name].js',
+            library: 'Survey',
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        externals: {
+            'react': {
+                root: 'React',
+                commonjs2: 'react',
+                commonjs: 'react',
+                amd: 'react'
+            },
+            'react-dom': {
+                root: 'ReactDOM',
+                commonjs2: 'react-dom',
+                commonjs: 'react-dom',
+                amd: 'react-dom'
+            },
+            'knockout': {
+                root: 'ko',
+                commonjs2: 'knockout',
+                commonjs: 'knockout',
+                amd: 'knockout'
+            },
+            'jquery': {
+                root: 'jQuery',
+                commonjs2: 'jquery',
+                commonjs: 'jquery',
+                amd: 'jquery'
+            }
+        },
+        plugins: [
+            new webpack.NoEmitOnErrorsPlugin(),
+            new webpack.ProvidePlugin({
+                __extends: path.join(__dirname, './src', 'extends.ts'),
+                __assign: path.join(__dirname, './src', 'assign.ts')
+            })
+        ],
+        devtool: 'inline-source-map'
+    };
+
+    if (options.platform === 'angular' || options.platform === 'jquery') {
+        config.resolve.alias = {
+            'react': 'preact-compat',
+            'react-dom': 'preact-compat'
+        };
+    }
+
+    if (options.buildType === 'prod') {
+        config.devtool = false;
+        config.plugins.push(
+            new webpack.optimize.UglifyJsPlugin()
+        );
+    }
+
+    if (options.buildType === 'dev') {
+        config.plugins.push(
+            new webpack.LoaderOptionsPlugin({
+                debug: true
+            })
+        );
+    }
+
+    config.entry['survey.' + options.platform] = path.resolve(__dirname, './src/entries/' + options.platform);
+
+    return config;
+}

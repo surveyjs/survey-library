3bfdd8501a7f28e0f66a2dc05eae7eab8609f92a
dmitrykurmanov <kurmanov.work@gmail.com>
2017-02-01 15:21:57 +0300

    vuejs
    -- working sample component

--- src/vue/vueSurvey.vue
+++ src/vue/vueSurvey.vue
@@ -1,20 +1,26 @@
 <template>
-    <div>{{model.questions[0].name}}</div>
+    <!--<div>{{model.questions[0].name}}</div>-->
+    <div>Model has no questions for while :(</div>
 </template>
 
 <script lang="ts">
     import * as Vue from 'vue'
     import Component from 'vue-class-component'
+    import {VueSurveyModel} from './vueSurveyModel'
 
     @Component({
         props: {
             json: Object
         }
     })
-    export class Survey extends Vue {
+    export default class Survey extends Vue {
         json: Object;
+
+        model: VueSurveyModel;
         constructor () {
             super();
+            this.model = new VueSurveyModel(this.json);
         }
     }
+
 </script>

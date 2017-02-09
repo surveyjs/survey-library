7023774b8fd1502ff402e00c4816bf79116236bc
tsv2013 <tsv2013@users.noreply.github.com>
2017-02-09 13:14:26 +0300

    vue
      -- got rid of typings

--- lib_typings/extends.d.ts
+++
@@ -1,5 +0,0 @@
-// Type definitions for Survey JavaScript library v0.10.4
-// Project: http://surveyjs.org/
-// Definitions by: tdHeader <https://github.com/surveyjs/>
-
-export declare function __extends(d: any, b: any): void;

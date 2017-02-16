0275833ba66b50902afb8bbd592db41ed4949514
AndrewTelnov <andrew.telnov@gmail.com>
2017-02-16 23:12:57 +0300

    Correct the french translation: https://github.com/surveyjs/surveyjs/issues/240

--- src/localization/french.ts
+++ src/localization/french.ts
@@ -12,6 +12,7 @@ export var frenchSurveyStrings = {
     loadingSurvey: "Le questionnaire est en cours de chargement...",
     optionsCaption: "Choisissez...",
     requiredError: "La r\u00e9ponse \u00e0 cette question est obligatoire.",
+    requiredInAllRowsError: "Toutes les lignes sont obligatoires",
     numericError: "La r\u00e9ponse doit \u00eatre un nombre.",
     textMinLength: "Merci d'entrer au moins {0} symboles.",
     minSelectError: "Merci de s\u00e9lectionner au moins {0}r\u00e9ponses.",

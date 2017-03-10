d0f32c7820ccf1c3564756838696d26dbc94b3ff
tsv2013 <tsv2013@users.noreply.github.com>
2017-03-10 09:48:58 +0300

    Fix npm deployment script

--- deploy_npm.sh
+++ deploy_npm.sh
@@ -2,15 +2,15 @@ cd packages
 cd survey-angular
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd survey-jquery
+cd ../survey-jquery
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd survey-knockout
+cd ../survey-knockout
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd survey-react
+cd ../survey-react
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd survey-vue
+cd ../survey-vue
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .

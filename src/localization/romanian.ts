334fe8e1014f7c26e578dde238a041fba1945ae3
Laurentiu Dobrota <rauldobrota@gmail.com>
2017-03-13 17:13:41 +0200

    Create romanian.ts
    Romanian localization

---
+++ src/localization/romanian.ts
@@ -0,0 +1,26 @@
+import {surveyLocalization} from "../surveyStrings";
+
+export var romanianSurveyStrings = {
+    pagePrevText: "Precedent",
+    pageNextText: "Următor",
+    completeText: "Finalizare",
+    otherItemText: "Altul(precizaţi)",
+    progressText: "Pagina {0} din {1}",
+    emptySurvey: "Nu sunt întrebări pentru acest chestionar",
+    completingSurvey: "Vă mulţumim pentru timpul acordat!",
+    loadingSurvey: "Chestionarul se încarcă...",
+    optionsCaption: "Alegeţi...",
+    requiredError: "Răspunsul la această întrebare este obligatoriu.",
+    requiredInAllRowsError: "Toate răspunsurile sunt obligatorii",
+    numericError: "Răspunsul trebuie să fie numeric.",
+    textMinLength: "Trebuie să introduci minim {0} caractere.",
+    minSelectError: "Trebuie să selectezi minim {0} opţiuni.",
+    maxSelectError: "Trebuie să selectezi maxim {0} opţiuni.",
+    numericMinMax: "Răspunsul '{0}' trebuie să fie mai mare sau egal ca {1} şî mai mic sau egal cu {2}",
+    numericMin: "Răspunsul '{0}' trebuie să fie mai mare sau egal ca {1}",
+    numericMax: "Răspunsul '{0}' trebuie să fie mai mic sau egal ca {1}",
+    invalidEmail: "Trebuie să introduceţi o adresa de email validă.",
+    exceedMaxSize: "Dimensiunea fişierului nu trebuie să depăşească {0}.",
+    otherRequiredError: "Trebuie să completezi câmpul 'Altul'."
+};
+surveyLocalization.locales["ro"] = romanianSurveyStrings;

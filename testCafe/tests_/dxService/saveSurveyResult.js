45aa8d0669da62425d8eb54afe7cc99e0b0afba7
dmitrykurmanov <kurmanov.work@gmail.com>
2017-03-14 11:23:22 +0300

    testcafe
      -- added "saveSurveyResult" test

---
+++ testCafe/tests_/dxService/saveSurveyResult.js
@@ -0,0 +1,39 @@
+import {frameworks, url, setOptions, initSurvey, getSurveyResult} from "../settings";
+import {Selector, ClientFunction} from 'testcafe';
+const assert = require('assert');
+const title = `saveSurveyResult`;
+
+const json = {
+    surveyId: '0edf84f9-14f7-4944-a857-e327e1dceebb',
+    surveyPostId: '8c03f02b-0b55-4cda-96b8-d1bf7c87b05d'
+};
+
+frameworks.forEach( (framework) => {
+    fixture `${framework} ${title}`
+        .page `https://dxsurvey.com/Home/SurveyResults/0edf84f9-14f7-4944-a857-e327e1dceebb`;
+
+    test(`correct loading`, async t => {
+        const getResultsCount = ClientFunction(() =>
+            +document.querySelectorAll("dd")[2].textContent.trim());
+        let oldCount;
+
+        await t
+            .typeText(`#Email`, `kurmanov.work@gmail.com`)
+            .typeText(`#Password`, `Aa123456`)
+            .click(`input[value="Log in"]`);
+
+        oldCount = await getResultsCount();
+
+        await t
+            .navigateTo(url + framework);
+
+        await initSurvey(json);
+
+        await t
+            .click(`input[type=checkbox]`)
+            .click(`input[value=Complete]`)
+            .navigateTo(`https://dxsurvey.com/Home/SurveyResults/0edf84f9-14f7-4944-a857-e327e1dceebb`);
+
+        assert.equal(oldCount + 1, await getResultsCount());
+    });
+});

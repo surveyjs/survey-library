aa485e3d5d9ff8e1bdcb5aa18de0bf0986fbcf5d
dmitrykurmanov <kurmanov.work@gmail.com>
2017-03-14 12:26:22 +0300

    testcafe
      -- removed unused json

--- testCafe/tests_/dxService/runSurveyOneTime.js
+++ testCafe/tests_/dxService/runSurveyOneTime.js
@@ -91,10 +91,6 @@ const initSurvey = ClientFunction((framework) => {
     }
 });
 
-const json = {
-    surveyId: '5af48e08-a0a5-44a5-83f4-1c90e8e98de1'
-};
-
 frameworks.forEach( (framework) => {
     fixture `${framework} ${title}`
 

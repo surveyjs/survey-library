b3d9c4c89c1c6ee8067538910699b8673c961847
dmitrykurmanov <kurmanov.work@gmail.com>
2017-04-04 14:49:55 +0300

    VueJS:  added needed imports

--- src/vue/panel.vue
+++ src/vue/panel.vue
@@ -14,6 +14,7 @@
     import {Component, Prop} from 'vue-property-decorator'
     import {PanelModelBase, PanelModel, QuestionRowModel} from "../panel"
     import {Question as QuestionModel} from '../question'
+    import {ISurvey} from '../base'
     import {helpers} from './helpers'
 
     @Component({

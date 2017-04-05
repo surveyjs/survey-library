6cdbf91bb5ab8c6a24cf7ccce13891da8f130409
tsv2013 <tsv2013@users.noreply.github.com>
2017-04-05 18:07:39 +0300

    Update argument type

--- src/angular/SurveyNG.tsx
+++ src/angular/SurveyNG.tsx
@@ -4,15 +4,15 @@ import {Survey} from "../react/reactSurvey";
 import {SurveyWindow} from "../react/reactSurveyWindow";
 
 export class SurveyNG {
-    public static render(elementId: string | HTMLElement, props) {
-        var element: HTMLElement = typeof elementId === 'string' ? document.getElementById(elementId) : elementId;
+    public static render(elementId: string | Element, props) {
+        var element: Element = typeof elementId === 'string' ? document.getElementById(elementId) : elementId;
         ReactDOM.render(<Survey {...props} />, element);
     }
 }
 
 export class SurveyWindowNG {
-    public static render(elementId, props) {
-        var element: HTMLElement = typeof elementId === 'string' ? document.getElementById(elementId) : elementId;
+    public static render(elementId: string | Element, props) {
+        var element: Element = typeof elementId === 'string' ? document.getElementById(elementId) : elementId;
         ReactDOM.render(<SurveyWindow {...props} />, element);
     }
 }

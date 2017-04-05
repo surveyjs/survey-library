9006b44c93ccd5acc16c129e6ddd4c6088f5d8bd
tsv2013 <tsv2013@users.noreply.github.com>
2017-04-05 16:32:39 +0300

    Angular: allow rendering for HTML element

--- src/angular/SurveyNG.tsx
+++ src/angular/SurveyNG.tsx
@@ -4,13 +4,15 @@ import {Survey} from "../react/reactSurvey";
 import {SurveyWindow} from "../react/reactSurveyWindow";
 
 export class SurveyNG {
-    public static render(elementId, props){
-        ReactDOM.render(<Survey {...props} />, document.getElementById(elementId));
+    public static render(elementId: string | HTMLElement, props) {
+        var element: HTMLElement = typeof elementId === 'string' ? document.getElementById(elementId) : elementId;
+        ReactDOM.render(<Survey {...props} />, element);
     }
 }
 
 export class SurveyWindowNG {
-    public static render(elementId, props){
-        ReactDOM.render(<SurveyWindow {...props} />, document.getElementById(elementId));
+    public static render(elementId, props) {
+        var element: HTMLElement = typeof elementId === 'string' ? document.getElementById(elementId) : elementId;
+        ReactDOM.render(<SurveyWindow {...props} />, element);
     }
 }

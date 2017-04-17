1bd04ec5558554a793083b78e182daa702b544a9
dmitrykurmanov <kurmanov.work@gmail.com>
2017-04-17 13:01:27 +0300

    added cssbootstrapmaterial cssType

--- src/defaultCss/cssbootstrapmaterial.ts
+++ src/defaultCss/cssbootstrapmaterial.ts
@@ -1,6 +1,6 @@
-﻿import {surveyCss} from "./cssstandard";
+import {surveyCss} from "./cssstandard";
 
-export var defaultBootstrapCss = {
+export var defaultBootstrapMaterialCss = {
     root: "",
     header: "panel-heading",
     body: "panel-body",
@@ -9,17 +9,17 @@ export var defaultBootstrapCss = {
     progress: "progress center-block", progressBar: "progress-bar",
     pageTitle: "",
     row: "",
-    question: { root: "", title: "", comment: "form-control", indent: 20 },
+    question: { root: "form-group", title: "", comment: "form-control", indent: 20 },
     error: { root: "alert alert-danger", icon: "glyphicon glyphicon-exclamation-sign", item: "" },
 
     checkbox: { root: "form-inline", item: "checkbox", other: "" },
     comment: "form-control",
     dropdown: { root: "", control: "form-control" },
-    matrix: { root: "table" },
-    matrixdropdown: { root: "table" },
+    matrix: { root: "table", row: "form-group", label: "radio-inline", itemValue: "form-control" },
+    matrixdropdown: { root: "table", itemValue: "form-group" },
     matrixdynamic: { root: "table", button: "button" },
-    multipletext: { root: "table", itemTitle: "", itemValue: "form-control" },
-    radiogroup: { root: "form-inline", item: "radio", label: "", other: "" },
+    multipletext: { root: "table", itemTitle: "", row: "form-group", itemValue: "form-control" },
+    radiogroup: { root: "form-inline", item: "radio-inline", label: "radio-inline", other: "" },
     rating: { root: "btn-group", item: "btn btn-default" },
     text: "form-control",
     window: {
@@ -30,4 +30,4 @@ export var defaultBootstrapCss = {
         }
     }
 };
-surveyCss["bootstrap"] = defaultBootstrapCss;
+surveyCss["bootstrapmaterial"] = defaultBootstrapMaterialCss;

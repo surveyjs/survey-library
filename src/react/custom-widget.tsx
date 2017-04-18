77879a180aec6d244272a94b89a702183bf67404
tsv2013 <tsv2013@users.noreply.github.com>
2017-04-18 19:02:29 +0300

    Extracted custom widget and (fix for #339)

---
+++ src/react/custom-widget.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import {SurveyQuestionElementBase} from './reactquestionelement';
+
+export class SurveyCustomWidget extends SurveyQuestionElementBase {
+    constructor(props: any) {
+        super(props);
+    }
+    protected shouldComponentUpdate(): boolean {
+        return !!this.questionBase.customWidget.widgetJson.render;
+    }
+    componentDidMount() {
+        if (this.questionBase) {
+            let self = this;
+            let el = this.refs["root"];
+            if (this.questionBase.customWidget) {
+                el = this.refs["widget"];
+                if (el) this.questionBase.customWidget.afterRender(this.questionBase, el);
+            }
+        }
+    }
+    componentWillUnmount() {
+        let el = this.refs["root"];
+        if (this.questionBase.customWidget) {
+            el = this.refs["widget"];
+            if (el) this.questionBase.customWidget.willUnmount(this.questionBase, el);
+        }
+    }
+    render(): JSX.Element {
+        if (!this.questionBase || !this.creator) { return null; }
+        if (!this.questionBase.visible) { return null; }
+
+        let customWidget = this.questionBase.customWidget;
+
+        if (customWidget.widgetJson.isDefaultRender) {
+            return <div ref='widget'>{this.creator.createQuestionElement(this.questionBase)}</div>
+        }
+
+        let widget = null;
+        if (customWidget.widgetJson.render) {
+            widget = customWidget.widgetJson.render(this.questionBase);
+        } else {
+            if (customWidget.htmlTemplate) {
+                let htmlValue = { __html: customWidget.htmlTemplate };
+                return (<div ref='widget' dangerouslySetInnerHTML={htmlValue}></div>);
+            }
+        }
+        return <div ref='widget'>{widget}</div>
+    }
+}

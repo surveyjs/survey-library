b2e787cdfa4a8c446195e96b27eec7dbbd57ba40
tsv2013 <tsv2013@users.noreply.github.com>
2017-04-21 10:15:22 +0300

    Fixed tslint requirements

--- src/react/custom-widget.tsx
+++ src/react/custom-widget.tsx
@@ -7,19 +7,18 @@ export class SurveyCustomWidget extends SurveyQuestionElementBase {
     }
     componentDidMount() {
         if (this.questionBase) {
-            let self = this;
-            let el = this.refs["root"];
+            let el = this.refs['root'];
             if (this.questionBase.customWidget) {
-                el = this.refs["widget"];
-                if (el) this.questionBase.customWidget.afterRender(this.questionBase, el);
+                el = this.refs['widget'];
+                if (!!el) { this.questionBase.customWidget.afterRender(this.questionBase, el); }
             }
         }
     }
     componentWillUnmount() {
-        let el = this.refs["root"];
+        let el = this.refs['root'];
         if (this.questionBase.customWidget) {
-            el = this.refs["widget"];
-            if (el) this.questionBase.customWidget.willUnmount(this.questionBase, el);
+            el = this.refs['widget'];
+            if (!!el) { this.questionBase.customWidget.willUnmount(this.questionBase, el); }
         }
     }
     render(): JSX.Element {
@@ -29,7 +28,7 @@ export class SurveyCustomWidget extends SurveyQuestionElementBase {
         let customWidget = this.questionBase.customWidget;
 
         if (customWidget.widgetJson.isDefaultRender) {
-            return <div ref='widget'>{this.creator.createQuestionElement(this.questionBase)}</div>
+            return <div ref='widget'>{this.creator.createQuestionElement(this.questionBase)}</div>;
         }
 
         let widget = null;
@@ -38,9 +37,9 @@ export class SurveyCustomWidget extends SurveyQuestionElementBase {
         } else {
             if (customWidget.htmlTemplate) {
                 let htmlValue = { __html: customWidget.htmlTemplate };
-                return (<div ref='widget' dangerouslySetInnerHTML={htmlValue}></div>);
+                return <div ref='widget' dangerouslySetInnerHTML={htmlValue}></div>;
             }
         }
-        return <div ref='widget'>{widget}</div>
+        return <div ref='widget'>{widget}</div>;
     }
 }

d76dfcfd62f92db99c06db3a615f1078fc630528
tsv2013 <tsv2013@users.noreply.github.com>
2017-05-16 11:43:16 +0300

    Fix for: Radiogroup comments disappear when submitting survey #390

--- src/question_baseselect.ts
+++ src/question_baseselect.ts
@@ -204,7 +204,7 @@ export class QuestionSelectBase extends Question {
     }
     clearUnusedValues() {
         super.clearUnusedValues();
-        if(!this.isOtherSelected) {
+        if(!this.isOtherSelected && !this.hasComment) {
             this.comment = null;
         }
     }

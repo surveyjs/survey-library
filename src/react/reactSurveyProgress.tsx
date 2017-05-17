b8dd1b1d4d1da691feb5bf4f70aa0c414b3c942e
Mohsen Emami <moh.motamede@gmail.com>
2017-05-17 14:37:46 +0430

    fix: change css from bootstrap to foundation

--- src/react/reactSurveyProgress.tsx
+++ src/react/reactSurveyProgress.tsx
@@ -17,9 +17,11 @@ export class SurveyProgress extends SurveyNavigationBase {
     render(): JSX.Element {
         var style = this.isTop ? { width: "60%" } : { width: "60%", marginTop: "10px" };
         var progressStyle = { width: this.progress + "%" };
-        return (<div className={this.css.progress} style={style}>
-            <div style={progressStyle} className={this.css.progressBar} role="progressbar" aria-valuemin="0" aria-valuemax="100">
-                <span>{this.progressText}</span>
+        return (<div className={this.css.progress} >
+            <div className={this.css.progressBar} role="progressbar" aria-valuemin="0" aria-valuemax="100">
+                <span className='progress-meter' style={progressStyle}>
+                    <span className="progress-meter-text">{this.progressText}</span>
+                </span>
                 </div>
             </div>);
     }

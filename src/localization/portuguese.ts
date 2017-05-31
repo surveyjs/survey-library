6fce3d05da5bbfabf5fadd715443e9e368c7c48e
Ronney Ramon <ronney@ymail.com>
2017-05-31 03:48:08 -0300

    Portuguese (Brazil) localization. (#415)
    * Portuguese strings.
    
    * Fixing misspelled strings.
    
    * Fixing variable name.

---
+++ src/localization/portuguese.ts
@@ -0,0 +1,42 @@
+import {
+	surveyLocalization
+} from "../surveyStrings";
+
+export var portugueseSurveyStrings = {
+	pagePrevText: "Anterior",
+	pageNextText: "Próximo",
+	completeText: "Completo",
+	otherItemText: "Outros (descrever)",
+	progressText: "Pagina {0} de {1}",
+	emptySurvey: "Não há página visível ou pergunta na pesquisa.",
+	completingSurvey: "Obrigado por completar a pesquisa!",
+	loadingSurvey: "A pesquisa está carregando...",
+	optionsCaption: "Selecione...",
+	requiredError: "Por favor, responda a pergunta.",
+	requiredInAllRowsError: "Por favor, responda as perguntas em todas as linhas.",
+	numericError: "O valor deve ser numérico.",
+	textMinLength: "Por favor, insira pelo menos {0} caracteres.",
+	textMaxLength: "Por favor, insira menos de {0} caracteres.",
+	textMinMaxLength: "Por favor, insira mais de {0} e menos de {1} caracteres.",
+	minRowCountError: "Preencha pelo menos {0} linhas.",
+	minSelectError: "Selecione pelo menos {0} opções.",
+	maxSelectError: "Por favor, selecione não mais do que {0} opções.",
+	numericMinMax: "O '{0}' deve ser igual ou superior a {1} e igual ou menor que {2}",
+	numericMin: "O '{0}' deve ser igual ou superior a {1}",
+	numericMax: "O '{0}' deve ser igual ou inferior a {1}",
+	invalidEmail: "Por favor, informe um e-mail válido.",
+	urlRequestError: "A requisição retornou o erro '{0}'. {1}",
+	urlGetChoicesError: "A requisição não retornou dados ou o 'caminho' da requisição não está correto",
+	exceedMaxSize: "O tamanho do arquivo não deve exceder {0}.",
+	otherRequiredError: "Por favor, informe o outro valor.",
+	uploadingFile: "Seu arquivo está sendo carregado. Por favor, aguarde alguns segundos e tente novamente.",
+	addRow: "Adicionar linha",
+	removeRow: "Remover linha",
+	choices_firstItem: "primeiro item",
+	choices_secondItem: "segundo item",
+	choices_thirdItem: "terceiro item",
+	matrix_column: "Coluna",
+	matrix_row: "Linha"
+};
+
+surveyLocalization.locales["pt"] = portugueseSurveyStrings;

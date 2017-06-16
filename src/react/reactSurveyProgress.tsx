cf7a6ee4a6a7e03a01aa82faa2c70a30d98a3f3f
AndrewTelnov <andrew.telnov@gmail.com>
2017-06-16 12:47:16 +0300

    show progress text when the progress and width are small: https://github.com/surveyjs/surveyjs/issues/284

--- src/react/reactSurveyProgress.tsx
+++ src/react/reactSurveyProgress.tsx
@@ -16,7 +16,7 @@ export class SurveyProgress extends SurveyNavigationBase {
     protected get progressText(): string { return this.survey.progressText; }
     render(): JSX.Element {
         var style = this.isTop ? { width: "60%" } : { width: "60%", marginTop: "10px" };
-        var progressStyle = { width: this.progress + "%" };
+        var progressStyle = { width: "auto", minWidth: this.progress + "%", paddingLeft: "2px", paddingRight: "2px" };
         return (<div className={this.css.progress} style={style}>
             <div style={progressStyle} className={this.css.progressBar} role="progressbar" aria-valuemin="0" aria-valuemax="100">
                 <span>{this.progressText}</span>

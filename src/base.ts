3735e536ee2bcb58ff4a8c17738db4bed484e3b9
AndrewTelnov <andrew.telnov@gmail.com>
2017-06-26 11:44:59 +0300

    Fix a bug. A string value that contains spaces only should be considered as an empty value: https://github.com/surveyjs/surveyjs/issues/468

--- src/base.ts
+++ src/base.ts
@@ -81,6 +81,9 @@ export class Base {
      */
     public static isValueEmpty(value: any) {
         if (Array.isArray(value) && value.length === 0) return true;
+        if(value && (typeof value === 'string' || value instanceof String)) {
+            value = value.trim();
+        }
         return !value && value !== 0 && value !== false;
     }
     /**

3b275bb2ee0c26594c205cec6e5fbbf454c2dad3
Andrew Telnov (DevExpress) <Telnov@corp.devexpress.com>
2017-10-19 11:24:57 +0300

    Fix the build

--- src/vue/multipletext.vue
+++ src/vue/multipletext.vue
@@ -4,7 +4,7 @@
             <template v-for="(item, index) in row">
                 <td :class="question.cssClasses.itemTitle"><survey-string :locString="item.locTitle"/></td>
                 <td>
-                    <input v-if="!question.isReadOnly" :id="item.id" :class="question.cssClasses.itemValue" :type="item.inputType" :size="question.itemSize" :placeholder="item.placeHolder" :id="index === 0 ? question.inputId : ''" :value="item.value" @change="change(item, $event)" v-bind:aria-label="question.locTitle.renderedHtml"/>
+                    <input v-if="!question.isReadOnly" :id="item.id" :class="question.cssClasses.itemValue" :type="item.inputType" :size="question.itemSize" :placeholder="item.placeHolder" :value="item.value" @change="change(item, $event)" v-bind:aria-label="question.locTitle.renderedHtml"/>
                     <div v-else :class="question.cssClasses.itemValue" :id="item.id" :size="question.itemSize">{{item.value}}</div>
                 </td>
             </template>

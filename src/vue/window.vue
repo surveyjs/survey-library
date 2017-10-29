9d9b68444992b18a7b05595adc81d9c51f8401ce
Andrew Telnov (DevExpress) <Telnov@corp.devexpress.com>
2017-10-29 20:47:56 +0300

    FIx issues in survey window for vue platform: https://github.com/surveyjs/surveyjs/issues/740

--- src/vue/window.vue
+++ src/vue/window.vue
@@ -1,13 +1,13 @@
 <template>
-    <div v-show="window.isShowing" style="position: fixed; bottom: 3px; right: 10px;" :class="css.window.root">
+    <div v-show="surveyWindow.isShowing" style="position: fixed; bottom: 3px; right: 10px;" :class="css.window.root">
         <div :class="css.window.header.root">
             <a href="#" @click="doExpand" style="width:100%">
-                <span style="padding-right:10px" :class="css.window.header.title"><survey-string :locString="survey.locTitle"/></span>
+                <span style="padding-right:10px" :class="css.window.header.title"><survey-string :locString="windowSurvey.locTitle"/></span>
                 <span aria-hidden="true" :class="expandedCss"></span>
             </a>
         </div>
-        <div v-show="window.isExpanded" :class="css.window.body">
-            <survey :survey="survey">
+        <div v-show="isExpanded" :class="css.window.body">
+            <survey :survey="windowSurvey">
         </div>
     </div>
 </template>
@@ -18,36 +18,43 @@
     import {surveyCss} from "../defaultCss/cssstandard"
     import {SurveyModel} from '../survey'
     import {SurveyWindowModel} from '../surveyWindow'
+    import {VueSurveyWindowModel} from './surveyModel'
 
     @Component
     export default class Window extends Vue {
         @Prop
-        survey: SurveyModel
         window: SurveyWindowModel
+        @Prop
+        survey: SurveyModel
 
+        surveyWindow: SurveyWindowModel
         constructor () {
             super();
-            if(!this.window) {
-                this.window = new SurveyWindowModel(null, this.survey);
+            if(this.window) {
+                this.surveyWindow = this.window;
+            } else {
+                this.surveyWindow = new VueSurveyWindowModel(null, this.survey);
             }
-            this.survey = this.window.survey;
-            this.window.isShowing = true;
+            this.surveyWindow.isShowing = true;
             
             var self = this;
-            this.window.survey.onComplete.add(function(survey, options){
-                self.window.hide();
+            this.surveyWindow.survey.onComplete.add(function(survey, options){
+                Vue.set(self.surveyWindow, "isShowing", false);
             });
-            this.window.showingChangedCallback = function(){ Vue.set(self.window, "isShowing", self.window.isShowing); }
-            this.window.expandedChangedCallback = function(){ Vue.set(self.window, "isExpanded", self.window.isExpanded);}
+        }
+        get windowSurvey(): SurveyModel { 
+            return this.surveyWindow.survey; 
         }
         get css () {
             return surveyCss.getCss();
         }
         get expandedCss() {
-            return this.window.isExpanded ? this.css.window.header.buttonCollapsed : this.css.window.header.buttonExpanded;
+            return this.surveyWindow.isExpanded ? this.css.window.header.buttonCollapsed : this.css.window.header.buttonExpanded;
         }
+        get isExpanded(): boolean { return this.surveyWindow.isExpanded; }
+
         doExpand() {
-            this.window.isExpanded = !this.window.isExpanded;
+            this.surveyWindow.isExpanded = !this.surveyWindow.isExpanded;
         }
     }
     Vue.component("survey-window", Window)

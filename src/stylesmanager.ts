20edf4a6b29fe149df8ee217330e39c4cd3f3505
tsv2013 <tsv2013@users.noreply.github.com>
2017-12-18 16:40:33 +0300

    Fixed check and radio positions

--- src/stylesmanager.ts
+++ src/stylesmanager.ts
@@ -2,6 +2,16 @@ export class StylesManager {
   private sheet: CSSStyleSheet = null;
   private static SurveyJSStylesSheetId = "surveyjs";
   public static Styles: { [key: string]: string } = {
+    // ".sv_bootstrap_css":
+    //   "position: relative; width: 100%; background-color: #f4f4f4",
+    // ".sv_bootstrap_css .sv_custom_header":
+    //   "position: absolute; width: 100%; height: 275px; background-color: #e7e7e7;",
+    // ".sv_bootstrap_css .sv_container":
+    //   "max-width: 80%; margin: auto; position: relative; color: #6d7072; padding: 0 1em;",
+    // ".sv_bootstrap_css .panel-body":
+    //   "background-color: white; padding: 1em 1em 5em 1em; border-top: 2px solid lightgray;",
+    // ".sv_bootstrap_css .sv_qstn": "padding: 0.5em 1em 1.5em 1em;",
+
     ".sv_qstn": "display: inline-block; vertical-align: top;",
     ".progress": "width: 60%;",
     ".progress-bar": "width: auto; margin-left: 2px; margin-right: 2px;",
@@ -18,9 +28,9 @@ export class StylesManager {
     ".sv_q_radiogroup_inline .sv_q_radiogroup_other": "display: inline-block;",
     ".sv_q_checkbox_inline .sv_q_checkbox_other": "display: inline-block;",
     ".sv_q_checkbox_inline, .sv_q_radiogroup_inline": "line-height: 2.5em;",
-    ".form-inline .sv_q_checkbox_inline:not(:first-child)": "margin-left: 1em;",
-    ".form-inline .sv_q_radiogroup_inline:not(:first-child)":
-      "margin-left: 1em;",
+    ".form-inline .sv_q_checkbox_inline:not(:last-child)": "margin-right: 1em;",
+    ".form-inline .sv_q_radiogroup_inline:not(:last-child)":
+      "margin-right: 1em;",
     ".sv_qstn fieldset": "border: none; margin: 0; padding: 0;",
     "fieldset.form-inline": "display: inline-block;"
   };

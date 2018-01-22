c708b24467c9c80076dc7db70a0f189094860c82
dmitrykurmanov <kurmanov.work@gmail.com>
2018-01-22 14:18:29 +0300

    fixed https://surveyjs.answerdesk.io/ticket/details/T389 - dropdown with otherText in display mode

--- src/vue/dropdown.vue
+++ src/vue/dropdown.vue
@@ -6,25 +6,30 @@
                 <option v-for="(item, index) in question.visibleChoices" :value="item.value">{{item.text}}</option>
             </select>
         </div>
-        <div v-else :text="question.displayValue" :class="question.cssClasses.control"></div>
-        <survey-other-choice v-show="question.hasOther && question.isOtherSelected" :class="question.cssClasses.other" :question="question"/>
+        <div v-else :class="question.cssClasses.control">{{isOtherSelected ? question.otherText : question.displayValue}}</div>
+        <survey-other-choice v-show="isOtherSelected" :class="question.cssClasses.other" :question="question"/>
     </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import {Component, Prop} from 'vue-property-decorator'
-    import {default as Question} from './question'
-    import {QuestionDropdownModel} from '../question_dropdown'
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { default as Question } from "./question";
+import { QuestionDropdownModel } from "../question_dropdown";
 
-    @Component
-    export default class Dropdown extends Question<QuestionDropdownModel> {
-        get value() {
-            return !this.question.isEmpty() ?  this.question.value : '';
-        }
-        set value(newVal) {
-            this.question.value = newVal === '' ? undefined : newVal;
-        }
-    }
-    Vue.component("survey-dropdown", Dropdown)
+@Component
+export default class Dropdown extends Question<QuestionDropdownModel> {
+  get value() {
+    return !this.question.isEmpty() ? this.question.value : "";
+  }
+  set value(newVal) {
+    this.question.value = newVal === "" ? undefined : newVal;
+  }
+
+  get isOtherSelected() {
+    const question = this.question;
+    return question.hasOther && question.isOtherSelected;
+  }
+}
+Vue.component("survey-dropdown", Dropdown);
 </script>

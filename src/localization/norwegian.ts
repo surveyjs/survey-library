fdc9c9aaa1fa4f90bd1a91f8b2462cf4b4708084
John <johnnymoran44@hotmail.com>
2018-03-06 10:32:20 +0100

    Create norwegian.ts

---
+++ src/localization/norwegian.ts
@@ -0,0 +1,63 @@
+import { surveyLocalization } from "../surveyStrings";
+
+export var norwegianSurveyStrings = {
+  pagePrevText: "Tidligere",
+  pageNextText: "Neste",
+  completeText: "Fullstendig",
+  startSurveyText: "Start",
+  otherItemText: "Annet (beskrive)",
+  progressText: "Side {0} av {1}",
+  emptySurvey: "Det er ingen synlig side eller spørsmål i undersøkelsen.",
+  completingSurvey: "Takk for at du fullførte undersøkelsen!",
+  completingSurveyBefore:
+    "OVåre dokumenter viser at du allerede har gjennomført denne undersøkelsen.",
+  loadingSurvey: "Undersøkelsen laster inn ...",
+  optionsCaption: "Choose...",
+  value: "verdi",
+  requiredError: "Vennligst svar på spørsmålet.",
+  requiredErrorInPanel: "Vennligst svar på minst ett spørsmål.",
+  requiredInAllRowsError: "Vennligst svar på spørsmål i alle rader.",
+  numericError: "Verdien skal være numerisk.",
+  textMinLength: "Vennligst skriv inn minst {0} tegn.",
+  textMaxLength: "Vennligst skriv inn mindre enn {0} tegn.",
+  textMinMaxLength: "Vennligst skriv inn mer enn {0} og mindre enn {1} tegn.",
+  minRowCountError: "Vennligst fyll inn minst {0} rader.",
+  minSelectError: "Vennligst velg minst {0} varianter.",
+  maxSelectError: "Vennligst velg ikke mer enn {0} varianter.",
+  numericMinMax:
+    "'{0}' bør være lik eller mer enn {1} og lik eller mindre enn {2}",
+  numericMin: "'{0}' bør være lik eller mer enn {1}",
+  numericMax: "'{0}' bør være lik eller mindre enn {1}",
+  invalidEmail: "Vennligst skriv inn en gyldig e-post adresse.",
+  urlRequestError: "Forespørselen returnerte feilen '{0}'. {1}",
+  urlGetChoicesError:
+    "Forespørselen returnerte tomme data, eller 'banen' -egenskapen er feil",
+  exceedMaxSize: "Filstørrelsen bør ikke overstige {0}.",
+  otherRequiredError: "Vennligst skriv inn den andre verdien.",
+  uploadingFile:
+    "Filen din lastes opp. Vennligst vent noen sekunder og prøv igjen.",
+  confirmDelete: "Ønsker du å slette posten?",
+  keyDuplicationError: "Denne verdien skal være unik.",
+  addRow: "Legg til rad",
+  removeRow: "Fjerne",
+  addPanel: "Legg til ny",
+  removePanel: "Fjerne",
+  choices_Item: "punkt",
+  matrix_column: "Kolonne",
+  matrix_row: "Rad",
+  savingData: "Resultatene lagrer på serveren ...",
+  savingDataError: "Det oppsto en feil, og vi kunne ikke lagre resultatene.",
+  savingDataSuccess: "Resultatene ble lagret!",
+  saveAgainButton: "Prøv igjen",
+  timerMin: "min",
+  timerSec: "sek",
+  timerSpentAll: "Du har brukt {0} på denne siden og {1} totalt.",
+  timerSpentPage: "Du har brukt {0} på denne siden.",
+  timerSpentSurvey: "Du har tilbrakt {0} totalt.",
+  timerLimitAll:
+    "Du har brukt {0} av {1} på denne siden og totalt {2} av {3}.",
+  timerLimitPage: "Du har brukt {0} av {1} på denne siden.",
+  timerLimitSurvey: "Du har brukt {0} av {1} totalt.",
+  cleanCaption: "Ren"
+};
+surveyLocalization.locales["no"] = norwegianSurveyStrings;

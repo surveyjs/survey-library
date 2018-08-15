26c387ca477c9335deabdb1d2b46962209a12863
Andrew Telnov <andrew.telnov@gmail.com>
2018-08-15 21:38:59 +0300

    Fix the bug: panelState property does not work well in PanelDynamic in vue version, https://github.com/surveyjs/surveyjs/issues/1276

--- src/vue/panel.vue
+++ src/vue/panel.vue
@@ -6,7 +6,7 @@
         </h4>
         <div v-show="hasDescription" :class="css.panel.description"><survey-string :locString="question.locDescription"/></div>
         <survey-errors :question="question"/>
-        <div :style="{ paddingLeft: getIndentSize(question, question.innerIndent) }" v-show="!question.isCollapsed">
+        <div :style="{ paddingLeft: getIndentSize(question, question.innerIndent) }" v-show="!isCollapsed">
             <div v-for="(row, index) in rows" :key="question.id + '_' + index" v-if="row.visible"   :class="css.row">
                 <survey-row :row="row" :survey="survey" :css="css"></survey-row>
             </div>
@@ -28,11 +28,19 @@ export class Panel extends Vue {
   @Prop question: PanelModel;
   @Prop isEditMode: Boolean;
   @Prop css: any;
+  private isCollapsedValue: boolean = false;
 
   mounted() {
     if (this.question.survey) {
       this.question.survey.afterRenderPanel(this.question, this.$el);
     }
+    this.isCollapsed = this.question.isCollapsed;
+    var self = this;
+    this.question.registerFunctionOnPropertyValueChanged("state", function(
+      val
+    ) {
+      self.isCollapsed = self.question.isCollapsed;
+    });
   }
   get rootStyle() {
     var result = {};
@@ -60,8 +68,14 @@ export class Panel extends Vue {
   }
   get iconCss() {
     var result = "sv_panel_icon";
-    if (!this.question.isCollapsed) result += " sv_expanded";
+    if (!this.isCollapsed) result += " sv_expanded";
     return result;
+  }
+  get isCollapsed() {
+    return this.isCollapsedValue;
+  }
+  set isCollapsed(val: boolean) {
+    this.isCollapsedValue = val;
   }
   changeExpanded() {
     if (this.question.isCollapsed || this.question.isExpanded) {
@@ -79,8 +93,7 @@ export class Panel extends Vue {
     }
     return result;
   }
-  
 }
 Vue.component("survey-panel", Panel);
-    export default Panel;
+export default Panel;
 </script>

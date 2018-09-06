14f91a19635cb86d0d9a205a638c0920691e5fc7
dmitrykurmanov <kurmanov.work@gmail.com>
2018-09-06 10:31:10 +0300

    work for https://github.com/surveyjs/surveyjs/issues/1315

--- src/question_matrixdynamic.ts
+++ src/question_matrixdynamic.ts
@@ -197,7 +197,10 @@ export class QuestionMatrixDynamicModel extends QuestionMatrixDropdownModelBase
       );
     }
     if (this.survey) {
-      if (prevRowCount + 1 == this.rowCount) this.survey.matrixRowAdded(this);
+      if (prevRowCount + 1 == this.rowCount) {
+        this.survey.matrixRowAdded(this);
+        this.fireCallback(this.visibleRowsChangedCallback);
+      }
     }
   }
   /**

ec764afe50cf9e2d5f3871bd1ff637995172f26c
Andrew Telnov <andrew.telnov@gmail.com>
2018-11-30 16:26:25 +0300

    Update flow panel creation

--- src/flowpanel.ts
+++ src/flowpanel.ts
@@ -2,6 +2,7 @@ import { PanelModelBase } from "./panel";
 
 /**
  * The flow panel object. It is a container with flow layout where you can mix questions with mardown text.
+ *
  */
 export class FlowPanelModel extends PanelModelBase {
   constructor(name: string) {

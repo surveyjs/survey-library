aeee8003de4e76ddc9b3473a4163f2ce9f934494
Andrew Telnov <andrew.telnov@gmail.com>
2018-11-30 16:20:41 +0300

    Add flow panel class

---
+++ src/flowpanel.ts
@@ -0,0 +1,10 @@
+import { PanelModelBase } from "./panel";
+
+/**
+ * The flow panel object. It is a container with flow layout where you can mix questions with mardown text.
+ */
+export class FlowPanelModel extends PanelModelBase {
+  constructor(name: string) {
+    super(name);
+  }
+}

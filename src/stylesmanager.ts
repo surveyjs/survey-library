238910d3730c8a6a331f09ac36bf1afee5994bc5
tsv2013 <tsv2013@users.noreply.github.com>
2019-03-05 16:31:29 +0300

    Added inner question styles for bootstrap

--- src/stylesmanager.ts
+++ src/stylesmanager.ts
@@ -370,7 +370,8 @@ export class StylesManager {
     ".sv_main .progress": "width: 60%;",
     ".sv_main .progress-bar":
       "width: auto; margin-left: 2px; margin-right: 2px;",
-    ".sv_main .table>tbody>tr>td": "min-width: 90px;"
+    ".sv_main .table>tbody>tr>td": "min-width: 90px;",
+    ".sv_main f-panel .sv_qstn": "padding: 0; vertical-align: middle;"
   };
 
   public static bootstrapmaterialThemeCss: { [key: string]: string } = {

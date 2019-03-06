176c8fbbed2d50952256421e9a0a48ced624b8d4
tsv2013 <tsv2013@users.noreply.github.com>
2019-03-06 17:17:20 +0300

    Passed styles to flow panel text content in react

--- src/react/flow-panel.tsx
+++ src/react/flow-panel.tsx
@@ -61,12 +61,29 @@ export class SurveyFlowPanel extends SurveyPanel {
     }
     return nodes;
   }
+  private getStyle(nodeType: string) {
+    var style: any = {};
+    if (nodeType.toLowerCase() === "b") {
+      style.fontWeight = "bold";
+    }
+    if (nodeType.toLowerCase() === "i") {
+      style.fontStyle = "italic";
+    }
+    if (nodeType.toLowerCase() === "u") {
+      style.textDecoration = "underline";
+    }
+    return style;
+  }
   protected renderParentNode(node: Node): JSX.Element {
     var nodeType = node.nodeName.toLowerCase();
     var children = this.renderNodes(this.getChildDomNodes(node));
     if (nodeType == "div")
       return <div key={this.getNodeIndex()}>{children}</div>;
-    return <span key={this.getNodeIndex()}>{children}</span>;
+    return (
+      <span key={this.getNodeIndex()} style={this.getStyle(nodeType)}>
+        {children}
+      </span>
+    );
   }
   protected renderNode(node: Node): JSX.Element {
     if (!this.hasTextChildNodesOnly(node)) {
@@ -89,7 +106,11 @@ export class SurveyFlowPanel extends SurveyPanel {
     if (nodeType === "div") {
       return <div key={this.getNodeIndex()}>{node.textContent}</div>;
     }
-    return <span key={this.getNodeIndex()}>{node.textContent}</span>;
+    return (
+      <span key={this.getNodeIndex()} style={this.getStyle(nodeType)}>
+        {node.textContent}
+      </span>
+    );
   }
   private getChildDomNodes(node: Node): Array<Node> {
     var domNodes = [];

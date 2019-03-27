731ec3b8dc1ea897369a0f6e3864978eb5e0fcd3
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-03-27 15:12:34 +0300

    Add  automatic  update of sidebar

--- doc_generator/lib_docupdater.js
+++ doc_generator/lib_docupdater.js
@@ -12,24 +12,33 @@ var github = new Github({
 var repository = github.getRepo('surveyjs', 'service');
 
 var classesContent = fs.readFileSync('doc_generator/classes.json', 'utf8');
-
 repository.writeFile(
    'master',
    'surveyjs.io/App_Data/Docs/classes.json',
    classesContent,
-   'Updated API documentation',
+   'Updated Classes API documentation',
    function(err) {
       console.log(err);
    }
 );
 
 var pmesContent = fs.readFileSync('doc_generator/pmes.json', 'utf8');
-
 repository.writeFile(
    'master',
    'surveyjs.io/App_Data/Docs/pmes.json',
    pmesContent,
-   'Updated API documentation',
+   'Updated Properties API documentation',
+   function(err) {
+      console.log(err);
+   }
+);
+
+var sidebarContent = fs.readFileSync('doc_generator/sidebar.json', 'utf8');
+repository.writeFile(
+   'master',
+   'surveyjs.io/App_Data/Docs/sidebar.json',
+   sidebarContent,
+   'Updated SideBar API documentation',
    function(err) {
       console.log(err);
    }

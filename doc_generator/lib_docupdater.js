a4d3224ccd19c197bb2583eb85c202353d327ecd
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-03-27 17:15:03 +0300

    Add automatic push docs to service

--- doc_generator/lib_docupdater.js
+++ doc_generator/lib_docupdater.js
@@ -8,38 +8,27 @@ var github = new Github({
   password: process.env.GIT_PASS,
   auth: 'basic'
 });
-
 var repository = github.getRepo('surveyjs', 'service');
 
-var classesContent = fs.readFileSync('doc_generator/classes.json', 'utf8');
-repository.writeFile(
-   'master',
-   'surveyjs.io/App_Data/Docs/classes.json',
-   classesContent,
-   'Updated Classes API documentation',
-   function(err) {
-      console.log(err);
-   }
-);
+function commit(inputPath, outputPath, comment) {
+   var content = fs.readFileSync(inputPath, 'utf8');
+   repository.writeFile(
+      'master',
+      outputPath,
+      content,
+      comment,
+      function(err) {
+         console.log(err);
+      }
+   );
+}
 
-var pmesContent = fs.readFileSync('doc_generator/pmes.json', 'utf8');
-repository.writeFile(
-   'master',
-   'surveyjs.io/App_Data/Docs/pmes.json',
-   pmesContent,
-   'Updated Properties API documentation',
-   function(err) {
-      console.log(err);
-   }
-);
-
-var sidebarContent = fs.readFileSync('doc_generator/sidebar.json', 'utf8');
-repository.writeFile(
-   'master',
-   'surveyjs.io/App_Data/Docs/sidebar.json',
-   sidebarContent,
-   'Updated SideBar API documentation',
-   function(err) {
-      console.log(err);
-   }
-);
+commit('doc_generator/classes.json', 'surveyjs.io/App_Data/Docs/classes.json', 'Updated Classes API documentation');
+commit('doc_generator/pmes.json', 'surveyjs.io/App_Data/Docs/pmes.json', 'Updated Properties API documentation');
+commit('docs/sidebar.json', 'surveyjs.io/App_Data/Docs/sidebar.json', 'Updated SideBar');
+commit('docs/CreateQuiz.md', 'surveyjs.io/App_Data/Docs/CreateQuiz.md', 'Updated CreadQuiz.md');
+commit('docs/LibraryOverview.md', 'surveyjs.io/App_Data/Docs/LibraryOverview.md', 'Updated LibraryOverview.md');
+commit('docs/Question/visibleIf.md', 'surveyjs.io/App_Data/Docs/Question/visibleIf.md', 'Updated visibleIf.md');
+commit('docs/images/builder-adorners.png', 'surveyjs.io/Content/Images/docs/builder-adorners.png', 'Updated builder-adorners.png');
+commit('docs/images/builder-question-editor.png', 'surveyjs.io/Content/Images/docs/builder-question-editor.png', 'Updated builder-question-editor.png');
+commit('docs/images/choicesbyurl.png', 'surveyjs.io/Content/Images/docs/choicesbyurl.png', 'Updated choicesbyurl.png');

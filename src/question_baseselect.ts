dc593bfa16e44d3e1887d603624db5fe7624a31a
Andrew Telnov <andrew.telnov@gmail.com>
2019-04-23 21:13:01 +0300

    Fix the typo

--- src/question_baseselect.ts
+++ src/question_baseselect.ts
@@ -298,7 +298,7 @@ export class QuestionSelectBase extends Question {
     this.setPropertyValue("hideIfChoicesEmpty", val);
   }
   /**
-   * Please user survey.storeOthersAsComment to change the behavior on the survey level. This property is depricated and invisible in Survey Creator.
+   * Please use survey.storeOthersAsComment to change the behavior on the survey level. This property is depricated and invisible in Survey Creator.
    * By default the entered text in the others input in the checkbox/radiogroup/dropdown are stored as "question name " + "-Comment". The value itself is "question name": "others". Set this property to false, to store the entered text directly in the "question name" key.
    * Possible values are: "default", true, false
    * @see SurveyModel.storeOthersAsComment

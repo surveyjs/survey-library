1dd7158b716e2ab2b2782d2d2af11e7fe57c0dc7
gologames <gologames@users.noreply.github.com>
2019-07-04 13:26:20 +0300

    Change git token to git extraHeader

--- docupdate_npm.sh
+++ docupdate_npm.sh
@@ -1,7 +1,9 @@
 mkdir __update_docs_temp
 cd ./__update_docs_temp
 mkdir service
-git clone https://$GTTOKEN@github.com/surveyjs/service
+REPO_URL="https://github.com/surveyjs/service"
+git -c http.$REPO_URL.extraHeader="Authorization: Basic $GITAUTH" \
+  clone $REPO_URL
 mv ../docs/images .
 cp -a ../docs/. ./service/surveyjs.io/App_Data/Docs
 mv ./images ../docs

195d32edf145bdddb7aa1c00b0ea8f57a4f73bd4
dmitrykurmanov <kurmanov.work@gmail.com>
2019-08-20 20:37:08 +0300

    started with rating (vue); added title-container (vue);

--- src/vue/rating.vue
+++ src/vue/rating.vue
@@ -1,15 +1,42 @@
 <template>
-    <div>
-        <div :class="question.cssClasses.root">
-            <label v-for="(item, index) in question.visibleRateValues" :key="item.value" :class="getCss(question, item)">
-                <input type="radio" style="display: none;" :name="question.name" :id="question.name + index" :value="item.value" :disabled="question.isReadOnly" @change="change" v-bind:aria-required="question.isRequired" :aria-label="item.locText.text"/>
-                <span v-if="index === 0" :class="question.cssClasses.minText"><survey-string :locString="question.locMinRateDescription"/></span>
-                <span :class="question.cssClasses.itemText"><survey-string :locString="item.locText"/></span>
-                <span v-if="index === question.visibleRateValues.length-1" :class="question.cssClasses.maxText"><survey-string :locString="question.locMaxRateDescription"/></span>
-            </label>
-        </div>
-        <survey-other-choice v-show="question.hasOther" :class="question.cssClasses.other" :question="question"/>
+  <div>
+    <div :class="question.cssClasses.root">
+      <label
+        v-for="(item, index) in question.visibleRateValues"
+        :key="item.value"
+        :class="getCss(question, item)"
+      >
+        <input
+          type="radio"
+          style="display: none;"
+          :name="question.name"
+          :id="question.name + index"
+          :value="item.value"
+          :disabled="question.isReadOnly"
+          @change="change"
+          v-bind:aria-required="question.isRequired"
+          :aria-label="item.locText.text"
+        />
+        <span v-if="index === 0" :class="question.cssClasses.minText">
+          <survey-string :locString="question.locMinRateDescription" />
+        </span>
+        <span :class="question.cssClasses.itemText">
+          <survey-string :locString="item.locText" />
+        </span>
+        <span
+          v-if="index === question.visibleRateValues.length-1"
+          :class="question.cssClasses.maxText"
+        >
+          <survey-string :locString="question.locMaxRateDescription" />
+        </span>
+      </label>
     </div>
+    <survey-other-choice
+      v-show="question.hasOther"
+      :class="question.cssClasses.other"
+      :question="question"
+    />
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,17 +47,17 @@ import { QuestionRatingModel } from "../question_rating";
 
 @Component
 export class Rating extends QuestionVue<QuestionRatingModel> {
-  getCss(question: QuestionRatingModel, item:any) {
+  getCss(question: QuestionRatingModel, item: any) {
     let css = question.cssClasses.item;
     if (question.value == item.value) {
       css = css + " " + question.cssClasses.selected;
     }
     return css;
   }
-  change(e:any) {
+  change(e: any) {
     this.question.value = e.target.value;
   }
 }
 Vue.component("survey-rating", Rating);
-    export default Rating;
+export default Rating;
 </script>

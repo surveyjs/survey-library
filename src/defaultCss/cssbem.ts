1055f8a23b6b0dec4200bb7d021c115f1f5995c3
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-09-10 12:22:28 +0300

    Refactor

--- src/defaultCss/cssbem.ts
+++ src/defaultCss/cssbem.ts
@@ -85,9 +85,9 @@ export var bemCss = {
     itemInline: "sv-selectbase__item--inline",
     label: "sv-selectbase__label",
     itemControl: "sv-visuallyhidden sv-item__control sv-checkbox__control",
-    itemDecorator: "sv-item__decorator sv-checkbox__decorator",
+    itemDecorator: "sv-item__svg sv-checkbox__svg",
     controlLabel: "sv-item__control-label",
-    materialDecorator: "sv-item__material-decorator sv-checkbox__material-decorator",
+    materialDecorator: "sv-item__decorator sv-checkbox__decorator",
     other: "sv-comment",
     column: "sv-selectbase__column"
   },
@@ -97,9 +97,9 @@ export var bemCss = {
     itemInline: "sv-selectbase__item--inline",
     label: "sv-selectbase__label",
     itemControl: "sv-visuallyhidden sv-item__control sv-radio__control",
-    itemDecorator: "sv-item__decorator sv-radio__decorator",
+    itemDecorator: "sv-item__svg sv-radio__svg",
     controlLabel: "sv-item__control-label",
-    materialDecorator: "sv-item__material-decorator sv-radio__material-decorator",
+    materialDecorator: "sv-item__decorator sv-radio__decorator",
     other: "sv-comment",
     clearButton: "sv-btn sv-selectbase__clear-btn",
     column: "sv-selectbase__column"
@@ -108,12 +108,12 @@ export var bemCss = {
     root: "sv-selectbase",
     item: "sv-item sv-boolean sv-selectbase__label",
     control: "sv-visuallyhidden sv-item__control sv-boolean__control",
-    checkedMark: "sv-boolean__checked-mark",
-    uncheckedMark: "sv-boolean__unchecked-mark",
-    undefinedMark: "sv-boolean__undefined-mark",
-    itemDecorator: "sv-item__decorator sv-boolean__decorator",
+    checkedMark: "sv-boolean__checked-path",
+    uncheckedMark: "sv-boolean__unchecked-path",
+    undefinedMark: "sv-boolean__undefined-path",
+    itemDecorator: "sv-item__svg  sv-boolean__svg",
     label: "sv-item__control-label",
-    materialDecorator: "sv-item__material-decorator sv-boolean__material-decorator "
+    materialDecorator: "sv-item__decorator sv-boolean__decorator "
   },
   text: {
     root: "sv-text",
@@ -151,8 +151,8 @@ export var bemCss = {
     headerCell: "sv-table__cell sv-table__cell--header",
     label: "sv-item sv-radio sv-matrix__label",
     itemValue: "sv-visuallyhidden sv-item__control sv-radio__control",
-    materialDecorator: "sv-item__material-decorator sv-radio__material-decorator",
-    itemDecorator: "sv-item__decorator sv-radio__decorator",
+    materialDecorator: "sv-item__decorator sv-radio__decorator",
+    itemDecorator: "sv-item__svg sv-radio__svg",
     cellText: "sv-matrix__cell-text",
     cellTextSelected: "sv-matrix__cell-text--selected"
   },

36a2c32b67c2f580f1d30a3463d62011ac5f889f
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-09-20 13:21:48 +0300

    Add remove file svg for file question

--- src/vue/file.vue
+++ src/vue/file.vue
@@ -15,7 +15,9 @@
                 <img v-if="question.canPreviewImage(val)" :src="val.content" :height="question.imageHeight" :width="question.imageWidth" alt="File preview">
                 <div v-if="val.name && !question.isReadOnly">
                   <span @click="doRemoveFile(val)" :class="question.cssClasses.removeFile">{{question.removeFileCaption}}</span>
-                  <svg  @click="doRemoveFile(val)" :class="question.cssClasses.removeFileSvg" viewBox="0 0 14 14"></svg>                
+                  <svg :class="question.cssClasses.removeFileSvg" @click="doRemoveFile(val)" viewBox="0 0 16 16">
+                    <path d="M8,2C4.7,2,2,4.7,2,8s2.7,6,6,6s6-2.7,6-6S11.3,2,8,2z M11,10l-1,1L8,9l-2,2l-1-1l2-2L5,6l1-1l2,2l2-2l1,1L9,8 L11,10z"/>
+                  </svg>   
                 </div>
                   <div v-if="val.name" :class="question.cssClasses.fileSignBottom">
                   <a :href="val.content" :title="val.name" :download="val.name" :width="question.imageWidth">{{val.name}}</a>

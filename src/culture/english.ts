25c35200f69b028ab376d981efa0d0da22d82e41
gologames <gologames@users.noreply.github.com>
2019-11-12 13:59:37 +0300

    Add currencySymbol to cultureInfo

--- src/culture/english.ts
+++ src/culture/english.ts
@@ -4,7 +4,8 @@
 export var englishCulture = {
     shortDateFormats: <Array<any>>[
     ],
-    dateSeparators: [ "/" ]
+    dateSeparators: [ "/" ],
+    currencySymbol: "$"
 };
 
 //Uncomment these two lines on creating a translation file. You should replace "en" and enStrings with your locale ("fr", "de" and so on) and your variable.

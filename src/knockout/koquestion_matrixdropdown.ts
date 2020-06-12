1a2e0b18769edc6bc8e55f985c5b75fc9d98e002
Andrew Telnov <andrew.telnov@gmail.com>
2020-06-12 15:06:50 +0300

    onAfterRenderQuestionInput event: urvey-react and survey-knockout giving different htmlElements #2218

--- src/knockout/koquestion_matrixdropdown.ts
+++ src/knockout/koquestion_matrixdropdown.ts
@@ -16,6 +16,7 @@ import { ImplementorBase } from "./kobase";
 
 export class QuestionMatrixBaseImplementor extends QuestionImplementor {
   koCellAfterRender: any;
+  koCellQuestionAfterRender: any;
   koRecalc: any;
   koAddRowClick: any;
   koRemoveRowClick: any;
@@ -27,6 +28,9 @@ export class QuestionMatrixBaseImplementor extends QuestionImplementor {
     var self = this;
     this.koCellAfterRender = function (el: any, con: any) {
       return self.cellAfterRender(el, con);
+    };
+    this.koCellQuestionAfterRender = function (el: any, con: any) {
+      return self.cellQuestionAfterRender(el, con);
     };
     this.koRecalc = ko.observable(0);
     this.koTable = ko.pureComputed(function () {
@@ -61,6 +65,9 @@ export class QuestionMatrixBaseImplementor extends QuestionImplementor {
     });
     (<any>this.question)["koTable"] = this.koTable;
     (<any>this.question)["koCellAfterRender"] = this.koCellAfterRender;
+    (<any>this.question)[
+      "koCellQuestionAfterRender"
+    ] = this.koCellQuestionAfterRender;
     (<any>this.question)["koAddRowClick"] = this.koAddRowClick;
     (<any>this.question)["koRemoveRowClick"] = this.koRemoveRowClick;
     (<any>this.question)["koIsAddRowOnTop"] = this.koIsAddRowOnTop;
@@ -74,7 +81,6 @@ export class QuestionMatrixBaseImplementor extends QuestionImplementor {
     var el = SurveyElement.GetFirstNonTextElement(elements);
     if (!el) return;
     var cell = <QuestionMatrixDropdownRenderedCell>con;
-    cell.question.afterRenderQuestionElement(el);
     if (cell.question.customWidget) {
       cell.question.customWidget.afterRender(cell.question, el);
       ko.utils.domNodeDisposal.addDisposeCallback(el, () => {
@@ -89,6 +95,13 @@ export class QuestionMatrixBaseImplementor extends QuestionImplementor {
       column: !!cell.cell ? cell.cell.column : null,
     };
     this.question.survey.matrixAfterCellRender(this.question, options);
+  }
+  private cellQuestionAfterRender(elements: any, con: any) {
+    if (!this.question.survey) return;
+    var el = SurveyElement.GetFirstNonTextElement(elements);
+    if (!el) return;
+    var cell = <QuestionMatrixDropdownRenderedCell>con;
+    cell.question.afterRenderQuestionElement(el);
   }
   protected isAddRowTop(): boolean {
     return false;

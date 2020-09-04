bfbeba97aeca9f330f84f57718b8b5c7d27410d1
gologames <gologames@bk.ru>
2020-09-04 17:46:37 +0300

    Implemented click on elements event of progress buttons navigation

--- src/knockout/components/progress/buttons.ts
+++ src/knockout/components/progress/buttons.ts
@@ -1,17 +1,32 @@
 import * as ko from "knockout";
+import { PageModel } from '../../../page';
 var template = require("html-loader?interpolate!val-loader!./buttons.html");
 export var progressButtonsComponent: any;
 
 ko.components.register("survey-progress-buttons", {
     viewModel: function (params: any) {
-      this.survey = params.model;
       var getListElementCss = function(index: any): string {
         return (params.model.currentPageNo === index() ?
           params.model.css.progressButtonsListElementCurrent : "") ||
           (params.model.visiblePages[index()].passed ?
             params.model.css.progressButtonsListElementPassed : "");
-      }
-      return { model: this.survey, getListElementCss: getListElementCss };
+      };
+      var clickListElement = function(page: PageModel): void {
+        if (params.model['isCompleted']) return;
+        if (page.visibleIndex < params.model.currentPageNo) {
+          params.model.currentPageNo = page.visibleIndex;
+        }
+        else if (page.visibleIndex > params.model.currentPageNo) {
+          var i = params.model.currentPageNo;
+          for (; i < page.visibleIndex; i++) {
+            if (params.model.visiblePages[i].hasErrors(true, true)) break;
+            params.model.visiblePages[i].passed = true;
+          }
+          params.model.currentPageNo = i;
+        }
+      };
+      return { model: params.model,
+        getListElementCss: getListElementCss, clickListElement: clickListElement };
       //nodeDOMremoval чтобы отключать таймер
     },
     template: template

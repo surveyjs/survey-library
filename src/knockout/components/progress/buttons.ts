1435d9ec37eec821f6c84c5893c40a2a01fe7026
gologames <gologames@bk.ru>
2020-09-25 20:04:39 +0300

    Changed viewmodel of progress buttons component to class. Implemented checking scroll buttons visibility via setInterval

--- src/knockout/components/progress/buttons.ts
+++ src/knockout/components/progress/buttons.ts
@@ -1,42 +1,68 @@
 import * as ko from "knockout";
-import { PageModel } from '../../../page';
+import { SurveyModel } from "../../../survey";
+import { PageModel } from "../../../page";
 var template = require("html-loader?interpolate!val-loader!./buttons.html");
 
-export var progressButtonsViewModel: any = function(params: any) {
-  var getListElementCss = function(index: any): string {
-    if (index() >= params.model.visiblePages.length) return;
-    var elementCss: string = params.model.visiblePages[index()].passed ?
-      params.model.css.progressButtonsListElementPassed : "";
-    if (params.model.currentPageNo === index()) {
+export class ProgressButtonsViewModel {
+  private hasScroller = ko.observable(false);
+  private updateScroller: any = undefined;
+  constructor (private model: SurveyModel, private element: any) {
+    this.updateScroller = setInterval(() => {
+      let listContainerElement: HTMLElement = this.element.querySelector(
+        "." + model.css.progressButtonsListContainer);
+      if (!!listContainerElement) {
+        this.hasScroller(listContainerElement.scrollWidth > listContainerElement.offsetWidth);
+      }
+    }, 100);
+  }
+  public getListElementCss(index: any): string {
+    if (index() >= this.model.visiblePages.length) return;
+    let elementCss: string = this.model.visiblePages[index()].passed ?
+      this.model.css.progressButtonsListElementPassed : "";
+    if (this.model.currentPageNo === index()) {
       elementCss += !!elementCss ? " " : "";
-      elementCss += params.model.css.progressButtonsListElementCurrent;
+      elementCss += this.model.css.progressButtonsListElementCurrent;
     }
     return elementCss;
-  };
-  var clickListElement = function(page: PageModel): void {
-    if (page.visibleIndex < params.model.currentPageNo) {
-      params.model.currentPageNo = page.visibleIndex;
+  }
+  public clickListElement(page: PageModel): void {
+    if (page.visibleIndex < this.model.currentPageNo) {
+      this.model.currentPageNo = page.visibleIndex;
     }
-    else if (page.visibleIndex > params.model.currentPageNo) {
-      var i = params.model.currentPageNo;
+    else if (page.visibleIndex > this.model.currentPageNo) {
+      let i: number = this.model.currentPageNo;
       for (; i < page.visibleIndex; i++) {
-        if (params.model.visiblePages[i].hasErrors(true, true)) break;
-        params.model.visiblePages[i].passed = true;
+        if (this.model.visiblePages[i].hasErrors(true, true)) break;
+        this.model.visiblePages[i].passed = true;
       }
-      params.model.currentPageNo = i;
+      this.model.currentPageNo = i;
     }
-  };
-  var clickScrollButton = function(listContainerElement: Element, scrollToLeft: boolean) {
-    listContainerElement.scrollLeft += (scrollToLeft ? -1 : 1) * 70;
-  };
-  return { model: params.model,
-    getListElementCss: getListElementCss,
-    clickListElement: clickListElement,
-    clickScrollButton: clickScrollButton };
-  //nodeDOMremoval чтобы отключать таймер
+  }
+  public getScrollButtonCss(isLeftScroll: boolean) {
+    return ko.computed(function() {
+      let scrollCss: string = isLeftScroll ?
+        this.model.css.progressButtonsImageButtonLeft :
+        this.model.css.progressButtonsImageButtonRight;
+      if (!this.hasScroller()) scrollCss += " " + this.model.css.progressButtonsImageButtonHidden;
+      return scrollCss;
+    }, this);
+  }
+  public clickScrollButton(listContainerElement: Element, isLeftScroll: boolean) {
+    listContainerElement.scrollLeft += (isLeftScroll ? -1 : 1) * 70;
+  }
+  public dispose() {
+    if (typeof this.updateScroller !== "undefined") {
+      clearInterval(this.updateScroller);
+      this.updateScroller = undefined;
+    }
+  }
 };
 
 ko.components.register("survey-progress-buttons", {
-  viewModel: progressButtonsViewModel,
+  viewModel: {
+    createViewModel: (params: any, componentInfo: any) => {
+      return new ProgressButtonsViewModel(params.model, componentInfo.element.nextElementSibling);
+    },
+  },
   template: template
 });

1ab17dba77798eb87c8fd481c5369a303e3bf5e3
gologames <gologames@bk.ru>
2020-10-05 13:04:48 +0300

    Refactored progress buttons knockout components

--- src/knockout/components/progress/progress.ts
+++ src/knockout/components/progress/progress.ts
@@ -4,8 +4,21 @@ export var progressProgressViewModel: any = function(params: any) {
   this.survey = params.model;
   return { model: this.survey };
 };
-
 ko.components.register("survey-progress-progress", {
   viewModel: progressProgressViewModel,
   template: template
+});
+
+var templateBridge = "<!-- ko component: { name: 'survey-progress-progress', params: $data } --><!-- /ko -->";
+ko.components.register("survey-progress-pages", {
+  viewModel: progressProgressViewModel,
+  template: templateBridge
+});
+ko.components.register("survey-progress-questions", {
+  viewModel: progressProgressViewModel,
+  template: templateBridge
+});
+ko.components.register("survey-progress-correctquestions", {
+  viewModel: progressProgressViewModel,
+  template: templateBridge
 });

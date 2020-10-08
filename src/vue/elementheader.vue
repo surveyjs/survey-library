74abc440a52f7f00dc6a9d8232077a16ac13e43d
Andrew Telnov <andrew.telnov@gmail.com>
2020-10-08 14:07:22 +0300

    Vue: undefined click handler error on clicking checkbox items in modern theme #2443

--- src/vue/elementheader.vue
+++ src/vue/elementheader.vue
@@ -1,5 +1,5 @@
  <template>
-  <div :class="getHeaderClass(element)" @click="element.clickTitleFunction">
+  <div :class="getHeaderClass(element)" @click="function() { if (element.hasInput) element.focus(); return true; }">
     <h5
       v-if="element.hasTitle"
       :class="getTitleClass(element)"

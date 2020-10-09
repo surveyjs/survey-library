7b12c4ac815b67a3271696692ea4734f1a2f0b01
Andrew Telnov <andrew.telnov@gmail.com>
2020-10-09 17:39:56 +0300

    End-user can drop/add a file into read-only file question #2448

--- src/vue/file.vue
+++ src/vue/file.vue
@@ -108,11 +108,15 @@ import { QuestionFileModel } from "../question_file";
 import {
   confirmAction,
   detectIEOrEdge,
-  loadFileFromBase64
+  loadFileFromBase64,
 } from "../utils/utils";
 @Component
 export class File extends QuestionVue<QuestionFileModel> {
   onDragOver = (event: any) => {
+    if (this.question.isReadOnly) {
+      event.returnValue = false;
+      return false;
+    }
     event.preventDefault();
   };
   onDrop = (event: any) => {

58d2d9fa6b2c17f84aae3e50a54e802837c377b5
gologames <gologames@bk.ru>
2020-10-12 13:40:58 +0300

    Implemented react progress buttons markup

--- src/react/reactSurveyProgressButtons.tsx
+++ src/react/reactSurveyProgressButtons.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { SurveyNavigationBase } from "./reactSurveyNavigationBase";
 import { ReactElementFactory } from "./element-factory";
+import { PageModel } from "../page";
 
 export class SurveyProgressButtons extends SurveyNavigationBase {
   constructor(props: any) {
@@ -8,8 +9,43 @@ export class SurveyProgressButtons extends SurveyNavigationBase {
   }
   render(): JSX.Element {
     return (
-      <div>Progress Buttons Navigations</div>
+      <div className={this.css.progressButtonsContainerCenter}>
+        <div className={this.css.progressButtonsContainer}>
+          <img className={this.getScrollButtonCss(true)}></img>
+          <div className={this.css.progressButtonsListContainer}>
+            <ul className={this.css.progressButtonsList}>
+              {this.getListElements()}
+            </ul>
+          </div>
+          <img className={this.getScrollButtonCss(false)}></img>
+        </div>
+      </div>
     );
+  }
+  protected getScrollButtonCss(isLeftScroll: boolean): string {
+    return "";
+  }
+  protected getListElements(): JSX.Element[] {
+    let buttons: JSX.Element[] = [];
+    this.survey.visiblePages.forEach((page: PageModel) => {
+      buttons.push(this.renderListElement(page));
+    });
+    return buttons;
+  }
+  protected renderListElement(page: PageModel): JSX.Element {
+    return (
+      <li key={"listelement" + page.visibleIndex} className={this.getListElementCss(page)}>
+        <div className={this.css.progressButtonsPageTitle}>
+          {page.navigationTitle || page.name}
+        </div>
+        <div className={this.css.progressButtonsPageDescription}>
+          {page.navigationDescription}
+        </div>
+      </li>
+    );
+  }
+  protected getListElementCss(page: PageModel): string {
+    return "";
   }
 }
 

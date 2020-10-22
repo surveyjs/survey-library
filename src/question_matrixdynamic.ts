e6418b45bcb33d0ac1e549fae4a0bd58e52d01f8
Andrew Telnov <andrew.telnov@gmail.com>
2020-10-22 17:17:10 +0300

    Fix: Do not generate an empty rows error for matrix dynamic if it not requried and minRowCount is set #2479
    (cherry picked from commit 1ae8bb45a1cc9baf9186ed73710ff8231988a900)

--- src/question_matrixdynamic.ts
+++ src/question_matrixdynamic.ts
@@ -522,8 +522,7 @@ export class QuestionMatrixDynamicModel extends QuestionMatrixDropdownModelBase
     return isDuplicated || prevValue;
   }
   private hasErrorInRows(): boolean {
-    if (this.minRowCount <= 0 || !this.generatedVisibleRows) return false;
-    var res = false;
+    if (this.minRowCount <= 0 || !this.isRequired || !this.generatedVisibleRows) return false;
     var setRowCount = 0;
     for (
       var rowIndex = 0;

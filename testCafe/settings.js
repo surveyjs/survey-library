01d4ca13d14c0522d97ddceda6be173aa491f1b3
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2020-11-05 12:55:38 +0300

    changed f-f tests to minified scripts;

--- testCafe/settings.js
+++ testCafe/settings.js
@@ -1,6 +1,6 @@
 import { ClientFunction } from "testcafe";
 export const frameworks = ["knockout", "jquery", "react", "vue"];
-export const url = "http://127.0.0.1:8080/examples/";
+export const url = "http://127.0.0.1:8080/examples_test/default/";
 export const url_test = "http://127.0.0.1:8080/examples_test/";
 
 export const initSurvey = ClientFunction((framework, json, events) => {

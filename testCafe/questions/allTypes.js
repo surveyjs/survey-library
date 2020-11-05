be14e2fa248161c173bae217112d97d744a60723
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2020-11-05 12:31:36 +0300

    added bootstrap theme for "allTypes" test

--- testCafe/questions/allTypes.js
+++ testCafe/questions/allTypes.js
@@ -7,34 +7,38 @@ var json = {
   questions: [
     {
       type: "text",
-      name: "text_question"
-    }
-  ]
+      name: "text_question",
+    },
+  ],
 };
 
 const applyTheme = ClientFunction((theme) => {
   Survey.StylesManager.applyTheme(theme);
 });
-frameworks.forEach((framework) => {
-  fixture`${framework} ${title}`.page`${url_test}modern/${framework}.html`.beforeEach(
-    async (t) => {
-      await applyTheme("modern");
+["modern", "bootstrap"].forEach((theme) => {
+  frameworks.forEach((framework) => {
+    fixture`${framework} ${title}`
+      .page`${url_test}${theme}/${framework}.html`.beforeEach(async (t) => {
+      await applyTheme(theme);
       await initSurvey(framework, json);
-    }
-  );
-  test("check survey will all types", async t => {
-    const getQuestionInputIdByName = ClientFunction((name) => {
-      let question = survey.getQuestionByName(name);
-      return question.inputId;
     });
+    test("check survey will all types", async (t) => {
+      const getQuestionInputIdByName = ClientFunction((name) => {
+        let question = survey.getQuestionByName(name);
+        return question.inputId;
+      });
 
-    await t
-      .typeText(`input[id=${await getQuestionInputIdByName("text_question")}]`, "test text")
-      .click("input[value=Complete]");
+      await t
+        .typeText(
+          `input[id=${await getQuestionInputIdByName("text_question")}]`,
+          "test text"
+        )
+        .click("input[value=Complete]");
 
-    let surveyResult = await getSurveyResult();
-    assert.deepEqual(surveyResult, {
-      text_question: "test text"
+      let surveyResult = await getSurveyResult();
+      assert.deepEqual(surveyResult, {
+        text_question: "test text",
+      });
     });
   });
 });

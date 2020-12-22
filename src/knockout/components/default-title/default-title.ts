2d03a8215f67ee21b1e24b3368f01fe15b789570
Dmitry Kuzin (DevExpress) <dmitry.kuzin@devexpress.com>
2020-12-22 12:58:55 +0300

    Fix default title for panel

--- src/knockout/components/default-title/default-title.ts
+++ src/knockout/components/default-title/default-title.ts
@@ -1,14 +1,29 @@
 import * as ko from "knockout";
+import { Question } from "../../../question";
 import { RendererFactory } from "../../../rendererFactory";
+import { Panel } from "../../kopage";
 
 const template = require("./default-title.html");
 
-export var DefaultTitleViewModel: any;
+export class DefaultTitleViewModel {
+  constructor(public element: Question | Panel) {}
+
+  getIconClass() {
+    var element = this.element;
+    var cssClasses = element.isPanel
+      ? element.cssClasses.panel
+      : element.cssClasses;
+    return (
+      cssClasses.icon +
+      (!element.isCollapsed ? " " + cssClasses.iconExpanded : "")
+    );
+  }
+}
 
 ko.components.register("sv-default-title", {
   viewModel: {
     createViewModel: (params: any, componentInfo: any) => {
-      return params.element;
+        return new DefaultTitleViewModel(params.element);
     },
   },
   template: template,

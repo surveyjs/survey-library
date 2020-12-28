5bcb8e35e5ed9527663fd7d4162b493311e57064
Andrew Telnov <andrew.telnov@gmail.com>
2020-12-28 18:18:30 +0300

    Add support for nested question in Logic UI for composite components #2592

--- src/question_custom.ts
+++ src/question_custom.ts
@@ -1,4 +1,4 @@
-import { Question } from "./question";
+import { Question, IConditionObject } from "./question";
 import { Serializer, CustomPropertiesCollection } from "./jsonobject";
 import {
   ISurveyImpl,
@@ -613,6 +613,22 @@ export class QuestionCompositeModel extends QuestionCustomModelBase {
       this.settingNewValue = true;
       q.value = newValue;
       this.settingNewValue = false;
+    }
+  }
+  public addConditionObjectsByContext(
+    objects: Array<IConditionObject>,
+    context: any
+  ) {
+    if (!this.contentPanel) return;
+    var questions = this.contentPanel.questions;
+    var prefixName = this.name;
+    var prefixText = this.title;
+    for (var i = 0; i < questions.length; i++) {
+      objects.push({
+        name: prefixName + "." + questions[i].name,
+        text: prefixText + "." + questions[i].title,
+        question: questions[i],
+      });
     }
   }
   protected convertDataValue(name: string, newValue: any): any {

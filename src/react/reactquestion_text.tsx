9c0742500cfb2b1c9886c1fd1d4f7e7925622c6c
Andrew Telnov <andrew.telnov@gmail.com>
2021-01-04 09:20:07 +0300

    Imlement questiontext.dataList property in react, #2596

--- src/react/reactquestion_text.tsx
+++ src/react/reactquestion_text.tsx
@@ -3,9 +3,7 @@ import { SurveyQuestionUncontrolledElement } from "./reactquestion_element";
 import { QuestionTextModel } from "../question_text";
 import { ReactQuestionFactory } from "./reactquestion_factory";
 
-export class SurveyQuestionText extends SurveyQuestionUncontrolledElement<
-  QuestionTextModel
-> {
+export class SurveyQuestionText extends SurveyQuestionUncontrolledElement<QuestionTextModel> {
   private _isWaitingForEnter = false;
   constructor(props: any) {
     super(props);
@@ -17,45 +15,62 @@ export class SurveyQuestionText extends SurveyQuestionUncontrolledElement<
       : null;
     var onKeyDown = null;
     var onKeyUp = null;
-    if(this.question.isInputTextUpdate) {
-      onKeyDown = (e: any) => this._isWaitingForEnter = (e.keyCode === 229);
+    if (this.question.isInputTextUpdate) {
+      onKeyDown = (e: any) => (this._isWaitingForEnter = e.keyCode === 229);
       onKeyUp = (e: any) => {
-        if(!this._isWaitingForEnter || (e.keyCode === 13)) {
+        if (!this._isWaitingForEnter || e.keyCode === 13) {
           this.updateValueOnEvent(e);
           this._isWaitingForEnter = false;
         }
-      }
+      };
     }
     var placeHolder =
       this.question.inputType === "range" || this.question.isReadOnly
         ? ""
         : this.question.placeHolder;
+    var dataList = this.renderDataList();
     return (
-      <input
-        id={this.question.inputId}
-        disabled={this.isDisplayMode}
-        className={cssClasses.root}
-        type={this.question.inputType}
-        ref={(input) => (this.control = input)}
-        maxLength={this.question.getMaxLength()}
-        min={this.question.renderedMin}
-        max={this.question.renderedMax}
-        step={this.question.step}
-        size={this.question.size}
-        placeholder={placeHolder}
-        autoComplete={this.question.autoComplete}
-        onBlur={onBlur}
-        // onChange={this.updateValueOnEvent}
-        onKeyUp={onKeyUp}
-        onKeyDown={onKeyDown}
-        aria-required={this.question.isRequired}
-        aria-label={this.question.locTitle.renderedHtml}
-        aria-invalid={this.question.errors.length > 0}
-        aria-describedby={
-          this.question.errors.length > 0 ? this.question.id + "_errors" : null
-        }
-      />
+      <React.Fragment>
+        <input
+          id={this.question.inputId}
+          disabled={this.isDisplayMode}
+          className={cssClasses.root}
+          type={this.question.inputType}
+          ref={(input) => (this.control = input)}
+          maxLength={this.question.getMaxLength()}
+          min={this.question.renderedMin}
+          max={this.question.renderedMax}
+          step={this.question.step}
+          size={this.question.size}
+          placeholder={placeHolder}
+          list={this.question.dataListId}
+          autoComplete={this.question.autoComplete}
+          onBlur={onBlur}
+          // onChange={this.updateValueOnEvent}
+          onKeyUp={onKeyUp}
+          onKeyDown={onKeyDown}
+          aria-required={this.question.isRequired}
+          aria-label={this.question.locTitle.renderedHtml}
+          aria-invalid={this.question.errors.length > 0}
+          aria-describedby={
+            this.question.errors.length > 0
+              ? this.question.id + "_errors"
+              : null
+          }
+        />
+        {dataList}
+      </React.Fragment>
     );
+  }
+  private renderDataList(): JSX.Element {
+    if (!this.question.dataListId) return null;
+    var items = this.question.dataList;
+    if (items.length == 0) return null;
+    var options = [];
+    for (var i = 0; i < items.length; i++) {
+      options.push(<option value={items[i]}></option>);
+    }
+    return <datalist id={this.question.dataListId}>{options}</datalist>;
   }
 }
 

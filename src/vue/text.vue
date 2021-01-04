c00d9560252dcde67b436cccb4b70664226f3773
Andrew Telnov <andrew.telnov@gmail.com>
2021-01-04 16:53:19 +0300

    Implement questiontext.dataList property in vue, #2596

--- src/vue/text.vue
+++ src/vue/text.vue
@@ -1,30 +1,36 @@
 <template>
-  <input
-    :disabled="question.isReadOnly"
-    :class="question.cssClasses.root"
-    :type="question.inputType"
-    :maxlength="question.getMaxLength()"
-    :min="question.renderedMin"
-    :max="question.renderedMax"
-    :step="question.step"
-    :size="question.size"
-    :id="question.inputId"
-    :placeholder="
-      question.inputType === 'range' || question.isReadOnly
-        ? ''
-        : question.placeHolder
-    "
-    :autocomplete="question.autoComplete"
-    :value="question.value"
-    @change="change"
-    @keyup="keyup"
-    v-bind:aria-required="question.isRequired"
-    v-bind:aria-label="question.locTitle.renderedHtml"
-    :aria-invalid="question.errors.length > 0"
-    :aria-describedby="
-      question.errors.length > 0 ? question.id + '_errors' : null
-    "
-  />
+  <div>
+    <input
+      :disabled="question.isReadOnly"
+      :class="question.cssClasses.root"
+      :type="question.inputType"
+      :maxlength="question.getMaxLength()"
+      :min="question.renderedMin"
+      :max="question.renderedMax"
+      :step="question.step"
+      :size="question.size"
+      :id="question.inputId"
+      :list="question.dataListId"
+      :placeholder="
+        question.inputType === 'range' || question.isReadOnly
+          ? ''
+          : question.placeHolder
+      "
+      :autocomplete="question.autoComplete"
+      :value="question.value"
+      @change="change"
+      @keyup="keyup"
+      v-bind:aria-required="question.isRequired"
+      v-bind:aria-label="question.locTitle.renderedHtml"
+      :aria-invalid="question.errors.length > 0"
+      :aria-describedby="
+        question.errors.length > 0 ? question.id + '_errors' : null
+      "
+    />
+    <datalist v-if="question.dataListId" :id="question.dataListId">
+      <option v-for="item in question.dataList" :value="item"></option>
+    </datalist>
+  </div>
 </template>
 
 <script lang="ts">

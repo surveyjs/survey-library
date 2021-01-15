0948d5dca7a46e7374c62e7e41705142d207f94e
Andrew Telnov <andrew.telnov@gmail.com>
2021-01-15 16:27:03 +0300

    Do not create content model for modal action before first showing it

--- src/knockout/components/action-bar/action-bar-item-modal.ts
+++ src/knockout/components/action-bar/action-bar-item-modal.ts
@@ -6,8 +6,21 @@ const template = require("./action-bar-item-modal.html");
 
 export class ActionBarItemModalViewModel {
   public koIsVisible: any = ko.observable(false);
+  public koWasShown = ko.observable(false);
+  public koData = ko.observable();
 
   constructor(private _item: IActionBarItem) {}
+
+  public show() {
+    if (!this.koWasShown()) {
+      if (!!this._item.data.onCreated) {
+        this._item.data.onCreated();
+      }
+    }
+    this.koData(this.contentComponentData);
+    this.koWasShown(true);
+    this.koIsVisible(true);
+  }
 
   public get icon() {
     return this._item.icon;

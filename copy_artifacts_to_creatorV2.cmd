345ed3cb4896f1274dc55c316b66529b888ec8ca
mviorno <mviorno@mail.ru>
2021-02-12 14:50:29 +0300

    rename folders

--- copy_artifacts_to_creatorV2.cmd
+++ copy_artifacts_to_creatorV2.cmd
@@ -2,13 +2,13 @@
 set src_ko=.\packages\survey-knockout
 set src_re=.\packages\survey-react
 set tgtv1=..\survey-creator\packages\survey-creator\node_modules\survey-knockout
-set tgtv2=..\survey-creator\packages\survey-creator-v2\node_modules\survey-knockout
-set tgtv3=..\survey-creator\packages\survey-creator-v2-core\node_modules\survey-knockout
-set tgtv4=..\survey-creator\packages\survey-creator-v2-knockout\node_modules\survey-knockout
-set tgtv5=..\survey-creator\packages\survey-creator-v2-react\node_modules\survey-react
-set tgtv6=..\survey-creator\packages\survey-creator-v2-react\node_modules\survey-knockout
+::set tgtv2=..\survey-creator\packages\survey-creator\node_modules\survey-knockout
+set tgtv3=..\survey-creator\packages\survey-creator-core\node_modules\survey-knockout
+set tgtv4=..\survey-creator\packages\survey-creator-knockout\node_modules\survey-knockout
+set tgtv5=..\survey-creator\packages\survey-creator-react\node_modules\survey-react
+set tgtv6=..\survey-creator\packages\survey-creator-react\node_modules\survey-knockout
 if exist %tgtv1% xcopy %src_ko%\*.* %tgtv1% /Y /Q
-if exist %tgtv2% xcopy %src_ko%\*.* %tgtv2% /Y /Q
+::if exist %tgtv2% xcopy %src_ko%\*.* %tgtv2% /Y /Q
 if exist %tgtv3% xcopy %src_ko%\*.* %tgtv3% /Y /Q
 if exist %tgtv4% xcopy %src_ko%\*.* %tgtv4% /Y /Q
 

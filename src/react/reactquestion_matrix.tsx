9f534303f456f41bd4e05a31aee5d67d53d0e377
dmitrykurmanov <kurmanov.work@gmail.com>
2021-02-28 12:25:21 +0300

    implemented allowRowsDragAndDrop for matrixDropDownBase (#2682)

--- src/react/reactquestion_matrix.tsx
+++ src/react/reactquestion_matrix.tsx
@@ -71,7 +71,10 @@ export class SurveyQuestionMatrix extends SurveyQuestionElementBase {
       </thead>
     );
     return (
-      <div className={cssClasses.tableWrapper} ref={(root) => (this.control = root)}>
+      <div
+        className={cssClasses.tableWrapper}
+        ref={(root) => (this.control = root)}
+      >
         <fieldset>
           <legend aria-label={this.question.locTitle.renderedHtml} />
           <table className={cssClasses.root}>
@@ -102,7 +105,7 @@ export class SurveyQuestionMatrixRow extends ReactSurveyElement {
   protected canRender(): boolean {
     return !!this.row;
   }
-  protected renderElement(): JSX.Element {    
+  protected renderElement(): JSX.Element {
     var rowsTD = null;
     if (this.question.hasRows) {
       var rowText = this.renderLocString(this.row.locText);
@@ -119,23 +122,26 @@ export class SurveyQuestionMatrixRow extends ReactSurveyElement {
   }
 
   generateDragDropTD() {
-    if (!this.question.allowRowsDragAndDrop)return null;
+    if (!this.question.allowRowsDragAndDrop) return null;
 
-    return <td className={this.question.cssClasses.cell}>
-      <svg
-        width="10"
-        height="16"
-        viewBox="0 0 10 16"
-        className={
-          this.cssClasses.itemIcon +
-          " " +
-          this.cssClasses.itemIconHoverMod
-        }
-        xmlns="http://www.w3.org/2000/svg"
+    return (
+      <td
+        key={this.question.name + "-td-d&d"}
+        className={this.question.cssClasses.cell}
       >
-        <path d="M6 2C6 0.9 6.9 0 8 0C9.1 0 10 0.9 10 2C10 3.1 9.1 4 8 4C6.9 4 6 3.1 6 2ZM2 0C0.9 0 0 0.9 0 2C0 3.1 0.9 4 2 4C3.1 4 4 3.1 4 2C4 0.9 3.1 0 2 0ZM8 6C6.9 6 6 6.9 6 8C6 9.1 6.9 10 8 10C9.1 10 10 9.1 10 8C10 6.9 9.1 6 8 6ZM2 6C0.9 6 0 6.9 0 8C0 9.1 0.9 10 2 10C3.1 10 4 9.1 4 8C4 6.9 3.1 6 2 6ZM8 12C6.9 12 6 12.9 6 14C6 15.1 6.9 16 8 16C9.1 16 10 15.1 10 14C10 12.9 9.1 12 8 12ZM2 12C0.9 12 0 12.9 0 14C0 15.1 0.9 16 2 16C3.1 16 4 15.1 4 14C4 12.9 3.1 12 2 12Z" />
-      </svg>
-    </td>;
+        <svg
+          width="10"
+          height="16"
+          viewBox="0 0 10 16"
+          className={
+            this.cssClasses.itemIcon + " " + this.cssClasses.itemIconHoverMod
+          }
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M6 2C6 0.9 6.9 0 8 0C9.1 0 10 0.9 10 2C10 3.1 9.1 4 8 4C6.9 4 6 3.1 6 2ZM2 0C0.9 0 0 0.9 0 2C0 3.1 0.9 4 2 4C3.1 4 4 3.1 4 2C4 0.9 3.1 0 2 0ZM8 6C6.9 6 6 6.9 6 8C6 9.1 6.9 10 8 10C9.1 10 10 9.1 10 8C10 6.9 9.1 6 8 6ZM2 6C0.9 6 0 6.9 0 8C0 9.1 0.9 10 2 10C3.1 10 4 9.1 4 8C4 6.9 3.1 6 2 6ZM8 12C6.9 12 6 12.9 6 14C6 15.1 6.9 16 8 16C9.1 16 10 15.1 10 14C10 12.9 9.1 12 8 12ZM2 12C0.9 12 0 12.9 0 14C0 15.1 0.9 16 2 16C3.1 16 4 15.1 4 14C4 12.9 3.1 12 2 12Z" />
+        </svg>
+      </td>
+    );
   }
 
   generateTds() {
--- src/react/reactquestion_matrixdropdownbase.tsx
+++ src/react/reactquestion_matrixdropdownbase.tsx
@@ -56,7 +56,7 @@ export class SurveyQuestionMatrixDropdownBase extends SurveyQuestionElementBase 
       ? ({ overflowX: "scroll" } as React.CSSProperties)
       : ({} as React.CSSProperties);
     return (
-      <div style={divStyle}>
+      <div style={divStyle} ref={(root) => (this.control = root)}>
         <table className={this.question.cssClasses.root}>
           {header}
           {rows}
@@ -131,6 +131,9 @@ export class SurveyQuestionMatrixDropdownBase extends SurveyQuestionElementBase 
   ): JSX.Element {
     var matrixrow = [];
     var cells = row.cells;
+
+    matrixrow.push(this.generateDragDropTD(cssClasses));
+
     for (var i = 0; i < cells.length; i++) {
       matrixrow.push(this.renderCell(cells[i], i, cssClasses));
     }
@@ -141,6 +144,24 @@ export class SurveyQuestionMatrixDropdownBase extends SurveyQuestionElementBase 
       </tr>
     );
   }
+  generateDragDropTD(cssClasses: any) {
+    if (!this.question.allowRowsDragAndDrop) return null;
+
+    return (
+      <td key={this.question.name + "-td-d&d"} className={cssClasses.cell}>
+        <svg
+          width="10"
+          height="16"
+          viewBox="0 0 10 16"
+          className={cssClasses.itemIcon + " " + cssClasses.itemIconHoverMod}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M6 2C6 0.9 6.9 0 8 0C9.1 0 10 0.9 10 2C10 3.1 9.1 4 8 4C6.9 4 6 3.1 6 2ZM2 0C0.9 0 0 0.9 0 2C0 3.1 0.9 4 2 4C3.1 4 4 3.1 4 2C4 0.9 3.1 0 2 0ZM8 6C6.9 6 6 6.9 6 8C6 9.1 6.9 10 8 10C9.1 10 10 9.1 10 8C10 6.9 9.1 6 8 6ZM2 6C0.9 6 0 6.9 0 8C0 9.1 0.9 10 2 10C3.1 10 4 9.1 4 8C4 6.9 3.1 6 2 6ZM8 12C6.9 12 6 12.9 6 14C6 15.1 6.9 16 8 16C9.1 16 10 15.1 10 14C10 12.9 9.1 12 8 12ZM2 12C0.9 12 0 12.9 0 14C0 15.1 0.9 16 2 16C3.1 16 4 15.1 4 14C4 12.9 3.1 12 2 12Z" />
+        </svg>
+      </td>
+    );
+  }
+
   renderCell(
     cell: QuestionMatrixDropdownRenderedCell,
     index: number,

442191436068ed50fdb572f2cf9fad5217d471d3
gologames <gologames@bk.ru>
2021-05-26 14:52:33 +0300

    Renamed sv-logo-image for https://github.com/surveyjs/survey-creator/issues/1168

--- src/knockout/components/logo-image/logo-image.ts
+++ src/knockout/components/logo-image/logo-image.ts
@@ -2,7 +2,7 @@ import * as ko from "knockout";
 const template: string = require("./logo-image.html");
 export let LogoImageViewModel: any;
 
-ko.components.register("logo-image", {
+ko.components.register("sv-logo-image", {
   viewModel: {
     createViewModel: (params: any) => {
       return { survey: params };

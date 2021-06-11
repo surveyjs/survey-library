e807c3370509094cbc7bb31614113dca5e817591
Dmitry Kuzin <dmitry.kuzin@devexrepss.com>
2021-06-11 11:40:33 +0300

    Work for #1203: add unit tests for dropdown and text disabled class

--- tests/entries/test.ts
+++ tests/entries/test.ts
@@ -34,6 +34,7 @@ export * from "../questionImagepicker";
 export * from "../questionBoolean";
 export * from "../question_baseselecttests";
 export * from "../question_ratingtests";
+export * from "../question_texttests";
 export * from "../question_customtests";
 export * from "../question_ranking_tests";
 export * from "../question_matrix_base_tests";

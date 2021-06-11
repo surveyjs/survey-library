2d186fb51beb2f2a98e1064416dde8cad5e05bed
Dmitry Kuzin <dmitry.kuzin@devexrepss.com>
2021-06-11 11:33:45 +0300

    Work for #1203: add unit tests for item hover class in design mode

---
+++ tests/question_ratingtests.ts
@@ -0,0 +1,20 @@
+import { QuestionRatingModel } from "../src/question_rating";
+import { SurveyModel } from "../src/survey";
+
+QUnit.test("check allowhover class in design mode", (assert) => {
+  var json = {
+    questions: [
+      {
+        type: "rating",
+        name: "q1",
+      },
+    ],
+  };
+  const survey = new SurveyModel(json);
+  const q1 = <QuestionRatingModel>survey.getQuestionByName("q1");
+  q1.cssClasses.itemHover = "sv_q_rating_hover";
+  const item = q1.visibleRateValues[0];
+  assert.ok(q1.getItemClass(item).indexOf("sv_q_rating_hover") != -1);
+  survey.setDesignMode(true);
+  assert.ok(q1.getItemClass(item).indexOf("sv_q_rating_hover") == -1);
+});

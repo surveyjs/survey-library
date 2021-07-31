4c5b17729f902d4df787ae84b32ba305e7eb1ad2
dmitrykurmanov <kurmanov.work@gmail.com>
2021-07-31 17:05:47 +0300

    work for https://github.com/surveyjs/survey-library/issues/3083

--- src/question_matrixdynamic.ts
+++ src/question_matrixdynamic.ts
@@ -71,7 +71,7 @@ export class QuestionMatrixDynamicModel extends QuestionMatrixDropdownModelBase
       }
     );
     this.registerFunctionOnPropertyValueChanged("allowRowsDragAndDrop", () => {
-      this.clearRowsAndResetRenFderedTable();
+      this.clearRowsAndResetRenderedTable();
     });
   }
 

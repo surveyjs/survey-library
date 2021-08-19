e56452173d459d8c29ac46ffec5e5437f345e02b
Andrew Telnov <andrew.telnov@gmail.com>
2021-08-19 23:46:35 +0300

    Vue: If a question becomes visible in row with several questions, it may not functional correctly #3202

--- src/vue/row.vue
+++ src/vue/row.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="sv-vue-row-additional-div">
+  <div v-if="row.isNeedRender" class="sv-vue-row-additional-div">
     <div
-      v-if="row.isNeedRender && element.isVisible"
-      v-for="element in row.elements"
-      :key="element.idValue"
+      v-for="element in row.visibleElements"
+      :key="element.id"
       :style="{
         flexBasis: element.renderWidth,
         flexGrow: 1,

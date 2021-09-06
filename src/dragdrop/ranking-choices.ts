067bdef77efe5e221dc7400fa3f4ca898f602a4b
Dmitry Kurmanov <kurmanov.work@gmail.com>
2021-09-06 10:17:51 +0300

    Feature/3217 abandon sortablejs (#3262)
    * https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * abandone SortableJS: clone dragged node as shortcut spike
    
    * work for the #3217
    
    * work for the #3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217
    
    * work for the https://github.com/surveyjs/survey-library/issues/3217

--- src/dragdrop/ranking-choices.ts
+++ src/dragdrop/ranking-choices.ts
@@ -1,9 +1,31 @@
 import { ItemValue } from "../itemvalue";
 import { DragDropChoices } from "./choices";
 export class DragDropRankingChoices extends DragDropChoices {
-  protected getShortcutText(draggedElement: ItemValue): string {
-    const index = this.parentElement.ran;
-    return draggedElement.text;
+  protected createDraggedElementShortcut(
+    text: string,
+    draggedElementNode: HTMLElement
+  ): HTMLElement {
+    const draggedElementShortcut = document.createElement("div");
+    // draggedElementShortcut.innerText = text;
+    draggedElementShortcut.style.cssText = ` 
+          cursor: grabbing;
+          position: absolute;
+          z-index: 1000;
+          border-radius: 36px;
+          min-width: 100px;
+          box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);
+          background-color: white;
+          padding-right: 16px;
+          padding-left: 20px;
+        `;
+
+    const isDeepClone = true;
+    const clone = <HTMLElement>draggedElementNode.cloneNode(isDeepClone);
+    clone.classList.remove("sv-dragdrop-moveup");
+    clone.classList.remove("sv-dragdrop-movedown");
+    draggedElementShortcut.appendChild(clone);
+
+    return draggedElementShortcut;
   }
 
   protected getDropTargetByDataAttributeValue(
@@ -12,27 +34,72 @@ export class DragDropRankingChoices extends DragDropChoices {
     return this.parentElement.rankingChoices[dataAttributeValue];
   }
 
-  protected isDropTargetValid(dropTarget: ItemValue): boolean {
+  protected isDropTargetValid(
+    dropTarget: ItemValue,
+    isBottom: boolean,
+    dropTargetNode?: HTMLElement
+  ): boolean {
     const choices = this.parentElement.visibleChoices;
 
-    // shouldn't allow to drop on "adorners" (selectall, none, other)
-    if (choices.indexOf(dropTarget) === -1) return false;
+    if (dropTarget === this.draggedElement) return false;
+
+    const dropTargetIndex = choices.indexOf(this.dropTarget);
+    const draggedElementIndex = choices.indexOf(this.draggedElement);
+
+    if (
+      draggedElementIndex > dropTargetIndex &&
+      dropTargetNode.classList.contains("sv-dragdrop-movedown")
+    )
+      return false;
+
+    if (
+      draggedElementIndex < dropTargetIndex &&
+      dropTargetNode.classList.contains("sv-dragdrop-moveup")
+    )
+      return false;
+
+    if (choices.indexOf(dropTarget) === -1)
+      // shouldn't allow to drop on "adorners" (selectall, none, other)
+      return false;
 
     return true;
   }
 
-  protected afterDragOver(): void {
+  protected afterDragOver(dropTargetNode: HTMLElement): void {
     const choices = this.parentElement.choices;
     const dropTargetIndex = choices.indexOf(this.dropTarget);
     const draggedElementIndex = choices.indexOf(this.draggedElement);
 
+    dropTargetNode.classList.remove("sv-dragdrop-moveup");
+    dropTargetNode.classList.remove("sv-dragdrop-movedown");
+    this.parentElement.dropTargetNodeMove = null;
+
     choices.splice(draggedElementIndex, 1);
     choices.splice(dropTargetIndex, 0, this.draggedElement);
+
     this.parentElement.setValue();
+    this.updateDraggedElementShortcut(dropTargetIndex + 1);
+
+    if (draggedElementIndex > dropTargetIndex) {
+      // dropTargetNode.classList.add("sv-dragdrop-movedown");
+      this.parentElement.dropTargetNodeMove = "down";
+    } else {
+      // dropTargetNode.classList.add("sv-dragdrop-moveup");
+      this.parentElement.dropTargetNodeMove = "up";
+    }
+  }
+
+  private updateDraggedElementShortcut(newIndex: number) {
+    const newIndexText = newIndex + "";
+    // TODO should avoid direct DOM manipulation, do through the frameworks instead
+    const indexNode: HTMLElement = this.draggedElementShortcut.querySelector(
+      ".sv-ranking-item__index"
+    );
+    indexNode.innerText = newIndexText;
   }
 
   protected ghostPositionChanged(): void {
-    this.parentElement.currentDragTarget = this.draggedElement;
+    this.parentElement.currentDrropTarget = this.draggedElement;
     super.ghostPositionChanged();
   }
 

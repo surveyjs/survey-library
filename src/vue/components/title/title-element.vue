257ffa9fb2fcaa2382f5eb695780e968f436004b
dmitrykurmanov <kurmanov.work@gmail.com>
2021-09-16 22:58:24 +0300

    work for the #3340

--- src/vue/components/title/title-element.vue
+++ src/vue/components/title/title-element.vue
@@ -1,7 +1,11 @@
 <template>
-  <component v-if="element.hasTitle"
+  <component
+    v-if="element.hasTitle"
     v-bind:is="element.titleTagName"
     :class="element.cssTitle"
+    v-bind:aria-label="
+      element.getType() === 'radiogroup' ? '' : element.locTitle.renderedHtml
+    "
     v-bind:id="element.ariaTitleId"
     v-bind:tabindex="element.titleTabIndex"
     v-bind:aria-expanded="element.titleAriaExpanded"
@@ -16,8 +20,16 @@
       }
     "
   >
-  <survey-element-title-content v-if="!element.hasTitleActions" :element="element" :css="css"></survey-element-title-content>
-  <sv-title-actions v-if="element.hasTitleActions" :element="element" :css="css"></sv-title-actions>
+    <survey-element-title-content
+      v-if="!element.hasTitleActions"
+      :element="element"
+      :css="css"
+    ></survey-element-title-content>
+    <sv-title-actions
+      v-if="element.hasTitleActions"
+      :element="element"
+      :css="css"
+    ></sv-title-actions>
   </component>
 </template>
 

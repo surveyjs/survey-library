c3534333ebb3cdcd3608512708855ae972785f87
Andrew <andrew.telnov@gmail.com>
2021-10-22 10:09:12 +0300

    parentQuestion property is null for questions in template panel in dynamic panel fixed #3520 (#3521)

--- src/question_paneldynamic.ts
+++ src/question_paneldynamic.ts
@@ -81,11 +81,10 @@ class QuestionPanelDynamicItemTextProcessor extends QuestionTextProcessor {
       ) == 0
     ) {
       var q = <Question>(<any>this.data);
-      if (!!q && !!q.parentQuestion && !!q.parent) {
-        var data = (<any>q.parent).data;
+      if (!!q && !!q.parentQuestion && !!q.parent && !!(<any>q.parent).data) {
         var processor = new QuestionPanelDynamicItemTextProcessor(
           <IQuestionPanelDynamicData>(<any>q.parentQuestion),
-          <QuestionPanelDynamicItem>data,
+          <QuestionPanelDynamicItem>(<any>q.parent).data,
           QuestionPanelDynamicItem.ItemVariableName
         );
         var text = textValue.name.replace(
@@ -219,13 +218,12 @@ export class QuestionPanelDynamicModel extends Question
     this.template.renderWidth = "100%";
     this.template.selectedElementInDesign = this;
 
-    var self = this;
-    this.template.addElementCallback = function(element) {
-      self.addOnPropertyChangedCallback(element);
-      self.rebuildPanels();
+    this.template.addElementCallback = (element) => {
+      this.addOnPropertyChangedCallback(<SurveyElement><any>element);
+      this.rebuildPanels();
     };
-    this.template.removeElementCallback = function(element) {
-      self.rebuildPanels();
+    this.template.removeElementCallback = () => {
+      this.rebuildPanels();
     };
 
     this.createLocalizableString("confirmDeleteText", this);
@@ -234,8 +232,8 @@ export class QuestionPanelDynamicModel extends Question
     this.createLocalizableString("panelRemoveText", this);
     this.createLocalizableString("panelPrevText", this);
     this.createLocalizableString("panelNextText", this);
-    this.registerFunctionOnPropertyValueChanged("panelsState", function() {
-      self.setPanelsState();
+    this.registerFunctionOnPropertyValueChanged("panelsState", () => {
+      this.setPanelsState();
     });
   }
   public get hasSingleInput(): boolean {
@@ -249,17 +247,19 @@ export class QuestionPanelDynamicModel extends Question
   private assignOnPropertyChangedToTemplate() {
     var elements = this.template.elements;
     for (var i = 0; i < elements.length; i++) {
-      this.addOnPropertyChangedCallback(elements[i]);
+      this.addOnPropertyChangedCallback(<SurveyElement><any>elements[i]);
     }
   }
-  private addOnPropertyChangedCallback(element: IElement) {
-    var self = this;
-    (<Base>(<any>element)).onPropertyChanged.add(function(element, options) {
-      self.onTemplateElementPropertyChanged(element, options);
+  private addOnPropertyChangedCallback(element: SurveyElement) {
+    if(element.isQuestion) {
+      (<Question>element).setParentQuestion(this);
+    }
+    element.onPropertyChanged.add((element, options) => {
+      this.onTemplateElementPropertyChanged(element, options);
     });
     if (element.isPanel) {
-      (<PanelModel>(<any>element)).addElementCallback = function(element) {
-        self.addOnPropertyChangedCallback(element);
+      (<PanelModel>element).addElementCallback = (element) => {
+        this.addOnPropertyChangedCallback(<SurveyElement><any>element);
       };
     }
   }

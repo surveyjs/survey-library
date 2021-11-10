41b888d03cc41616366947891292beeffd1a112b
novikov82 <novikov82@gmail.com>
2021-11-10 13:44:46 +0300

    fix css class builde #3250

--- src/utils/cssClassBuilder.ts
+++ src/utils/cssClassBuilder.ts
@@ -5,7 +5,7 @@ export class CssClassBuilder {
       return this.toString() === "";
     }
     public append(value: string, condition: boolean = true): CssClassBuilder {
-      if (!!value && condition) this.classes.push(value);
+      if (!!value && condition) this.classes.push(value.trim());
       return this;
     }
     public toString(): string {

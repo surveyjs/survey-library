85c9a10ad24b094d4d2dd3f2df45d99bb6abead7
T_S_V <tsv2013@noreply.github.com>
2021-11-25 15:42:30 +0300

    Added stub angular component and build scripts

---
+++ src/angular/survey.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "survey",
+  templateUrl: "./survey.component.html",
+  styleUrls: ["./survey.component.scss"]
+})
+export class AppComponent {
+  title = "SurveyJS";
+}

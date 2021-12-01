6bc8298ffec9d81d70072045f7d5081e9abeb0f9
OlgaLarina <olga.larina@devexpress.com>
2021-12-01 14:52:52 +0300

    work for https://github.com/surveyjs/survey-creator/issues/2277 : add pressed state

--- src/actions/adaptive-container.ts
+++ src/actions/adaptive-container.ts
@@ -76,6 +76,12 @@ export class AdaptiveActionContainer<T extends Action = Action> extends ActionCo
       },
       popupModel: this.dotsItemPopupModel
     });
+    this.dotsItemPopupModel.onHide = () => {
+      this.dotsItem.pressed = false;
+    };
+    this.dotsItemPopupModel.onShow = () => {
+      this.dotsItem.pressed = true;
+    };
   }
   protected onSet() {
     this.actions.forEach(action => action.updateCallback = () => this.raiseUpdate(false));

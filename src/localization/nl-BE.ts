1fd10fae6b63af0890c094f909de5cc280e938d7
Sam Mousa <sam@mousa.nl>
2021-12-02 14:43:56 +0100

    Add Flemish language config

---
+++ src/localization/nl-BE.ts
@@ -0,0 +1,10 @@
+import { surveyLocalization } from "../surveyStrings";
+import {dutchSurveyStrings} from "./dutch";
+
+/**
+ * This is initialized as a copy of the Dutch strings, when they start to deviate a choice has to be made:
+ * - Copy the Dutch set once and move forward as if it are 2 totally different languages
+ * - Override the relevant strings only
+ */
+surveyLocalization.locales["nl-BE"] = dutchSurveyStrings;
+surveyLocalization.localeNames["nl-BE"] = "vlaams";

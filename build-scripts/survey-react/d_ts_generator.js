f69f078911c15a705cd9f448eef71c4dde223bd2
Andrew Telnov <andrew.telnov@gmail.com>
2021-12-29 17:52:12 +0300

    Fix generation d.ts in survey-react

--- build-scripts/survey-react/d_ts_generator.js
+++ build-scripts/survey-react/d_ts_generator.js
@@ -2,7 +2,7 @@ var doc = require("surveyjs-doc-generator");
 
 doc.generateDts({ 
   entries: ["./src/entries/core.ts", "./src/entries/react-ui.ts"],
-  out: "./build/survey-react-ui/survey.react.d.ts",
+  out: "./build/survey-react/survey.react.d.ts",
   name: "Type definition for Survey JavaScript library for React",
   license: "MIT (http://www.opensource.org/licenses/mit-license.php)"
 });

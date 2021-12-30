ae10e75836d49331ab98ca53294bb49316b2c5a3
T_S_V <tsv2013@noreply.github.com>
2021-12-30 14:09:36 +0300

    Renamed example app and removed extra content #3681

--- examples/angular-ui/karma.conf.js
+++ examples/angular-ui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/example-sjs-ng-ui'),
+      dir: require('path').join(__dirname, '../../coverage/angular-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- examples/angular-ui/src/app/app.component.spec.ts
+++ examples/angular-ui/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -10,22 +10,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'example-sjs-ng-ui'`, () => {
+  it("should have as title 'angular-ui'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('example-sjs-ng-ui');
+    expect(app.title).toEqual("angular-ui");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('example-sjs-ng-ui app is running!');
+    expect(compiled.querySelector(".content span")?.textContent).toContain("angular-ui app is running!");
   });
 });

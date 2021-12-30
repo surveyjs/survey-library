7f499f0f1ac4624e77ba0f8a6d01721e226d63c3
T_S_V <tsv2013@noreply.github.com>
2021-12-30 15:28:04 +0300

    Work for #3681 - test Observable and Subject

--- src/angular/survey.component.ts
+++ src/angular/survey.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+// import { SurveyModel } from "survey-core";
 
 @Component({
   selector: "survey",
@@ -7,4 +9,24 @@ import { Component } from "@angular/core";
 })
 export class SurveyComponent {
   title = "SurveyJS";
+  @Input() json: string;
+  // model = new SurveyModel(json);
+  constructor() {
+    debugger;
+  }
+  _visible = new Subject<boolean>();
+  _visibleVal = true;
+  get visible() {
+    return this._visibleVal;
+  }
+  set visible(val: boolean) {
+    this._visible.next(val);
+    this._visibleVal = val;
+  }
+  toggle() {
+    this.visible = !this.visible;
+  }
+  time = new Observable<string>(observer => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
 }

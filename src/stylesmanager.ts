259e5d9a3b009aad4670ae75a42ed1b21990977d
OlgaLarina <olga.larina@devexpress.com>
2022-01-13 15:47:38 +0300

    resolve #2205 (#3865)

--- src/stylesmanager.ts
+++ src/stylesmanager.ts
@@ -404,6 +404,8 @@ export class StylesManager {
     ".sv-list__item--selected .sv-list__item-icon use": "fill: #fff;",
     ".sv-list__item--disabled": "color: rgba(22, 22, 22, 0.16); cursor: default; pointer-events: none;",
     ".sv-list__item span": "white-space: nowrap;",
+    ".sv-list__container": "position: relative;",
+    ".sv-list__filter": "position: sticky; top: 0; padding: 8px 0; border-bottom: 1px solid rgba(0, 0, 0, 0.16); background: #ffffff;",
     ".sv-list__input": "-webkit-appearance: none; -moz-appearance: none; appearance: none; display: block; box-sizing: border-box; width: 100%; height: calc(2em + 1px); padding-left: 1em; outline: none; font-size: 1em; border: 1px solid transparent;",
     //eo list
     ".sv-skeleton-element": "min-height: 50px;",

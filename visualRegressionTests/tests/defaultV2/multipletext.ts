399386461fd34ce89c0e6baf9bd957f685bcebb0
Dmitry Kuzin <dmitry.kuzin@devexpress.com>
2022-01-25 23:05:04 +0300

    Work for #3920: unify templates in for multipletext in react/vue/knockout

--- visualRegressionTests/tests/defaultV2/multipletext.ts
+++ visualRegressionTests/tests/defaultV2/multipletext.ts
@@ -2,7 +2,7 @@ import { Selector, ClientFunction } from "testcafe";
 import { createScreenshotsComparer } from "devextreme-screenshot-comparer";
 import { url, screenshotComparerOptions, frameworks, initSurvey, url_test } from "../../helper";
 
-const title = "Html Screenshot";
+const title = "Multipletext Screenshot";
 
 fixture`${title}`.page`${url}`.beforeEach(async (t) => {
 
@@ -19,22 +19,35 @@ frameworks.forEach(framework => {
     .page`${url_test}${theme}/${framework}.html`.beforeEach(async t => {
     await applyTheme(theme);
   });
-  test("Check html question", async (t) => {
+  test("Check multipletext question", async (t) => {
     await t.resizeWindow(1920, 1080);
     await initSurvey(framework, {
       questions: [
         {
-          type: "html",
-          name: "html_question",
-          html: "<b>Hello, world!</b><p>Hello, world!</p><b>Hello, world!</b>",
-          width: "768px"
+          type: "multipletext",
+          name: "q1",
+          width: "768px",
+          title: "Personal Information",
+          items: [
+            {
+              name: "item1",
+              title: "Full Name"
+            },
+            {
+              name: "item2",
+              title: "Email Address"
+            },
+            {
+              name: "item3",
+              title: "ID"
+            },
+          ]
         },
       ]
     });
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-    const questionRoot = Selector(".sd-question--html");
-    await t.wait(1000);
-    await takeScreenshot("html-question.png", questionRoot, screenshotComparerOptions);
+    const questionRoot = Selector(".sd-question");
+    await takeScreenshot("mutlipletext.png", questionRoot, screenshotComparerOptions);
     await t
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());

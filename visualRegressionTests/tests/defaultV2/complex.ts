1eb0f96b4bd6e51023f4052906e3488dc5a71ce6
Dmitry Kuzin <dmitry.kuzin@devexpress.com>
2022-02-01 01:59:01 +0300

    Add vrt test for complex question

---
+++ visualRegressionTests/tests/defaultV2/complex.ts
@@ -0,0 +1,106 @@
+import { Selector, ClientFunction } from "testcafe";
+import { createScreenshotsComparer } from "devextreme-screenshot-comparer";
+import { url, screenshotComparerOptions, frameworks, initSurvey, url_test } from "../../helper";
+
+const title = "Complex Screenshot";
+
+fixture`${title}`.page`${url}`.beforeEach(async (t) => {
+
+});
+
+const applyTheme = ClientFunction(theme => {
+  (<any>window).Survey.StylesManager.applyTheme(theme);
+});
+
+const theme = "defaultV2";
+
+const json = {
+  showQuestionNumbers: "off",
+  questions: [
+    {
+      type: "Paneldynamic",
+      title: "My Order",
+      name: "order",
+      renderMode: "progressBottom",
+      templateTitle: "{panel.itemName}",
+      panelRemoveText: "Remove Item",
+      panelCount: 5,
+      templateElements: [
+        {
+          type: "text",
+          name: "itemName",
+          title: "Item Name",
+          defaultValue: "Converse Chuck Taylor"
+        },
+        {
+          type: "text",
+          name: "count",
+          title: "Count",
+          defaultValue: "1",
+          startWithNewLine: false,
+        },
+        {
+          type: "panel",
+          name: "shipping",
+          title: "Shipping",
+          elements: [
+            {
+              type: "radiogroup",
+              name: "delivery",
+              title: "Delivery",
+              choices: ["DHL", "Pony Express", "FedEx"],
+              defaultValue: "FedEx"
+            },
+            {
+              type: "boolean",
+              name: "fast",
+              title: "Would you like to get the order as fast as it possible?",
+              defaultValue: true
+            },
+            {
+              type: "matrix",
+              name: "best_delivery",
+              title: "What’s the best delivery time for you?",
+              "columns": ["Morning", "Afternoon", "Evening"],
+              "rows": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+              defaultValue: {
+                "Monday": "Afternoon",
+                "Tuesday": "Afternoon",
+                "Wednesday": "Morning",
+                "Thursday": "Morning",
+                "Friday": "Evening"
+              }
+            }
+          ]
+        },
+        {
+          type: "panel",
+          name: "blilling",
+          title: "Billing",
+          state: "collapsed",
+          elements: [{ "type": "text", name: "dummy" }]
+        }
+      ]
+    }
+  ]
+};
+
+frameworks.forEach(framework => {
+  fixture`${framework} ${title} ${theme}`
+    .page`${url_test}${theme}/${framework}.html`.beforeEach(async t => {
+    await applyTheme(theme);
+  });
+  test("Check complex question", async (t) => {
+    await t.resizeWindow(1920, 1800);
+    await ClientFunction(() => { (window as any).Survey.surveyLocalization.locales.en.panelDynamicProgressText = "{0} of {1}"; })();
+    await initSurvey(framework, json);
+    await ClientFunction(() => { (window as any).survey.getQuestionByName("order").panelCount = 3; })();
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    const questionRoot = Selector(".sd-question");
+    await takeScreenshot("complex-question.png", questionRoot, screenshotComparerOptions);
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
+

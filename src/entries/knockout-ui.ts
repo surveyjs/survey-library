88df94ff1f5b7b97a6213f8d396565bdc4448f84
Andrew Telnov <andrew.telnov@gmail.com>
2022-02-11 17:20:16 +0300

    Remove default exports, #3996

--- src/entries/knockout-ui.ts
+++ src/entries/knockout-ui.ts
@@ -1,4 +1,4 @@
 export * from "./knockout-ui-model";
-export * from "./core-export";
-import { SurveyWindowModel } from "survey-core";
-export class SurveyWindow extends SurveyWindowModel {}
+//export * from "./core-export";
+//import { SurveyWindowModel } from "survey-core";
+//export class SurveyWindow extends SurveyWindowModel {}

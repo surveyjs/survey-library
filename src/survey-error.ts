463f4a97c89be2111d9a954dfdb8ad74eb7bd696
Andrew <andrew.telnov@gmail.com>
2022-02-22 10:59:40 +0300

    Date input field - lost focus on chaning if question has an error fixed #4037 (#4048)

--- src/survey-error.ts
+++ src/survey-error.ts
@@ -8,7 +8,12 @@ export class SurveyError {
     public text: string = null,
     protected errorOwner: ISurveyErrorOwner = null
   ) {}
-  public get locText() {
+  public equalsTo(error: SurveyError): boolean {
+    if(!error || !error.getErrorType) return false;
+    if(this.getErrorType() !== error.getErrorType()) return false;
+    return this.text === error.text && this.visible === error.visible;
+  }
+  public get locText(): LocalizableString {
     if (!this.locTextValue) {
       this.locTextValue = new LocalizableString(this.errorOwner, true);
       this.locTextValue.text = this.getText();

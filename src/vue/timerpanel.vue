df4e192ec4a4df42b4221ce28fddc4ed6e0d9bea
andrewtelnov <andrew.telnov@gmail.com>
2022-02-22 11:57:41 +0300

    Make survey timer model reactive, fixed #4047

--- src/vue/timerpanel.vue
+++ src/vue/timerpanel.vue
@@ -5,14 +5,18 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
-import { SurveyTimerModel } from "survey-core";
+import { Base, SurveyTimerModel } from "survey-core";
+import { BaseVue } from "./base";
 
 @Component
-export class TimerPanel extends Vue {
+export class TimerPanel extends BaseVue {
   @Prop() timerModel: SurveyTimerModel;
   @Prop() css: any;
   get text() {
     return this.timerModel.text;
+  }
+  protected getModel(): Base {
+    return this.timerModel;
   }
 }
 Vue.component("survey-timerpanel", TimerPanel);

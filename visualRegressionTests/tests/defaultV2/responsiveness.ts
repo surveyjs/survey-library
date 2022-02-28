2310567a6223a01c1cd2379b21fec7b03a9b4e2c
OlgaLarina <olga.larina.developer@gmail.com>
2022-02-28 10:30:34 +0300

    Implement responsive styles for ranking question in defaultV2 theme (#4071)
    * resolve #3970 Implement responsive styles for ranking question in defaultV2 theme
    
    * work for #3970 : update etalons
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- visualRegressionTests/tests/defaultV2/responsiveness.ts
+++ visualRegressionTests/tests/defaultV2/responsiveness.ts
@@ -247,4 +247,23 @@ frameworks.forEach(framework => {
     await ClientFunction(() => { document.body.focus(); })();
     await checkElementScreenshot("responsiveness-checkbox-col-count-2.png", Selector(".sd-question"), t);
   });
+
+  test("Check ranking question on small screen", async (t) => {
+    await t.resizeWindow(600, 1080);
+    await initSurvey(framework, {
+      showQuestionNumbers: "off",
+      questions: [
+        {
+          type: "ranking",
+          title: "Tell me about a time you strongly disagreed with your manager. What did you do to convince him or her that you were right? What happened?",
+          name: "ranking_question",
+          choices: ["item1", "item2", "item3", "item4"]
+        }
+      ]
+    });
+    await checkElementScreenshot("responsiveness-ranking.png", Selector(".sd-question"), t);
+
+    await t.hover(".sv-ranking-item");
+    await checkElementScreenshot("responsiveness-ranking-hover-item.png", Selector(".sd-question"), t);
+  });
 });

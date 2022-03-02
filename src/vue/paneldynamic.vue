7ea4bb835ee52a5a88130c7e6131a8240ce76eef
Dmitry Kuzin <dmitry.kuzin@devexpress.com>
2022-03-02 20:29:27 +0300

    Work for #4066: fix vue

--- src/vue/paneldynamic.vue
+++ src/vue/paneldynamic.vue
@@ -4,7 +4,7 @@
       <span>
         <survey-string :locString="question.locNoEntriesText"></survey-string>
       </span>
-      <sv-paneldynamic-add-btn :question="question" />
+      <sv-paneldynamic-add-btn :data="{ question }" />
     </div>
     <survey-paneldynamicprogress
       v-if="question.isProgressTopShowing"
@@ -21,7 +21,7 @@
       v-if="question.isProgressBottomShowing"
       :question="question"
     />
-    <sv-paneldynamic-add-btn v-if="question.isRenderModeList" :question="question" />
+    <sv-paneldynamic-add-btn v-if="question.isRenderModeList" :data="{ question }" />
     <survey-paneldynamicprogress-v2 v-if="question.panelCount !== 0"
       :question="question"
     />

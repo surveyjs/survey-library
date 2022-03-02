ad5453c18eaceba21f2ccc57e2cea4232f26bbd4
Mārtiņš Mednis <mrt@mednis.info>
2022-03-02 14:45:02 +0200

    Fixed a typo in Russian localization (#4098)

--- src/localization/russian.ts
+++ src/localization/russian.ts
@@ -12,7 +12,7 @@ export var russianSurveyStrings = {
   selectAllItemText: "Выбрать всё",
   progressText: "Страница {0} из {1}",
   panelDynamicProgressText: "Запись {0} из {1}",
-  questionsProgressText: "Ответы на {0}/{1}вопросы",
+  questionsProgressText: "Ответы на {0}/{1} вопросы",
   emptySurvey: "Нет ни одного вопроса.",
   completingSurvey: "Благодарим Вас за заполнение анкеты!",
   completingSurveyBefore:

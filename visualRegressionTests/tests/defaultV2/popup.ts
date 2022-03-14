cf2fe344f7e3533de2d89db8d1a8e1548b6cffa0
OlgaLarina <olga.larina.dev@gmail.com>
2022-03-14 12:34:39 +0300

    resolve https://github.com/surveyjs/survey-creator/issues/2727

---
+++ visualRegressionTests/tests/defaultV2/popup.ts
@@ -0,0 +1,65 @@
+import { Selector, ClientFunction } from "testcafe";
+import { frameworks, initSurvey, url, url_test, checkElementScreenshot } from "../../helper";
+
+const title = "Popup Screenshot";
+
+fixture`${title}`.page`${url}`;
+
+const applyTheme = ClientFunction(theme => {
+  (<any>window).Survey.StylesManager.applyTheme(theme);
+});
+
+const json = {
+  showQuestionNumbers: "off",
+  questions: [
+    {
+      name: "often",
+      type: "radiogroup",
+      title: "How often do you use this applications?",
+      choices: ["Rare", "Sometimes", "Always"]
+    }
+  ]
+};
+
+const theme = "defaultV2";
+
+const popupSelector = Selector(".sv-popup");
+const clickButton = Selector(".sv-action");
+
+function addDropdownTitleAction(_, opt) {
+  let items = [];
+  for (let index = 0; index < 10; index++) {
+    items[index] = new window["Survey"].Action({ title: "item" + index });
+  }
+  const itemPopupModel = new window["Survey"].PopupModel("sv-list", {
+    model: new window["Survey"].ListModel(items)
+  }, "bottom", "left", true);
+
+  const item = new window["Survey"].Action({
+    component: "sv-action-bar-item-dropdown",
+    title: "Click",
+    showTitle: true,
+    action: () => {
+      itemPopupModel.toggleVisibility();
+    },
+    popupModel: itemPopupModel
+  });
+  opt.titleActions = [item];
+}
+
+frameworks.forEach(framework => {
+  fixture`${framework} ${title} ${theme}`
+    .page`${url_test}${theme}/${framework}.html`
+    .beforeEach(async t => {
+      await applyTheme(theme);
+      await initSurvey(framework, json);
+    });
+
+  test("Popup styles", async t => {
+    await initSurvey(framework, json, { onGetQuestionTitleActions: addDropdownTitleAction });
+    await t
+      .resizeWindow(1000, 1000)
+      .click(clickButton);
+    await checkElementScreenshot("popup.png", popupSelector, t);
+  });
+});

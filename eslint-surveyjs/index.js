a0d0e5e201068323f503def14c1a4314589571cf
OlgaLarina <olga.larina.dev@gmail.com>
2022-04-01 11:54:13 +0400

    add new eslint rule: "no-test-debug"
    (cherry picked from commit 4fd8f4bb9643e3e8083b2ed908412189d95db812)
    (cherry picked from commit 2e3b357266b06431ecb1ef40b8b522720ca49e2e)

--- eslint-surveyjs/index.js
+++ eslint-surveyjs/index.js
@@ -1,13 +1,27 @@
+const testOnlyMessage = " :( please don't forget to remove 'test.only' testcafe statement it will disable all other tests :( ";
+const debugMessage = " :( please don't forget to remove 'debug()'  :( ";
+
 module.exports = {
   rules: {
     "no-test-only": context =>
     ({
       MemberExpression: function (node) {
         if (node.object.name === "test" && node.property.name === "only") {
-          context.report(node, " :( please don't forget to remove 'test.only' testcafe statement it will disable all other tests :( ");
+          context.report(node, testOnlyMessage);
+        } else if (node.object.type === "CallExpression" && node.property.name === "only" ){
+          context.report(node, testOnlyMessage);
+        }
+      }
+    }),
+    "no-test-debug": context =>
+    ({
+      MemberExpression: function (node) {
+        if (node.object.name === "t" && node.property.name === "debug") {
+          context.report(node, debugMessage);
+        } else if (node.object.type === "CallExpression" && node.property.name === "debug" ){
+          context.report(node, debugMessage);
         }
       }
     }),
   }
 };
-

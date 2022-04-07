b6046c85bfbaafd57f7548ad82dd791101f674b0
OlgaLarina <olga.larina.dev@gmail.com>
2022-04-07 12:51:33 +0400

    fix test

--- visualRegressionTests/tests/defaultV2/matrixdynamic.ts
+++ visualRegressionTests/tests/defaultV2/matrixdynamic.ts
@@ -158,17 +158,11 @@ frameworks.forEach(framework => {
     await t.hover(".sd-table__question-wrapper", { offsetX: 50, offsetY: 20 });
     await checkElementScreenshot("matrixdynamic-errors-in-cell.png", matrixdynamicRoot, t);
   });
-});
-
-["knockout", "react"].forEach(framework => {
-  fixture`${framework} ${title} ${theme}`
-    .page`${url_test}${theme}/${framework}.html`
-    .beforeEach(async t => {
-      await explicitErrorHandler();
-      await applyTheme(theme);
-    });
 
   test("Check Matrixdynamic with allowRowsDragAndDrop", async (t) => {
+    if (framework === "vue") {
+      return;
+    }
     await t.resizeWindow(1280, 1100);
     await initSurvey(framework, { "pages": [{ "name": "page1", "elements": [{ "type": "matrixdynamic", "name": "relatives", "title": "Please enter all blood relatives you know", "columns": [{ "name": "relativeType", "title": "Relative", "cellType": "dropdown", "isRequired": true, "choices": ["father", "mother", "brother", "sister", "son", "daughter"] }, { "name": "firstName", "title": "First name", "cellType": "text", "isRequired": true }, { "name": "lastName", "title": "Last name", "cellType": "text", "isRequired": true }], "detailElements": [{ "type": "radiogroup", "name": "isalive", "startWithNewLine": false, "title": "Alive?", "isRequired": true, "choices": ["Yes", "No"], "colCount": 0 }, { "type": "dropdown", "name": "liveage", "visibleIf": "{row.isalive} = 'Yes'", "startWithNewLine": false, "title": "Age", "isRequired": true, "choicesMin": 1, "choicesMax": 115 }, { "type": "dropdown", "name": "deceasedage", "visibleIf": "{row.isalive} = 'No'", "startWithNewLine": false, "title": "Deceased Age", "isRequired": true, "choices": [{ "value": -1, "text": "Unknown" }], "choicesMin": 1, "choicesMax": 115 }, { "type": "radiogroup", "name": "causeofdeathknown", "visibleIf": "{row.isalive} = 'No'", "startWithNewLine": false, "title": "Cause of Death Known?", "isRequired": true, "choices": ["Yes", "No"], "colCount": 0 }, { "type": "text", "name": "causeofdeath", "visibleIf": "{row.isalive} = 'No' and {row.causeofdeathknown} = 'Yes'", "startWithNewLine": false, "title": "Cause of Death", "isRequired": true }], "detailPanelMode": "underRow", "addRowText": "Add a blood relative", "removeRowText": "Remove the relative", "allowRowsDragAndDrop": true }] }], "checkErrorsMode": "onValueChanged" });
 

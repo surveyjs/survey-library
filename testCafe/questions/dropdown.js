6d3526c4f4d5be695a9b2296178e5fe46470f5e0
OlgaLarina <olga.larina.dev@gmail.com>
2022-05-25 19:02:39 +0300

    refactoring tests & add f-test

--- testCafe/questions/dropdown.js
+++ testCafe/questions/dropdown.js
@@ -158,13 +158,11 @@ frameworks.forEach((framework) => {
 });
 
 frameworks.forEach((framework) => {
-  fixture`${framework} ${title}`.page`${url}${framework}.html`.beforeEach(
-    async (t) => {
-      await initSurvey(framework, json, undefined, true);
-    }
-  );
+  fixture`${framework} ${title}`.page`${url}${framework}.html`;
 
   test("click on question title state editable", async (t) => {
+    await initSurvey(framework, json, undefined, true);
+
     const newTitle = "MyText";
     let json = JSON.parse(await getQuestionJson());
     let questionValue = await getQuestionValue();
@@ -183,74 +181,69 @@ frameworks.forEach((framework) => {
     json = JSON.parse(await getQuestionJson());
     await t.expect(json.title).eql(newTitle);
   });
-});
 
-frameworks.forEach((framework) => {
-  fixture`${framework} ${title}`.page`${url}${framework}.html`.beforeEach(
-    async (t) => {
-      const jsonWithDropDown = {
-        questions: [
-          {
-            type: "dropdown",
-            name: "cars",
-            title: "Dropdown",
-            isRequired: true,
-            hasNone: true,
-            colCount: 4,
-            choices: [
-              "Ford",
-              "Vauxhall",
-              "Volkswagen",
-              "Nissan",
-              "Audi",
-              "Mercedes-Benz",
-              "BMW",
-              "Peugeot",
-              "Toyota",
-              "Citroen"
-            ]
-          },
-          {
-            type: "dropdown",
-            renderAs: "select",
-            name: "DropdownRenderAsSelect",
-            hasOther: "true",
-            choices: [
-              "item1",
-              "item2",
-              "item3",
-              "item4",
-              "item5",
-              "item6",
-              "item7",
-              "item8",
-              "item9",
-              "item10",
-              "item11",
-              "item12",
-              "item13",
-              "item14",
-              "item15",
-              "item16",
-              "item17",
-              "item18",
-              "item19",
-              "item20",
-              "item21",
-              "item22",
-              "item23",
-              "item24",
-              "item25",
-              "item26",
-              "item27"
-            ]
-          }
-        ]
-      };
-      await initSurvey(framework, jsonWithDropDown);
-    }
-  );
   test("Check dropdown disabled items", async (t) => {
+    const jsonWithDropDown = {
+      questions: [
+        {
+          type: "dropdown",
+          name: "cars",
+          title: "Dropdown",
+          isRequired: true,
+          hasNone: true,
+          colCount: 4,
+          choices: [
+            "Ford",
+            "Vauxhall",
+            "Volkswagen",
+            "Nissan",
+            "Audi",
+            "Mercedes-Benz",
+            "BMW",
+            "Peugeot",
+            "Toyota",
+            "Citroen"
+          ]
+        },
+        {
+          type: "dropdown",
+          renderAs: "select",
+          name: "DropdownRenderAsSelect",
+          hasOther: "true",
+          choices: [
+            "item1",
+            "item2",
+            "item3",
+            "item4",
+            "item5",
+            "item6",
+            "item7",
+            "item8",
+            "item9",
+            "item10",
+            "item11",
+            "item12",
+            "item13",
+            "item14",
+            "item15",
+            "item16",
+            "item17",
+            "item18",
+            "item19",
+            "item20",
+            "item21",
+            "item22",
+            "item23",
+            "item24",
+            "item25",
+            "item26",
+            "item27"
+          ]
+        }
+      ]
+    };
+    await initSurvey(framework, jsonWithDropDown);
+
     await ClientFunction(() => {
       const updateChoiceEnabled = (_, opt) => {
         opt.choices.forEach((ch, index) => { ch.setIsEnabled(index % 2 === 0); });
@@ -282,5 +275,46 @@ frameworks.forEach((framework) => {
       .expect(popupContainer.visible).ok()
       .click(Selector(".sv-list__item span").withText("item3").filterVisible())
       .expect(popupContainer.visible).notOk();
+  });
+
+  test("Check dropdown clear button", async (t) => {
+    const jsonWithDropDown = {
+      questions: [
+        {
+          type: "dropdown",
+          name: "cars",
+          title: "Dropdown",
+          renderAs: "select",
+          choices: [
+            "Ford",
+            "Vauxhall",
+            "Volkswagen",
+            "Nissan",
+            "Audi",
+            "Mercedes-Benz",
+            "BMW",
+            "Peugeot",
+            "Toyota",
+            "Citroen"
+          ]
+        }
+      ]
+    };
+    await initSurvey(framework, jsonWithDropDown);
+
+    const questionDropdownSelect = Selector(".sv_q_dropdown_control");
+    const clearButton = Selector(".sv_q_dropdown_clean-button");
+    await t
+      .expect(clearButton.exists).ok()
+      .expect(clearButton.visible).notOk()
+
+      .click(questionDropdownSelect)
+      .click(Selector(".sv-list__item span").withText("Ford").filterVisible())
+
+      .expect(clearButton.visible).ok()
+
+      .click(clearButton)
+      .expect(clearButton.exists).ok()
+      .expect(clearButton.visible).notOk();
   });
 });

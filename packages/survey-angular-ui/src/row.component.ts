47313283bbdfa4cd94b78576e040c1e50b1623c0
Dmitry Kuzin <dk981234@gmail.com>
2022-06-01 04:38:33 -0700

    Work for #3681: remove additional element when adding row component (breaks styles)

--- packages/survey-angular-ui/src/row.component.ts
+++ packages/survey-angular-ui/src/row.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from "@angular/core";
 import { PageModel, QuestionRowModel } from "survey-core";
 import { BaseAngular } from "./base-angular";
 @Component({
-  selector: "sv-ng-row",
+  selector: "sv-ng-row, '[sv-ng-row]'",
   templateUrl: "./row.component.html",
 })
 export class RowComponent extends BaseAngular {

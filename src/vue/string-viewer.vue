c58183b4c9e686bfd006b6a5c7deac6e1e396bcc
OlgaLarina <olga.larina.dev@gmail.com>
2022-06-09 12:13:55 +0300

    resolve #4412 radiogroup, checkbox example: others text setting is not reactive in Vue

--- src/vue/string-viewer.vue
+++ src/vue/string-viewer.vue
@@ -15,6 +15,16 @@ import { LocalizableString } from "survey-core";
 @Component
 export class SurveyStringViewer extends Vue {
   @Prop() locString: LocalizableString;
+
+  constructor() {
+    super();
+    if (!this.locString) return;
+    const self = this;
+    this.locString.onChanged = () => {
+      self.$forceUpdate();
+    };
+    this.locString.onChanged();
+  }
 }
 Vue.component(LocalizableString.defaultRenderer, SurveyStringViewer);
 export default SurveyStringViewer;

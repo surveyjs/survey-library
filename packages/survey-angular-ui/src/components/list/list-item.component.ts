154c280b539a4c75c14741c0b1aba27be5b6faec
OlgaLarina <olga.larina.dev@gmail.com>
2022-06-24 11:35:08 +0300

    workfor #3681: fix list item accessibility

--- packages/survey-angular-ui/src/components/list/list-item.component.ts
+++ packages/survey-angular-ui/src/components/list/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ViewContainerRef } from "@angular/core";
+import { Component, HostBinding, HostListener, Input, ViewChild, ViewContainerRef } from "@angular/core";
 import { ListModel, Action } from "survey-core";
 import { BaseAngular } from "../../base-angular";
 import { AngularComponentFactory } from "../../component-factory";
@@ -9,19 +9,32 @@ import { AngularComponentFactory } from "../../component-factory";
 })
 
 export class ListItemComponent extends BaseAngular {
+  @Input() element: any;
   @Input() item!: Action;
   @Input() model!: ListModel;
+
   @ViewChild("itemComponent", { read: ViewContainerRef, static: true }) itemComponent!: ViewContainerRef;
+
+  @HostBinding("attr.aria-selected") get ariaSelected(): boolean | string {
+    return this.model.isItemSelected(this.item) || "";
+  }
+  @HostBinding("class") get class(): string {
+    return this.model.getItemClass(this.item);
+  }
+  @HostBinding("style.paddingLeft") get paddingLeft(): string {
+    return this.model.getItemIndent(this.item);
+  }
+  @HostListener("click") click(): void {
+    this.model.selectItem(this.item);
+  }
+  @HostListener("pointerdown") pointerdown(event: PointerEvent): void {
+    this.model.onPointerDown(event, this.item);
+  }
 
   getModel() {
     return this.item;
   }
-  itemClick() {
-    this.model.selectItem(this.item);
-  }
-  pointerdown(event: Event) {
-    this.model.onPointerDown(event, this.item);
-  }
+
   ngOnInit(): void {
     if (!this.item.component) return;
 

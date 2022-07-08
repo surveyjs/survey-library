a7a3465cff0a41960dc7d138bd501594cb0ef4c0
Dmitry Kuzin <dk981234@gmail.com>
2022-07-08 16:17:54 +0400

    Work for #3681: implement paneldynamic

--- packages/survey-angular-ui/src/questions/paneldynamic.component.ts
+++ packages/survey-angular-ui/src/questions/paneldynamic.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
 import { QuestionAngular } from "../question";
 import { PanelModel, QuestionPanelDynamicModel } from "survey-core";
 import { AngularComponentFactory } from "../component-factory";
@@ -7,7 +7,7 @@ import { AngularComponentFactory } from "../component-factory";
   selector: "sv-ng-paneldynamic-question",
   templateUrl: "./paneldynamic.component.html"
 })
-export class PanelDynamicQuestionComponent extends QuestionAngular<QuestionPanelDynamicModel | any> {
+export class PanelDynamicQuestionComponent extends QuestionAngular<QuestionPanelDynamicModel | any> implements OnInit {
   get renderedPanels(): PanelModel[] {
     if (this.model.isRenderModeList) return this.model.panels;
     const panels = [];
@@ -16,6 +16,22 @@ export class PanelDynamicQuestionComponent extends QuestionAngular<QuestionPanel
     }
     return panels;
   }
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.model.panelCountChangedCallback = () => {
+      this.changeDetectorRef.detectChanges();
+    };
+    this.model.currentIndexChangedCallback = () => {
+      this.changeDetectorRef.detectChanges();
+    };
+    this.model.renderModeChangedCallback = () => {
+      this.changeDetectorRef.detectChanges();
+    };
+  }
+
   get progressCssClass() {
     return this.model.isProgressTopShowing
       ? this.model.cssClasses.progressTop

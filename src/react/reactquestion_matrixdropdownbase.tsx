813907a9cea704284d81219d0f43df993cbc0cbd
novikov82 <novikov82@gmail.com>
2022-07-08 12:31:39 +0300

    try to fix Creator master with removing space

--- src/react/reactquestion_matrixdropdownbase.tsx
+++ src/react/reactquestion_matrixdropdownbase.tsx
@@ -271,7 +271,6 @@ class SurveyQuestionMatrixHeaderRequired extends ReactSurveyElement {
     if(!this.column.isRenderedRequired) return null;
     return (
       <>
-        <span>&nbsp;</span>
         <span className={this.question.cssClasses.cellRequiredText}>{this.column.requiredText}</span>
       </>
     );

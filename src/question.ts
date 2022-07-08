007d0aadb031080f606278549bef8b337be0e384
Andrew Telnov <andrew.telnov@gmail.com>
2022-07-08 12:36:15 +0300

    Keep question and column title if was set directly and equals to name fixed #4566

--- src/question.ts
+++ src/question.ts
@@ -95,6 +95,7 @@ export class Question extends SurveyElement
     this.locTitle.onGetDefaultTextCallback = (): string => {
       return this.name;
     };
+    this.locTitle.storeDefaultText = true;
     this.createLocalizableString("requiredErrorText", this);
     this.registerFunctionOnPropertyValueChanged("width", () => {
       this.updateQuestionCss();

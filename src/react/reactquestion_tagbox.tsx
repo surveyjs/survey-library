6c111750c2c09877dad4bf12e72c1aaf390933fc
OlgaLarina <olga.larina.dev@gmail.com>
2022-07-21 17:39:02 +0300

    work for #4400 : add react view tagbox question

---
+++ src/react/reactquestion_tagbox.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { Question } from "survey-core";
+import { ReactQuestionFactory } from "./reactquestion_factory";
+import { SurveyQuestionDropdownBase } from "./dropdown-base";
+import { SurveyQuestionCommentItem } from "./reactquestion_comment";
+
+export class SurveyQuestionTagbox extends SurveyQuestionDropdownBase<Question> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  protected renderElement(): JSX.Element {
+    const cssClasses = this.question.cssClasses;
+    const comment = this.question.isOtherSelected ? this.renderOther(cssClasses) : null;
+    const select = this.renderSelect(cssClasses);
+    return (
+      <div className={this.question.renderCssRoot}>
+        {select}
+        {comment}
+      </div>
+    );
+  }
+  protected renderOther(cssClasses: any): JSX.Element {
+    return (
+      <div className="form-group">
+        <SurveyQuestionCommentItem
+          question={this.question}
+          otherCss={cssClasses.other}
+          cssClasses={cssClasses}
+          isDisplayMode={this.isDisplayMode}
+        />
+      </div>
+    );
+  }
+}
+
+ReactQuestionFactory.Instance.registerQuestion("tagbox", (props) => {
+  return React.createElement(SurveyQuestionTagbox, props);
+});

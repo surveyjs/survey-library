5806b4a99fbf7f05f7173af49a158d919d46771e
OlgaLarina <olga.larina.dev@gmail.com>
2022-07-21 17:56:56 +0300

    work for #4400 : add markup tests

---
+++ tests/markup/etalon_tagbox.ts
@@ -0,0 +1,62 @@
+import { registerMarkupTests } from "./helper";
+
+registerMarkupTests(
+  [
+    {
+      name: "Test Tagbox question markup",
+      json: {
+        questions: [
+          {
+            "type": "tagbox",
+            "name": "name",
+            "title": "Question title",
+            "choices": [
+              "item1",
+              "item2",
+              "item3"
+            ],
+            titleLocation: "hidden"
+          }
+        ]
+      },
+      snapshot: "tagbox"
+    }, {
+      name: "Test Tagbox question markup Read only",
+      json: {
+        mode: "display",
+        questions: [
+          {
+            "type": "tagbox",
+            "name": "name",
+            "title": "Question title",
+            "choices": [
+              "item1",
+              "item2",
+              "item3"
+            ],
+            titleLocation: "hidden"
+          }
+        ]
+      },
+      snapshot: "tagbox-readonly"
+    }, {
+      name: "Test Tagbox question without clear button markup",
+      json: {
+        questions: [
+          {
+            "type": "tagbox",
+            "name": "name",
+            "title": "Question title",
+            "allowClear": false,
+            "choices": [
+              "item1",
+              "item2",
+              "item3"
+            ],
+            titleLocation: "hidden"
+          }
+        ]
+      },
+      snapshot: "tagbox-without-clear-button",
+    },
+  ]);

3326cdd710df03928f026b83fb2c15be26986fca
OlgaLarina <olga.larina.dev@gmail.com>
2022-08-15 12:15:16 +0300

    fixed setting of the needFilter property in ListModel

--- tests/dropdown_list_model_test.ts
+++ tests/dropdown_list_model_test.ts
@@ -107,6 +107,56 @@ QUnit.test("DropdownListModel with ListModel", (assert) => {
   assert.equal(list.actions[3].active, false);
 });
 
+QUnit.test("DropdownListModel with ListModel & searchEnabled false", (assert) => {
+  const survey = new SurveyModel({
+    questions: [{
+      type: "dropdown",
+      name: "question1",
+      searchEnabled: "false",
+      hasOther: "true",
+      choices: [
+        "item1",
+        "item2",
+        "item3",
+        "item4",
+        "item5",
+        "item6",
+        "item7",
+        "item8",
+        "item9",
+        "item10",
+        "item11",
+        "item12",
+        "item13",
+        "item14",
+        "item15",
+        "item16",
+        "item17",
+        "item18",
+        "item19",
+        "item20",
+        "item21",
+        "item22",
+        "item23",
+        "item24",
+        "item25",
+        "item26",
+        "item27"
+      ]
+    }]
+  });
+  const question = <QuestionDropdownModel>survey.getAllQuestions()[0];
+  const dropdownListModel = question.dropdownListModel;
+  assert.ok(dropdownListModel.popupModel.contentComponentData.model instanceof ListModel);
+
+  const list: ListModel = dropdownListModel.popupModel.contentComponentData.model as ListModel;
+  assert.equal(list.actions.length, 28);
+  assert.equal(list.needFilter, false);
+
+  question.searchEnabled = true;
+  assert.equal(list.needFilter, true);
+});
+
 QUnit.test("DropdownListModel with MultiListModel", (assert) => {
   const survey = new SurveyModel(jsonTagbox);
   const question = <QuestionTagboxModel>survey.getAllQuestions()[0];

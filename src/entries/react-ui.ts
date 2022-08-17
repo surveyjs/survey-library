60154e61d92995113012198dcebd2bbd45e8706e
Andrew Telnov <andrew.telnov@gmail.com>
2022-08-17 16:42:49 +0300

    Show console warning if SurveyJS Libraries has different versions core #4724

--- src/entries/react-ui.ts
+++ src/entries/react-ui.ts
@@ -1,2 +1,6 @@
 export * from "./react-ui-model";
 export * from "./core-export";
+
+import { checkLibraryVersion } from "survey-core";
+
+checkLibraryVersion(`${process.env.VERSION}`, "survey-knockout-ui");

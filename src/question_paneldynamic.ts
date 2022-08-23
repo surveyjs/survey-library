69b75c3e641a6e557133bec7af73fd54f3f78102
Dmitry Kuzin <dk981234@gmail.com>
2022-08-23 16:10:41 +0400

    Revert "Merge pull request #4745 from surveyjs/bug/4696-min-width"
    This reverts commit 04bba077ac0c3b09057cb3682e1494f6e4ebfedb, reversing
    changes made to 913ce7a213899d9b84db3938415adf2ee029dd27.

--- src/question_paneldynamic.ts
+++ src/question_paneldynamic.ts
@@ -1813,7 +1813,6 @@ Serializer.addClass(
       name: "templateDescription:text",
       serializationProperty: "locTemplateDescription",
     },
-    { name: "minWidth", default: "min-content" },
     { name: "noEntriesText:text", serializationProperty: "locNoEntriesText" },
     { name: "allowAddPanel:boolean", default: true },
     { name: "allowRemovePanel:boolean", default: true },

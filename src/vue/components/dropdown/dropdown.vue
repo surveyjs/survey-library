ac7d1a25b649d5369bac9c81bf191a2aa0961e88
OlgaLarina <olga.larina.dev@gmail.com>
2022-08-29 13:04:41 +0300

    work for #4758

--- src/vue/components/dropdown/dropdown.vue
+++ src/vue/components/dropdown/dropdown.vue
@@ -27,7 +27,7 @@
         <sv-svg-icon
           :class="question.cssClasses.cleanButtonSvg"
           :iconName="question.cssClasses.cleanButtonIconId"
-          title="question.cleanButtonCaption"
+          :title="question.cleanButtonCaption"
           size="auto"
         >
         </sv-svg-icon>

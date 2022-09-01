1595d05716578c25791a8cc4ed54c64ac8205503
Dmitry Kuzin <dk981234@gmail.com>
2022-09-01 15:57:32 +0400

    Work for #4779: add question base class in exports

--- packages/survey-angular-ui/src/angular-ui.ts
+++ packages/survey-angular-ui/src/angular-ui.ts
@@ -24,6 +24,7 @@ export * from "./questions/rating.component";
 export * from "./questions/boolean.component";
 export * from "./questions/imagepicker-item.component";
 export * from "./questions/imagepicker.component";
+export * from "./question";
 
 export * from "./components/action-bar/action-bar.component";
 export * from "./components/action-bar/action.component";

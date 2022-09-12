0f4950ad757e77ce71e9c745419c6b66fee0faca
OlgaLarina <olga.larina.dev@gmail.com>
2022-09-12 15:47:29 +0300

    work for #4813 : fix for angular

--- packages/survey-angular-ui/src/components/dropdown/dropdown.component.ts
+++ packages/survey-angular-ui/src/components/dropdown/dropdown.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
-import { DropdownListModel } from "survey-core";
+import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from "@angular/core";
+import { DropdownListModel, Helpers } from "survey-core";
 
 @Component({
   selector: "sv-ng-dropdown, '[sv-ng-dropdown]'",
   templateUrl: "./dropdown.component.html"
 })
-export class DropdownComponent implements OnDestroy, OnInit {
+export class DropdownComponent implements OnDestroy, OnInit, OnChanges {
     @Input() model: any;
+    @ViewChild("inputElement") inputElementRef!: ElementRef<HTMLDivElement>;
+
     private dropdownListModel!: DropdownListModel;
 
     get dropdownModel(): DropdownListModel {
@@ -21,6 +23,10 @@ export class DropdownComponent implements OnDestroy, OnInit {
       this.dropdownListModel?.dispose();
     }
 
+    ngOnChanges(changes: SimpleChanges): void {
+      this.updateInputDomElement();
+    }
+
     click(event: any) {
       this.dropdownListModel?.onClick(event);
     }
@@ -32,5 +38,22 @@ export class DropdownComponent implements OnDestroy, OnInit {
     }
     blur(event: any) {
       this.dropdownListModel?.onBlur(event);
+      this.updateInputDomElement();
+    }
+    inputChange(event: any) {
+      this.dropdownModel.filterString = event.target.value;
+    }
+    inputKeyUp(event: any) {
+      this.dropdownModel.filterString = event.target.value;
+      this.dropdownModel.inputKeyUpHandler(event);
+    }
+    updateInputDomElement() {
+      if (!!this.inputElementRef?.nativeElement) {
+        const control: any = this.inputElementRef.nativeElement;
+        const newValue = this.model.filterString;
+        if (!Helpers.isTwoValueEquals(newValue, control.value)) {
+          control.value = this.model.filterString || "";
+        }
+      }
     }
 }

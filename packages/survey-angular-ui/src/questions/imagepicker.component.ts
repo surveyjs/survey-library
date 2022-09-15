f834057adebf2f45214e4c34aec892e27b599a4d
T_S_V <tsv2013@noreply.github.com>
2022-09-15 14:49:06 +0300

    Work for https://github.com/surveyjs/survey-creator/issues/3390 - introduce wrapper infrastructure for image item values

--- packages/survey-angular-ui/src/questions/imagepicker.component.ts
+++ packages/survey-angular-ui/src/questions/imagepicker.component.ts
@@ -1,12 +1,27 @@
 import { Component } from "@angular/core";
 import { QuestionAngular } from "../question";
-import { QuestionImagePickerModel } from "survey-core";
+import { ImageItemValue, QuestionImagePickerModel } from "survey-core";
 import { AngularComponentFactory } from "../component-factory";
 
 @Component({
   selector: "sv-ng-imagepicker-question",
   templateUrl: "./imagepicker.component.html"
 })
-export class ImagePickerQuestionComponent extends QuestionAngular<QuestionImagePickerModel> {}
+export class ImagePickerQuestionComponent extends QuestionAngular<QuestionImagePickerModel> {
+  public getItemValueComponentName(item: ImageItemValue): string {
+    return this.model.getItemValueWrapperComponentName(item) || "sv-ng-imagepicker-item";
+  }
+  public getItemValueComponentData(item: ImageItemValue): any {
+    return {
+      componentName: "sv-ng-imagepicker-item",
+      componentData: {
+        question: this.model,
+        model: item,
+        data: this.model.getItemValueWrapperComponentData(item)
+      }
+    };
+  }
+
+}
 
 AngularComponentFactory.Instance.registerComponent("imagepicker-question", ImagePickerQuestionComponent);

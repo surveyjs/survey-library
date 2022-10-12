c57e9d24945c8e11064d01b6fc0febff8ff6fce4
Dmitry Kuzin <dk981234@gmail.com>
2022-10-12 13:12:37 +0400

    Call microtask without zone

--- packages/survey-angular-ui/src/base-angular.ts
+++ packages/survey-angular-ui/src/base-angular.ts
@@ -4,7 +4,7 @@ import { EmbeddedViewContentComponent } from "./embedded-view-content.component"
 
 @Component({
   template: ""
-})
+  })
 export abstract class BaseAngular<T extends Base = Base> extends EmbeddedViewContentComponent implements DoCheck, OnDestroy {
   constructor(protected changeDetectorRef: ChangeDetectorRef, viewContainerRef?: ViewContainerRef) {
     super(viewContainerRef);
@@ -90,7 +90,8 @@ export abstract class BaseAngular<T extends Base = Base> extends EmbeddedViewCon
       this.detectChanges();
       this.afterUpdate();
     } else {
-      queueMicrotask(() => {
+      (!!(<any>window)["__zone_symbol__queueMicrotask"]
+        ? (<any>window)["__zone_symbol__queueMicrotask"] : queueMicrotask)(() => {
         this.detectChanges();
         this.afterUpdate();
       });

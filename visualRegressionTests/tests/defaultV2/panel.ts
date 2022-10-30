26ba823a196fa175504e3a7931de79d3b3935f8c
Dmitry <dk981234@gmail.com>
2022-10-30 19:13:42 +0400

    Work for #5128: fix errors for questions with titleLocation: "left" (#5132)

--- visualRegressionTests/tests/defaultV2/panel.ts
+++ visualRegressionTests/tests/defaultV2/panel.ts
@@ -299,6 +299,44 @@ frameworks.forEach(framework => {
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());
   });
+  test("Check panel with errors and question title location left", async (t) => {
+    await t.resizeWindow(1920, 1080);
+    await initSurvey(framework, {
+      questions: [
+        {
+          type: "panel",
+          name: "delivery_details",
+          title: "Please, specify the delivery details.",
+          width: "708px",
+          elements: [
+            {
+              type: "radiogroup",
+              name: "delivery_agent",
+              isRequired: true,
+              titleLocation: "left",
+              title: "Delivery agent",
+              choices: ["DHL", "Pony Express", "FedEx"]
+            },
+            {
+              type: "boolean",
+              isRequired: true,
+              titleLocation: "left",
+              name: "delivery_speed",
+              title: "Do you like to get the order as fast as it possible?"
+            }
+          ]
+        },
+      ]
+    });
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    const panelRoot = Selector(".sd-panel");
+    await resetFocusToBody();
+    await t.click(Selector("input[value='Complete']"));
+    await takeScreenshot("panel-with-question-title-left-and-errors-above.png", panelRoot, screenshotComparerOptions);
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
   test("Check panel with errors below", async (t) => {
     await t.resizeWindow(1920, 1080);
     await initSurvey(framework, {

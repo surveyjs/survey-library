d1d927d52b1ad29d6bf6bd04c744024913f91d88
OlgaLarina <olga.larina.developer@gmail.com>
2022-11-14 09:00:35 +0300

    Ranking Items do not apply the background color of the body style (#5218)
    * work for #5211 Ranking Items do not apply the background color of the body style
    
    * work for #5218 : fix for other themes
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- src/dragdrop/ranking-choices.ts
+++ src/dragdrop/ranking-choices.ts
@@ -21,7 +21,7 @@ export class DragDropRankingChoices extends DragDropChoices {
           border-radius: 36px;
           min-width: 100px;
           box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);
-          background-color: white;
+          background-color: var(--background, white);
           padding-right: 16px;
           padding-left: 20px;
           font-family: "Open Sans";

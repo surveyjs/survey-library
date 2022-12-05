104b53693c18656d6bdfcbac644cdebea4f8ee4f
Olga Larina <olga.larina.dev@gmail.com>
2022-12-05 13:13:26 +0300

    move others tsconfig.json files

--- src/vue/components/dropdown/dropdown.vue
+++ src/vue/components/dropdown/dropdown.vue
@@ -77,7 +77,7 @@
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 import { Question, DropdownListModel, Helpers } from "survey-core";
-import BaseVue from "src/vue/base";
+import BaseVue from "../../base";
 
 @Component
 export class DropdownComponent extends BaseVue {

bd8e86251f5d6b6ba11ecef9cf56e79e4d7f825a
OlgaLarina <olga.larina.dev@gmail.com>
2022-12-07 19:11:12 +0300

    work for #5312 Generate .d.ts the same way in windows/linux

--- build-scripts/survey-knockout-ui/webpack.config.js
+++ build-scripts/survey-knockout-ui/webpack.config.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const webpackCommonConfigCreator = require("../webpack.common");
+const DtsGeneratorPlugin = require("../webpack-dts-generator");
 const { merge } = require("webpack-merge");
 var packageJson = require("./package.json");
 
@@ -18,7 +19,15 @@ const config = {
       commonjs: "survey-core",
       amd: "survey-core"
     }
-  }
+  },
+  plugins: [
+    new DtsGeneratorPlugin({
+      tsConfigPath: "./build-scripts/survey-knockout-ui/tsconfig.typing.ko-ui.json",
+      filePath: "build/survey-knockout-ui/survey-knockout-ui.d.ts",
+      moduleName: "survey-knockout-ui",
+      importName: "entries/knockout-ui-model"
+    }),
+  ],
 };
 
 module.exports = function (options) {

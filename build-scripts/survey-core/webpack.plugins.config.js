e89b960ae6d121aec096d545f74363e52e7fd036
OlgaLarina <olga.larina.dev@gmail.com>
2022-12-12 13:04:23 +0300

    work for #5197 : use DtsGeneratorProgressPlugin for generate theme d.ts

--- build-scripts/survey-core/webpack.plugins.config.js
+++ build-scripts/survey-core/webpack.plugins.config.js
@@ -3,6 +3,7 @@
 const webpackCommonConfigCreator = require("../webpack.common");
 const { merge } = require("webpack-merge");
 var FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const DtsGeneratorPlugin = require("../webpack-dts-generator");
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 var path = require("path");
 
@@ -19,7 +20,71 @@ const config = {
     ["survey-winter-theme"]: path.resolve(__dirname, "../../src/plugins/themes/legacy-default/winter.ts"),
     ["survey-winterstone-theme"]: path.resolve(__dirname, "../../src/plugins/themes/legacy-default/winterstone.ts"),
   },
-  plugins: [new FixStyleOnlyEntriesPlugin()],
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new DtsGeneratorPlugin({
+      arrayOptions: [{ 
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.default.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-default-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-default-theme",
+        importName: "legacy-default/standard"
+      },
+      {
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.bootstrap.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-bootstrap-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-bootstrap-theme",
+        importName: "bootstrap/index"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.bootstrapmaterial.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-bootstrapmaterial-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-bootstrapmaterial-theme",
+        importName: "bootstrapmaterial/index"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.modern.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-modern-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-modern-theme",
+        importName: "modern/index"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.orange.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-orange-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-orange-theme",
+        importName: "legacy-default/orange"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.darkblue.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-darkblue-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-darkblue-theme",
+        importName: "legacy-default/darkblue"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.darkrose.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-darkrose-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-darkrose-theme",
+        importName: "legacy-default/darkrose"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.stone.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-stone-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-stone-theme",
+        importName: "legacy-default/stone"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.winter.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-winter-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-winter-theme",
+        importName: "legacy-default/winter"
+      },
+      {        
+        tsConfigPath: "./build-scripts/survey-core/tsconfig.plugins.winterstone.theme.typing.json",
+        filePath: "build/survey-core/plugins/survey-winterstone-theme.d.ts",
+        moduleName: "survey-core/plugins/survey-winterstone-theme",
+        importName: "legacy-default/winterstone"
+      }]
+    }),
+  ],
   externals: {
     "survey-core": {
       root: "Survey",
--- build-scripts/webpack-dts-generator.js
+++ build-scripts/webpack-dts-generator.js
@@ -4,19 +4,29 @@ const fs = require('fs');
 const child_process = require('child_process');
 
 module.exports = function DtsGeneratorProgressPlugin(options) {
+  var dtsProcess = function(opt) {
+    const tsConfigPath = opt.tsConfigPath || "tsconfig.typing.json";
+    console.log("typescript start " + tsConfigPath);
+    const tsCommand = opt.tsCommand || "tsc --p";
+    const command = tsCommand + " " + tsConfigPath;
+    console.log("tsc command is \"" + command + "\"");
+    child_process.execSync(command);
+    console.log("typescript end");
+
+    const content = "\ndeclare module \"" + opt.moduleName + "\" { import main = require(\"" + opt.importName + "\"); export = main; }";
+    console.log("\"" + content + "\"");
+    fs.writeFile(opt.filePath, content, { flag: 'a' }, err => { if(err) { console.error(err); } });
+  };
+
   return new webpack.ProgressPlugin(function(progress) {
     if (progress === 1) {
-      console.log("typescript start");
-      const tsConfigPath = options.tsConfigPath || "tsconfig.typing.json";
-      const tsCommand = options.tsCommand || "tsc --p";
-      const command = tsCommand + " " + tsConfigPath;
-      console.log("tsc command is \"" + command + "\"");
-      child_process.execSync(command);
-      console.log("typescript end");
-
-      const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"" + options.importName + "\"); export = main; }";
-      console.log("\"" + content + "\"");
-      fs.writeFile(options.filePath, content, { flag: 'a' }, err => { if(err) { console.error(err); } });
+      if(Array.isArray(options.arrayOptions)) {
+        options.arrayOptions.forEach(opt => {
+          dtsProcess(opt);
+        });
+      } else {
+        dtsProcess(options);
+      }
     }
   });
 };

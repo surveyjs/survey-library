3c35bec910f60fe74d7bd9442c19745f7e566b5b
Aleksey Novikov <novikov82@gmail.com>
2022-12-19 16:38:33 +0300

    #5354 Optimize width for narrow matrix column (#5368)
    * #5354 Optimize width for narrow matrix column
    Fixes #5354
    
    * #5354 - remove rowTitleWidth from matrixdynamic
    
    * #5354 - fix build

--- tests/markup/etalon_matrix.ts
+++ tests/markup/etalon_matrix.ts
@@ -94,6 +94,36 @@ registerMarkupTests(
       snapshot: "matrix-mobile-v2",
       excludePlatform: "Vue"
     },
+    {
+      name: "Test matrix question row title and column width",
+      json: {
+        "elements": [
+          {
+            "type": "matrix",
+            "name": "matrix",
+            "titleLocation": "hidden",
+            "rowTitleWidth": "100px",
+            "columnMinWidth": "20px",
+            "columns": [
+              {
+                "value": "col_1",
+              }, {
+                "value": "col_2",
+              },
+            ],
+            "rows": [
+              {
+                "value": "row_1",
+              }, {
+                "value": "row_2",
+              },
+            ]
+          }
+        ]
+      },
+      snapshot: "matrix-widths",
+      excludePlatform: "Vue"
+    },
   ]
 );
 

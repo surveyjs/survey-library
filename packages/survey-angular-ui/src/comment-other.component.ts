0437d445a7e4bd378d29478cb9ef7fa289b2e506
Andrew Telnov <andrew.telnov@gmail.com>
2022-12-20 15:11:32 +0200

    Fix compilation errors in angular #5358

--- packages/survey-angular-ui/src/comment-other.component.ts
+++ packages/survey-angular-ui/src/comment-other.component.ts
@@ -1,16 +1,22 @@
 import { Component, Input } from "@angular/core";
-import { QuestionSelectBase } from "survey-core";
+import { Question, QuestionSelectBase } from "survey-core";
 
 @Component({
   selector: "sv-ng-comment-other, '[sv-ng-comment-other]'",
   templateUrl: "./comment-other.component.html",
 })
 export class SurveyCommentOtherComponent {
-  @Input() question!: QuestionSelectBase;
+  @Input() question!: Question;
   constructor() {
   }
   public get otherValue(): string {
-    if(!this.question.otherValue) return "";
-    return this.question.otherValue;
+    const val = (<QuestionSelectBase>this.question).otherValue;
+    return !!val ? val : "";
+  }
+  public onOtherValueChange(event: any): void {
+    (<QuestionSelectBase>this.question).onOtherValueChange(event);
+  }
+  public onOtherValueInput(event: any): void {
+    (<QuestionSelectBase>this.question).onOtherValueInput(event);
   }
 }

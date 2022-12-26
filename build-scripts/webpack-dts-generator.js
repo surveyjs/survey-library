d81cee44f235b3b0887a3c333b79e73d118040a8
Olga Larina <olga.larina.dev@gmail.com>
2022-12-26 18:18:47 +0300

    work for #5197 : rollback

--- build-scripts/webpack-dts-generator.js
+++ build-scripts/webpack-dts-generator.js
@@ -6,17 +6,17 @@ const child_process = require('child_process');
 module.exports = function DtsGeneratorProgressPlugin(options) {
   return new webpack.ProgressPlugin(function(progress) {
     if (progress === 1) {
-      const tsConfigPath = opt.tsConfigPath || "tsconfig.typing.json";
-      console.log("typescript start " + tsConfigPath);
-      const tsCommand = opt.tsCommand || "tsc --p";
+      console.log("typescript start");
+      const tsConfigPath = options.tsConfigPath || "tsconfig.typing.json";
+      const tsCommand = options.tsCommand || "tsc --p";
       const command = tsCommand + " " + tsConfigPath;
       console.log("tsc command is \"" + command + "\"");
       child_process.execSync(command);
       console.log("typescript end");
 
-      const content = "\ndeclare module \"" + opt.moduleName + "\" { import main = require(\"" + opt.importName + "\"); export = main; }";
+      const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"" + options.importName + "\"); export = main; }";
       console.log("\"" + content + "\"");
-      fs.writeFile(opt.filePath, content, { flag: 'a' }, err => { if (err) { console.error(err); } });
+      fs.writeFile(options.filePath, content, { flag: 'a' }, err => { if(err) { console.error(err); } });
     }
   });
 };

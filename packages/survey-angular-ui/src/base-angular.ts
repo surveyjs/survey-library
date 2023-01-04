ffc0cff543ba9127a363ab771808d17176954114
Dmitry Kuzin <dk981234@gmail.com>
2023-01-04 15:53:29 +0400

    Do not call detectChanges multiple times for component

--- packages/survey-angular-ui/src/base-angular.ts
+++ packages/survey-angular-ui/src/base-angular.ts
@@ -100,21 +100,22 @@ export abstract class BaseAngular<T extends Base = Base> extends EmbeddedViewCon
       });
     }
   }
+  private getChangeDetectorRef() {
+    return this.embeddedView ? this.embeddedView : this.changeDetectorRef;
+  }
   protected getPropertiesToUpdateSync(): Array<string> {
     return [];
   }
   protected detectChanges() {
-    if(!!this.embeddedView) {
-      this.embeddedView.detectChanges();
-    } else {
-      this.changeDetectorRef.detectChanges();
-    }
+    this.getChangeDetectorRef().detectChanges();
   }
 
   protected beforeUpdate(): void {
+    this.getChangeDetectorRef().detach();
     this.setIsRendering(true);
   }
   protected afterUpdate(): void {
+    this.getChangeDetectorRef().reattach();
     this.setIsRendering(false);
   }
   ngAfterViewChecked(): void {

4e44ca6dca5195216684ff85843b14d032100d5b
Dmitry Kuzin <dk981234@gmail.com>
2023-01-13 17:44:03 +0400

    Work for #5422: add markup test

--- tests/markup/etalon.ts
+++ tests/markup/etalon.ts
@@ -17,6 +17,7 @@ export * from "./etalon_file";
 export * from "./etalon_paneldynamic";
 export * from "./etalon_page_panel";
 export * from "./etalon_matrix";
+export * from "./etalon_matrixdropdown";
 export * from "./etalon_matrixdynamic";
 export * from "./etalon_question";
 export { markupTests } from "./helper";

d4e921bb79758745525d15235f9955119fea23b6
OlgaLarina <olga.larina.dev@gmail.com>
2023-01-17 16:30:47 +0300

    work for #5357 add Vue NotifierComponent

---
+++ src/vue/components/notifier.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-show="model.active" v-bind:class="model.css">
+    <span>{{ model.message }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { Notifier } from "survey-core";
+import { BaseVue } from "../base";
+
+@Component
+export class NotifierComponent extends BaseVue {
+  @Prop() model: Notifier;
+
+  constructor() {
+    super();
+  }
+
+  getModel() {
+    return this.model;
+  }
+}
+
+Vue.component("sv-notifier", NotifierComponent);
+
+export default NotifierComponent;
+</script>

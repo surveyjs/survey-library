a651f63eac468347c5289eb89243054dba28923e
Aleksey Novikov <novikov82@gmail.com>
2023-01-24 09:54:17 +0300

    #5534 - fixed knockout handler

--- src/knockout/components/boolean-radio/boolean-radio-item.ts
+++ src/knockout/components/boolean-radio/boolean-radio-item.ts
@@ -9,7 +9,7 @@ ko.components.register("sv-boolean-radio-item", {
   viewModel: {
     createViewModel: (params: any) => {
       params.handleChange = () =>{
-        params.question.booleanValue = params.value == "true";
+        params.question.booleanValue = params.value;
       };
       return params;
     },

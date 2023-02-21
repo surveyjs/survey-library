2ecada9dfa7d44d81f99c65114709f1b7ef8b33e
TSV <tsv2013@users.noreply.github.com>
2023-02-21 14:12:01 +0300

    Work for #5551 - Implement TOC navigation - fixed angular f-tests

--- packages/survey-angular-ui/src/components-container.component.ts
+++ packages/survey-angular-ui/src/components-container.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, TemplateRef, ViewContainerRef, OnChanges, OnInit, SimpleChanges, ChangeDetectorRef } from "@angular/core";
 import { SurveyModel } from "survey-core";
 import { AngularComponentFactory } from "./component-factory";
+import { EmbeddedViewContentComponent } from "./embedded-view-content.component";
 
 @Component({
   selector: "sv-components-container, [sv-components-container], sv-ng-components-container",
-  templateUrl: "./components-container.component.html"
+  templateUrl: "./components-container.component.html",
+  styleUrls: ["./hide-host.scss"]
 })
-export class ComponentsContainerComponent {
+export class ComponentsContainerComponent extends EmbeddedViewContentComponent {
   @Input() survey!: SurveyModel;
   @Input() container!: string;
   get components(): Array<any> {

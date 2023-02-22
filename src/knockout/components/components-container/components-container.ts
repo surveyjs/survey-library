ae7562faaf92c2bd74067bbbc1981521857fab2b
TSV <tsv2013@users.noreply.github.com>
2023-02-22 17:52:51 +0300

    Work for #5551 - Implement TOC navigation - fixed ko components container update

--- src/knockout/components/components-container/components-container.ts
+++ src/knockout/components/components-container/components-container.ts
@@ -2,18 +2,21 @@ import * as ko from "knockout";
 import { SurveyModel } from "survey-core";
 const template: string = require("./components-container.html");
 
+export class ComponentsContainer {
+  constructor(public survey: SurveyModel, private container: any) {
+  }
+  css = "sv-components-column";
+  get components(): Array<any> {
+    return this.survey.getContainerContent(this.container);
+  }
+}
+
 ko.components.register("sv-components-container", {
   viewModel: {
     createViewModel: (params: any) => {
       const survey = params.survey as SurveyModel;
-      return {
-        css: "sv-components-column",
-        survey,
-        components: survey.getContainerContent(params.container)
-      };
+      return new ComponentsContainer(survey, params.container);
     },
   },
   template: template
 });
-
-export const ComponentsConteiner: any = undefined;

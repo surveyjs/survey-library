438c622cecda9b9cd95cbac8cd1bb556d61d24eb
TSV <tsv2013@users.noreply.github.com>
2023-02-27 11:37:51 +0300

    Work for #5551 - Implement TOC navigation - fixed f-test and skipped one vr-test for Vue

--- testCafe/survey/options.js
+++ testCafe/survey/options.js
@@ -186,14 +186,19 @@ frameworks.forEach(framework => {
   });
 
   test("show top progress bar", async t => {
-    const questionElements = Selector(".sv_container > .sv-components-column > div");
+    let progressSelector = ".sv_container > div:nth-child(2)";
+    if(framework === "vue") { // TODO: reanimate Vue after Vue3 supported
+      progressSelector = ".sv_container > .sv-components-column > div";
+    }
+
+    const progressElement = Selector(progressSelector);
     await t.expect(progressbar.exists).notOk();
 
     await show_top_progress_bar();
     await t
       .expect(progressbar.visible).ok()
       .expect(progressbar.textContent).contains("Page 1 of 3")
-      .expect(questionElements.classNames).contains("sv_progress");
+      .expect(progressElement.classNames).contains("sv_progress");
   });
 
   test("show bottom progress bar", async t => {

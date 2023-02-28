33bd52f7d22382cbd40a4c104e71e10857b400a2
TSV <tsv2013@users.noreply.github.com>
2023-02-28 11:24:33 +0300

    Work for #5551 - Implement TOC navigation - fixed vr-test

--- packages/survey-angular-ui/src/components/progress/toc/progress.component.ts
+++ packages/survey-angular-ui/src/components/progress/toc/progress.component.ts
@@ -1,19 +1,22 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges, ChangeDetectorRef } from "@angular/core";
+import { EmbeddedViewContentComponent } from "../../../embedded-view-content.component";
 import { createTOCListModel, ListModel, SurveyModel } from "survey-core";
 import { AngularComponentFactory } from "../../../component-factory";
 
 @Component({
   selector: "sv-progress-toc, sv-ng-progress-toc",
-  templateUrl: "./progress.component.html"
-  })
-export class ProgressTocComponent implements OnChanges, OnInit {
+  templateUrl: "./progress.component.html",
+  styles: [":host { display: none; }"]
+})
+export class ProgressTocComponent extends EmbeddedViewContentComponent implements OnChanges, OnInit {
   public listModel!: ListModel;
   public containerCss = "sv_progress-toc";
   @Input() model!: SurveyModel;
   private createProgressTOCModel() {
     this.listModel = createTOCListModel(this.model);
   }
-  ngOnInit(): void {
+  override ngOnInit(): void {
+    super.ngOnInit();
     this.createProgressTOCModel();
   }
   ngOnChanges(changes: SimpleChanges): void {

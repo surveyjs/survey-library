1b61fb2b46f5d24a022216a8d5989b3daf5a426e
Dmitry Kuzin <dk981234@gmail.com>
2023-03-10 19:03:50 +0400

    Implement ActionBar component

---
+++ packages/survey-vue-ui/src/components/action-bar/ActionBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    v-if="model.hasActions"
+    ref="container"
+    :class="model.getRootCss()"
+    v-on:click="
+      (event) => {
+        event.stopPropagation();
+      }
+    "
+  >
+    <sv-action
+      v-for="item in model.renderedActions"
+      v-bind:key="item.id"
+      :item="item"
+    ></sv-action>
+  </div>
+</template>
+
+<script lang="ts">
+import { ActionContainer } from "survey-core";
+import { defineSurveyComponent } from "../../base";
+
+export default defineSurveyComponent({
+  // eslint-disable-next-line
+  name: "sv-action-bar",
+  props: {
+    model: ActionContainer,
+    handleClick: Boolean
+  },
+  computed: {
+    componentName() {
+      return this.item.component || "sv-action-bar-item";
+    },
+  },
+  mounted() {
+    if (!this.model.hasActions) return;
+    const container = this.$el;
+    this.model.initResponsivityManager(container);
+  },
+  beforeUnmount() {
+    this.model.resetResponsivityManager();
+  },
+  data: (vm: any) => {
+    return {
+      getModel: () => {
+        return vm.model;
+      },
+    };
+  },
+});
+</script>
---
+++ packages/survey-vue-ui/src/components/action-bar/ActionBarItemDropdown.vue
@@ -0,0 +1,61 @@
+<template>
+  <button
+    type="button"
+    :class="item.getActionBarItemCss()"
+    v-on:click="
+      () => {
+        item.action();
+      }
+    "
+    v-on:keyup="
+      (evt) => {
+        evt.stopPropagation();
+      }
+    "
+    v-bind:disabled="item.enabled !== undefined && item.enabled"
+    v-bind:title="item.tooltip || item.title"
+    v-bind:role="item.ariaRole"
+    v-bind:tabindex="item.disableTabStop ? -1 : 0"
+  >
+    <sv-svg-icon
+      v-if="item.iconName"
+      :class="item.cssClasses.itemIcon"
+      :iconName="item.iconName"
+      :size="item.iconSize"
+      :title="item.tooltip || item.title"
+    ></sv-svg-icon>
+
+    <span
+      v-if="item.hasTitle"
+      :class="item.getActionBarItemTitleCss()"
+      >{{ item.title }}</span
+    >
+    <sv-popup :model="item.popupModel"></sv-popup>
+  </button>
+</template>
+
+<script lang="ts">
+import { Action, ActionDropdownViewModel } from "survey-core";
+import { defineSurveyComponent } from "../../base";
+
+export default defineSurveyComponent({
+  // eslint-disable-next-line
+  name: "sv-action-bar-item-dropdown",
+  props: {
+    item: Action,
+  },
+  beforeCreate() {
+    this.viewModel = new ActionDropdownViewModel(this.item);
+  },
+  beforeUnmount() {
+    this.viewModel.dispose();
+  },
+  data: (vm: any) => {
+    return {
+      getModel: () => {
+        return vm.item;
+      },
+    };
+  },
+});
+</script>

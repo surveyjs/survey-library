4c00ac7fef3b876aa5e8b50e788580a5d347097d
Andrew Telnov <andrew.telnov@gmail.com>
2023-03-30 15:19:43 +0300

    Angular: onValueChanging callback doesn't work in composite component, add functional test #5903

---
+++ testCafe/survey/composite_components.ts
@@ -0,0 +1,57 @@
+import { url, initSurvey, getSurveyResult, frameworks } from "../helper";
+import { Selector, ClientFunction } from "testcafe";
+const title = "Components";
+
+const json = {
+  "elements": [
+    {
+      type: "comp1",
+      name: "q1"
+    }
+  ]
+};
+
+const registerComponet_onValueChanging = ClientFunction(() => {
+  window["Survey"].ComponentCollection.Instance.add({
+    name: "comp1",
+    elementsJSON: [
+      {
+        type: "text",
+        name: "item1"
+      },
+      {
+        type: "text",
+        name: "item2",
+        startWithNewLine: false
+      }
+    ],
+    onValueChanging(question, name, value) {
+      if (name === "item1" && value === "1") {
+        return "100";
+      }
+      return value;
+    }
+  });
+});
+
+frameworks.forEach(framework => {
+  fixture`${framework} ${title}`.page`${url}${framework}`.beforeEach(
+    async t => {
+    }
+  );
+  test("check custom markup in list behavior", async t => {
+    await registerComponet_onValueChanging();
+    await initSurvey(framework, json);
+    const item1Input = Selector("input[aria-label='item1']");
+    await t
+      .typeText(item1Input, "1")
+      .pressKey("tab")
+      .expect(item1Input.value).eql("100")
+      .click("input[value=Complete]");
+
+    const surveyResult = await getSurveyResult();
+    await t.expect(surveyResult).eql({
+      q1: { item1: "100" }
+    });
+  });
+});

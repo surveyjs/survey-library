64f2d8dfa2fb8b22b0c5b480edc3a8fe91439b94
dmitrykurmanov <kurmanov.work@gmail.com>
2023-03-30 16:56:53 +0400

    work for the https://github.com/surveyjs/survey-library/issues/5782

--- src/dragdrop/matrix-rows.ts
+++ src/dragdrop/matrix-rows.ts
@@ -27,6 +27,7 @@ export class DragDropMatrixRows extends DragDropCore<QuestionMatrixDynamicModel>
     const clone = <HTMLElement>(row.cloneNode(isDeepClone));
 
     clone.style.cssText = `
+      filter: drop-shadow(0px 2px 6px rgba(0, 0, 0, 0.1));
       box-shadow: rgb(0 0 0 / 10%) 0px 8px 16px;
       background-color: white;
       display: flex;

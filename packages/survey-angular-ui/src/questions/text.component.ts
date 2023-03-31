5ae2096b6ac29bc4fb76a8461385334ff2e046d0
Andrew <andrew.telnov@gmail.com>
2023-03-31 11:27:46 +0300

    Angular: onValueChanging callback doesn't work in composite component… (#5904)
    * Angular: onValueChanging callback doesn't work in composite component, add functional test #5903
    
    * Work for #5903: fix text question value update
    
    * Fix undefined value showing in editor
    
    ---------
    
    Co-authored-by: Dmitry Kuzin <dk981234@gmail.com>

--- packages/survey-angular-ui/src/questions/text.component.ts
+++ packages/survey-angular-ui/src/questions/text.component.ts
@@ -7,7 +7,14 @@ import { AngularComponentFactory } from "../component-factory";
   selector: "sv-ng-text-question",
   templateUrl: "./text.component.html",
   styleUrls: ["./text.component.scss"]
-  })
-export class TextQuestionComponent extends QuestionAngular<QuestionTextModel> {}
+})
+export class TextQuestionComponent extends QuestionAngular<QuestionTextModel> {
+  get value(): string {
+    return this.model.value || "";
+  }
+  set value(val: string) {
+    this.model.value = val || "";
+  }
+}
 
 AngularComponentFactory.Instance.registerComponent("text-question", TextQuestionComponent);

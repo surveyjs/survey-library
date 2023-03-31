b3d7d06830d4adccac3c330dac90c0d0bf043846
tsv2013 <tsv2013@users.noreply.github.com>
2023-03-31 15:00:46 +0300

    Fixed https://github.com/surveyjs/survey-creator/issues/3916 - 'Apply' button in Edit Totals popup causes console error 'Cannot read properties of null (#5912)

--- src/question_matrixdropdownbase.ts
+++ src/question_matrixdropdownbase.ts
@@ -2305,10 +2305,10 @@ export class QuestionMatrixDropdownModelBase extends QuestionMatrixBaseModel<Mat
     return this.SurveyModel.getMatrixCellTemplateData(cell);
   }
   public getCellWrapperComponentName(cell: MatrixDropdownCell) {
-    return this.SurveyModel.getElementWrapperComponentName(cell, "cell");
+    return this.SurveyModel.getElementWrapperComponentName(cell, cell.row instanceof MatrixDropdownTotalRowModel ? "row-footer" : "cell");
   }
   public getCellWrapperComponentData(cell: MatrixDropdownCell) {
-    return this.SurveyModel.getElementWrapperComponentData(cell, "cell");
+    return this.SurveyModel.getElementWrapperComponentData(cell, cell.row instanceof MatrixDropdownTotalRowModel ? "row-footer" : "cell");
   }
   public getColumnHeaderWrapperComponentName(cell: MatrixDropdownCell) {
     return this.SurveyModel.getElementWrapperComponentName(

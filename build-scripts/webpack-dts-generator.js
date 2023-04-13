6cd0b42907f966e2ee5b7b3579bf9542b966d9d8
TSV <tsv2013@users.noreply.github.com>
2023-04-13 11:18:21 +0300

    Work for #5763 - changed typings generation way

--- build-scripts/webpack-dts-generator.js
+++ build-scripts/webpack-dts-generator.js
@@ -1,22 +1,27 @@
 'use strict';
 const webpack = require("webpack");
 const fs = require('fs');
+const path = require('path');
 const child_process = require('child_process');
 
 module.exports = function DtsGeneratorProgressPlugin(options) {
   return new webpack.ProgressPlugin(function (progress) {
     if (progress === 1) {
+      console.log("typings start");
       console.log("typescript start");
       const tsConfigPath = options.tsConfigPath || "tsconfig.typing.json";
       const tsCommand = options.tsCommand || "tsc --p";
-      const command = tsCommand + " " + tsConfigPath + " --outFile " + options.filePath;
+      const command = tsCommand + " " + tsConfigPath + " --outDir " + path.dirname(options.filePath) + "/typings/";
       console.log("tsc command is \"" + command + "\"");
       child_process.execSync(command);
       console.log("typescript end");
 
-      const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"" + options.importName + "\"); export = main; }";
+      // const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"./typings/" + options.importName + "\"); export = main; }";
+      const content = "\nexport * from \"./typings/" + options.importName + "\";";
       console.log("\"" + content + "\"");
-      fs.writeFile(options.filePath, content, { flag: 'a' }, err => { if (err) { console.error(err); } });
+      console.log(options.filePath);
+      fs.writeFile(options.filePath, content, { flag: 'w' }, err => { if (err) { console.error(err); } });
+      console.log("typings end");
     }
   });
 };

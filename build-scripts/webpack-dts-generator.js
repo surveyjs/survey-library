dadbd0540e24d8bb6cb5465b32aae437cd6757ab
TSV <tsv2013@users.noreply.github.com>
2023-04-13 16:43:04 +0300

    Work for #5763 - changed typings generation way - fixed Creator V1 build

--- build-scripts/webpack-dts-generator.js
+++ build-scripts/webpack-dts-generator.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
 
+const legacyModules = ["survey-angular", "survey-jquery", "survey-knockout", "survey-react", "survey-vue"];
+
 module.exports = function DtsGeneratorProgressPlugin(options) {
   return new webpack.ProgressPlugin(function (progress) {
     if (progress === 1) {
@@ -11,16 +13,25 @@ module.exports = function DtsGeneratorProgressPlugin(options) {
       console.log("typescript start");
       const tsConfigPath = options.tsConfigPath || "tsconfig.typing.json";
       const tsCommand = options.tsCommand || "tsc --p";
-      const command = tsCommand + " " + tsConfigPath + " --outDir " + path.dirname(options.filePath) + "/typings/";
+      let command = tsCommand + " " + tsConfigPath + " --outDir " + path.dirname(options.filePath) + "/typings/";
+      if (legacyModules.indexOf(options.moduleName) !== -1) {
+        command = tsCommand + " " + tsConfigPath + " --outFile " + options.filePath;
+      }
       console.log("tsc command is \"" + command + "\"");
       child_process.execSync(command);
       console.log("typescript end");
 
-      // const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"./typings/" + options.importName + "\"); export = main; }";
-      const content = "\nexport * from \"./typings/" + options.importName + "\";";
+      let content = "\nexport * from \"./typings/" + options.importName + "\";";
+      if (legacyModules.indexOf(options.moduleName) !== -1) {
+        content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"" + options.importName + "\"); export = main; }";
+      }
       console.log("\"" + content + "\"");
       console.log(options.filePath);
-      fs.writeFile(options.filePath, content, { flag: 'w' }, err => { if (err) { console.error(err); } });
+      if (legacyModules.indexOf(options.moduleName) !== -1) {
+        fs.writeFile(options.filePath, content, { flag: 'a' }, err => { if (err) { console.error(err); } });
+      } else {
+        fs.writeFile(options.filePath, content, { flag: 'w' }, err => { if (err) { console.error(err); } });
+      }
       console.log("typings end");
     }
   });

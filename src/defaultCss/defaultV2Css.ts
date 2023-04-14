1dff99fb9245ea7d28c46449f941d31f58850640
OlgaLarina <olga.larina.dev@gmail.com>
2023-04-14 09:52:57 +0300

    resolve #5924 Dropdown-Tagbox: item focused state for keyboard navigation

--- src/defaultCss/defaultV2Css.ts
+++ src/defaultCss/defaultV2Css.ts
@@ -585,7 +585,8 @@ export var defaultV2Css = {
     root: "sv-list__container sd-list",
     item: "sv-list__item sd-list__item",
     itemBody: "sv-list__item-body sd-list__item-body",
-    itemSelected: "sv-list__item--selected sd-list__item--selected"
+    itemSelected: "sv-list__item--selected sd-list__item--selected",
+    itemFocused: "sv-list__item--focused sd-list__item--focused",
   },
   actionBar: {
     root: "sd-action-bar",

dc543d0263ed0baf2dcb319f06e35505d4c87161
OlgaLarina <olga.larina.dev@gmail.com>
2023-04-25 09:50:10 +0300

    fix hover in creator

--- src/defaultCss/defaultV2Css.ts
+++ src/defaultCss/defaultV2Css.ts
@@ -255,6 +255,7 @@ export var defaultV2Css = {
     itemChecked: "sd-boolean--checked",
     itemIndeterminate: "sd-boolean--indeterminate",
     itemDisabled: "sd-boolean--disabled",
+    itemHover: "sd-boolean--allowhover",
     label: "sd-boolean__label",
     switch: "sd-boolean__switch",
     disabledLabel: "sd-checkbox__label--disabled",

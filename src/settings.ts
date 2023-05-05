766c8b6eb935a278ba4e0ebc658c98335f0ded15
Roman Tsukanov <roman.tsukanov@devexpress.com>
2023-05-05 14:24:27 +0400

    Describe `matrix.rateSize`

--- src/settings.ts
+++ src/settings.ts
@@ -479,6 +479,9 @@ export var settings = {
    * - `columnWidthsByType`: `Object`\
    * An object that specifies fixed and minimum column width based on the column type.\
    * Example: `settings.matrix.columnWidthsByType = { "tagbox": { minWidth: "240px", width: "300px" } }`
+   *
+   * - `rateSize`: `"small"` (default) | `"normal"`\
+   * Specifies the size of rate values. Applies to [Rating Scale](https://surveyjs.io/form-library/examples/rating-scale/) questions within matrixes.
    */
   matrix: {
     columnWidthsByType: columnWidthsByType,

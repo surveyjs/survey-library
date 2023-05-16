3f8394a3d0a228c4af4b59866e6a1b38da73b42d
dmitrykurmanov <kurmanov.work@gmail.com>
2023-05-16 19:38:48 +0400

    work for the https://github.com/surveyjs/survey-library/issues/6058

---
+++ testCafe/a11y/textbase.ts
@@ -0,0 +1,81 @@
+import { frameworks, url, initSurvey } from "../helper";
+import { fixture, test } from "testcafe";
+import { axeCheck, createReport } from "axe-testcafe";
+const title = "textbase";
+
+const json = {
+  "elements": [
+    {
+      name: "name",
+      type: "text",
+      indent: 1
+    },
+    {
+      "type": "text",
+      "name": "question1",
+      "title": "Username",
+      "isRequired": true,
+      "maxLength": 25
+    },
+    {
+      name: "name",
+      type: "text",
+      title: "Question title",
+      titleLocation: "hidden"
+    },
+    {
+      "type": "multipletext",
+      "name": "register",
+      "title": "Root Title",
+      "items": [
+        {
+          "name": "username",
+          "isRequired": true,
+          "title": "Title 1",
+          "maxLength": 10
+        },
+        {
+          "name": "email",
+          "title": "Title 2",
+          "inputType": "email"
+        },
+      ]
+    }
+  ]
+};
+
+frameworks.forEach((framework) => {
+  fixture`${framework} a11y:${title}`.page`${url}${framework}.html`.beforeEach(
+    async (t) => {
+      await initSurvey(framework, json);
+    }
+  );
+
+  test("axe check", async (t) => {
+    const { error, violations } = await axeCheck(t, this, {
+      runOnly: {
+        type: "tag",
+        values: ["wcag21a", "wcag21aa"/*, 'wcag412'*/]
+      },
+      rules: {
+        //https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
+        "color-contrast": {
+          enabled: false
+        },
+        "document-title": {
+          enabled: false
+        },
+        "landmark-one-main": {
+          enabled: false
+        },
+        "page-has-heading-one": {
+          enabled: false
+        },
+        "region": {
+          enabled: false
+        }
+      }
+    });
+    await t.expect(violations.length === 0).ok(createReport(violations));
+  });
+});

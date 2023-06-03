10f734707e9aa110e539c12fee7df56c6f66ffe8
Andrew Telnov <andrew.telnov@gmail.com>
2023-06-03 19:24:36 +0300

    Remove koErrorClass from knockout #6301

--- src/knockout/kopage.ts
+++ src/knockout/kopage.ts
@@ -70,15 +70,11 @@ export class PanelImplementorBase extends ImplementorBase {
 export class Panel extends PanelModel {
   private _implementor: ImplementorBase;
   koElementType: any;
-  koErrorClass: any;
   constructor(name: string = "") {
     super(name);
     this.onCreating();
     var self = this;
     this.koElementType = ko.observable("survey-panel");
-    this.koErrorClass = ko.pureComputed(function () {
-      return self.cssError;
-    });
   }
   protected onBaseCreating() {
     super.onBaseCreating();
@@ -92,7 +88,6 @@ export class Panel extends PanelModel {
     this.locTitle.strChanged();
   }
   public dispose() {
-    this.koErrorClass.dispose();
     this._implementor.dispose();
     this._implementor = undefined;
     super.dispose();

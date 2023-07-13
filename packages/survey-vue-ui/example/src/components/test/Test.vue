3a5fb80f0e922af941bca274e573855bfcd8d886
Dmitry Kuzin <dk981234@gmail.com>
2023-07-13 17:42:47 +0400

    Add jQuery in test example

--- packages/survey-vue-ui/example/src/components/test/Test.vue
+++ packages/survey-vue-ui/example/src/components/test/Test.vue
@@ -2,6 +2,8 @@
 import * as Survey from "survey-core";
 import { RouterView } from 'vue-router'
 (window as any).Survey = Survey;
+import $ from 'jquery'
+window.jQuery = window.$ = $
 </script>
 <template>
     <RouterView></RouterView>

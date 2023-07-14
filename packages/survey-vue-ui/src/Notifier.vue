b33e126459191187fb4276d2afb11c259b97bda5
Dmitry Kuzin <dk981234@gmail.com>
2023-07-14 15:29:15 +0400

    Fix notifier is not reactive

--- packages/survey-vue-ui/src/Notifier.vue
+++ packages/survey-vue-ui/src/Notifier.vue
@@ -1,19 +1,30 @@
 <template>
-    <div v-bind:class="model.css" v-show="model.isDisplayed" :style="{ visibility: model.active ? 'visible' : 'hidden' }" role="alert" aria-live="polite">
-        <span>{{ model.message }}</span>
-        <sv-action-bar :model="model.actionBar"></sv-action-bar>
-    </div>
+  <div
+    v-bind:class="model.css"
+    v-show="model.isDisplayed"
+    :style="{ visibility: model.active ? 'visible' : 'hidden' }"
+    role="alert"
+    aria-live="polite"
+  >
+    <span>{{ model.message }}</span>
+    <sv-action-bar :model="model.actionBar"></sv-action-bar>
+  </div>
 </template>
 
 <script lang="ts">
 import type { Notifier } from "survey-core";
-import { defineComponent, type PropType } from 'vue';
+import { defineComponent, type PropType } from "vue";
 import { BaseVue } from "./base";
 
 export default defineComponent({
-    props: {
-        model: Object as PropType<Notifier>
+  props: {
+    model: { type: Object as PropType<Notifier>, required: true },
+  },
+  mixins: [BaseVue],
+  methods: {
+    getModel() {
+      return this.model;
     },
-    mixins: [BaseVue]
-})
+  },
+});
 </script>

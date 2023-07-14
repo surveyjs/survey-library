6aae0fa0aaa85774c8bd1124555ff487ab40d0c9
Dmitry Kuzin <dk981234@gmail.com>
2023-07-14 20:58:35 +0400

    Add custom items in test example

--- packages/survey-vue-ui/example/src/main.ts
+++ packages/survey-vue-ui/example/src/main.ts
@@ -1,12 +1,17 @@
 import SurveyPlugin  from "survey-vue-ui";
 
 import { createApp } from 'vue'
+import Action from "./components/test/test-custom-components/Action.vue"
+import Item from "./components/test/test-custom-components/Item.vue"
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(SurveyPlugin);
 
+
+app.component("svc-custom-action", Action);
+app.component("new-item", Item);
 
 app.use(router)
 

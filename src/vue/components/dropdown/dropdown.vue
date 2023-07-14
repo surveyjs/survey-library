66b32a5301ac8b003b6684acf0e2fd1b549be681
OlgaLarina <olga.larina.dev@gmail.com>
2023-07-14 15:10:01 +0300

    work for #6226 rollback event changed

--- src/vue/components/dropdown/dropdown.vue
+++ src/vue/components/dropdown/dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="question.cssClasses.selectWrapper" @pointerdown="click">
+  <div :class="question.cssClasses.selectWrapper" @click="click">
     <div
       v-if="!question.isReadOnly"
       :id="question.inputId"

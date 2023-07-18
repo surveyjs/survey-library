df160ddf0beae1ab876f8224c38edcd7a3790bc2
Dmitry Kuzin <dk981234@gmail.com>
2023-07-18 18:48:01 +0400

    Fix multi word component names error (eslint)

--- packages/survey-vue-ui/.eslintrc.cjs
+++ packages/survey-vue-ui/.eslintrc.cjs
@@ -1,15 +1,18 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
   ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};

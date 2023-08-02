bf57d24e33040095f8105fa0e00146d5735e84ba
Dmitry Kuzin <dk981234@gmail.com>
2023-08-02 14:25:07 +0400

    Fix f tests

--- testCafe/components/tooltip.ts
+++ testCafe/components/tooltip.ts
@@ -5,18 +5,6 @@ const title = "Tooltip errors";
 var json = {
   questions: [
     {
-      type: "matrixdynamic",
-      name: "matrixdynamic_question",
-      columns: [
-        {
-          isRequired: true,
-          name: "Column 1",
-        },
-      ],
-      choices: [1],
-      rowCount: 1,
-    },
-    {
       type: "multipletext",
       name: "multipletext",
       items: [
@@ -45,23 +33,14 @@ frameworks.forEach((framework) => {
     const getTooltipPosition = (base: number, offset: number): string => {
       return base + offset + 12 + "px";
     };
-    const matrixCellSelector = Selector(".sd-table__cell:not(.sd-table__cell--header)");
     const multipleTextCellSelector = Selector(".sd-multipletext__cell");
     const tooltipClass = ".sd-question__erbox--tooltip";
     await t
       .click(Selector("input[value='Complete']"));
     await focusBody();
-    const tooltipInMatrix = matrixCellSelector.find(tooltipClass);
     const tooltipInMultipleText = multipleTextCellSelector.find(tooltipClass);
-    const matrixCellClientRect = await matrixCellSelector.boundingClientRect;
     const multipleTextCellClientRect = await multipleTextCellSelector.boundingClientRect;
-    await t.hover(matrixCellSelector, { offsetX: 10, offsetY: 10 })
-      .expect(tooltipInMatrix.visible).ok()
-      .expect(tooltipInMatrix.getStyleProperty("left")).eql(getTooltipPosition(matrixCellClientRect.left, 10))
-      .expect(tooltipInMatrix.getStyleProperty("top")).eql(getTooltipPosition(matrixCellClientRect.top, 10))
-      .hover(matrixCellSelector, { offsetX: 20, offsetY: 20 })
-      .expect(tooltipInMatrix.getStyleProperty("left")).eql(getTooltipPosition(matrixCellClientRect.left, 20))
-      .expect(tooltipInMatrix.getStyleProperty("top")).eql(getTooltipPosition(matrixCellClientRect.top, 20))
+    await t
       .hover(multipleTextCellSelector, { offsetX: 10, offsetY: 10 })
       .expect(tooltipInMultipleText.visible).ok()
       .expect(tooltipInMultipleText.getStyleProperty("left")).eql(getTooltipPosition(multipleTextCellClientRect.left, 10))

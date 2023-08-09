66952b19b7845944e2aef92c1e66af8f4b274a66
Roman Tsukanov <roman.tsukanov@devexpress.com>
2023-08-09 10:33:48 +0400

    Update console warning texts

--- src/console-warnings.ts
+++ src/console-warnings.ts
@@ -1,10 +1,10 @@
 export class ConsoleWarnings {
   public static disposedObjectChangedProperty(propName: string, objType: string): void {
-    ConsoleWarnings.warn("Attempt to set property '" + propName + "' of a disposed object '" + objType + "'");
+    ConsoleWarnings.warn("An attempt to set a property \"" + propName + "\" of a disposed object \"" + objType + "\"");
   }
   public static inCorrectQuestionValue(questionName: string, val: any): void {
     const valStr = JSON.stringify(val, null, 3);
-    ConsoleWarnings.warn("Try to set incorrect value into question. Question name: '" + questionName + "', value: " + valStr);
+    ConsoleWarnings.warn("An attempt to assign an incorrect value" + valStr + " to the following question: \"" + questionName + "\"");
   }
   public static warn(text: string): void {
     // eslint-disable-next-line no-console

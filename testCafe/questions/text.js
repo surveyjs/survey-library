d5d3e3d3269688a448f7a2adecd1eec95be03b9b
Andrew Telnov <andrew.telnov@gmail.com>
2023-08-10 11:30:19 +0300

    Rename the functional test #6679

--- testCafe/questions/text.js
+++ testCafe/questions/text.js
@@ -219,9 +219,7 @@ frameworks.forEach((framework) => {
       .pressKey("backspace")
       .expect(characterCounter.textContent).eql("0/10");
   });
-  test("Remaining character counter", async (t) => {
-    const characterCounter = Selector(".sv-remaining-character-counter");
-
+  test("Allow Space As Answer", async (t) => {
     await initSurvey(framework, {
       questions: [
         {

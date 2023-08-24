dd5bd0cc7536fdf14536b1b32bd7f69820ff3e7a
Andrew Telnov <andrew.telnov@gmail.com>
2023-08-24 23:20:05 +0300

    Add a functional test

--- testCafe/survey/focusFirstQuestionAutomatic.ts
+++ testCafe/survey/focusFirstQuestionAutomatic.ts
@@ -1,4 +1,4 @@
-import { url, initSurvey, frameworks, getSurveyResult } from "../helper";
+import { url, initSurvey, frameworks, getSurveyResult, filterIsInViewport } from "../helper";
 import { Selector, ClientFunction } from "testcafe";
 
 const title = "focusFirstQuestionAutomatic";
@@ -67,5 +67,41 @@ frameworks.forEach(async framework => {
 
     let surveyResult = await getSurveyResult();
     await t.expect(surveyResult).eql({ });
+  });
+  test("Focus and scroll into view question", async t => {
+    const focusQuestion = ClientFunction((name, doScroll) => {
+      const q = window["survey"].getQuestionByName(name);
+      q.focus(false, doScroll);
+    });
+    const q1Sel = Selector("input[type=text]").nth(0);
+    const panel1_q1Sel = Selector("input[type=text]").nth(1);
+
+    await initSurvey(framework, {
+      elements: [
+        { type: "text", name: "q1" },
+        { type: "radiogroup", name: "q2", choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+        { type: "radiogroup", name: "q3", choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+        { type: "panel", name: "panel1",
+          elements: [
+            { type: "text", name: "panel1_q1" },
+            { type: "radiogroup", name: "panel1_q2", choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+            { type: "radiogroup", name: "panel1_q3", choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
+          ]
+        }
+      ]
+    });
+    await focusQuestion("panel1_q1", false);
+    await t.expect(panel1_q1Sel.filter(filterIsInViewport).exists).ok()
+      .pressKey("a");
+    await focusQuestion("q1", false);
+    await t.expect(q1Sel.filter(filterIsInViewport).exists).ok()
+      .pressKey("b");
+    await focusQuestion("panel1_q1", true);
+    await t.expect(panel1_q1Sel.filter(filterIsInViewport).exists).ok()
+      .expect(q1Sel.filter(filterIsInViewport).exists).notOk();
+
+    await t.click("input[value=Complete]");
+    let surveyResult = await getSurveyResult();
+    await t.expect(surveyResult).eql({ q1: "b", panel1_q1: "a" });
   });
 });

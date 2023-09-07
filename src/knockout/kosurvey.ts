8ec4627388ebdc09fa87c0fcf8340e569f1c71a8
Andrew Telnov <andrew.telnov@gmail.com>
2023-09-07 09:36:09 +0300

    Fix the issue #6865

--- src/knockout/kosurvey.ts
+++ src/knockout/kosurvey.ts
@@ -171,7 +171,7 @@ LocalizableString.prototype["onCreating"] = function () {
 };
 
 ItemValue.prototype["onCreating"] = function () {
-  // new ImplementorBase(this);
+  new ImplementorBase(this);
   this.koText = ko.pureComputed(() => { return this.locText.koRenderedHtml(); });
 };
 

7d7f80db7ee889bebe8e5d0284fba2fd9e665d0e
dmitrykurmanov <kurmanov.work@gmail.com>
2023-09-08 16:36:27 +0400

    work for the https://github.com/surveyjs/survey-library/issues/6859 (remove code duplication)

--- src/question_checkbox.ts
+++ src/question_checkbox.ts
@@ -579,54 +579,8 @@ export class QuestionCheckboxModel extends QuestionCheckboxBase {
   }
 
   //a11y
-  public get titleAriaLabel(): string | null {
-    return null;
-  }
-
-  public get ariaRole(): string {
-    return null;
-  }
-  public get ariaRequired():any {
-    return null;
-  }
-  public get ariaInvalid():any {
-    return null;
-  }
-  public get ariaLabel(): string {
-    return null;
-  }
-  public get ariaLabelledBy(): string {
-    return null;
-  }
-  public get ariaDescribedBy(): string {
-    return null;
-  }
-
-  public get a11y_input_ariaRole(): string {
-    return "listbox";
-  }
-  public get a11y_input_ariaRequired(): "true" | "false" {
-    return this.isRequired ? "true" : "false";
-  }
-  public get a11y_input_ariaInvalid(): "true" | "false" {
-    return this.errors.length > 0 ? "true" : "false";
-  }
-  public get a11y_input_ariaLabel(): string {
-    if (this.hasTitle) {
-      return null;
-    } else {
-      return this.locTitle.renderedHtml;
-    }
-  }
-  public get a11y_input_ariaLabelledBy(): string {
-    if (this.hasTitle) {
-      return this.ariaTitleId;
-    } else {
-      return null;
-    }
-  }
-  public get a11y_input_ariaDescribedBy(): string {
-    return this.errors.length > 0 ? this.id + "_errors" : null;
+  public get isNewA11yStructure(): boolean {
+    return true;
   }
   // EO a11y
 }

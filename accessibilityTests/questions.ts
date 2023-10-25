f32eaca92484f8169cbaa2d093be6aec918ccb6c
tsv2013 <tsv2013@noreply.github.com>
2023-10-25 15:53:23 +0300

    Work for https://github.com/surveyjs/service/issues/1288 - refactored a11y tests

--- accessibilityTests/questions.ts
+++ accessibilityTests/questions.ts
@@ -1,69 +1,58 @@
-import { frameworks, url, initSurvey, axeTags } from "./helper";
+import { frameworks, url, initSurvey, axeTags, axeContext, axeOptions } from "./helper";
 import { fixture, test } from "testcafe";
 import { axeCheck, createReport } from "axe-testcafe";
 const title = "others";
 
-const json = {
-  "elements": [
-    {
-      type: "boolean",
-      name: "bool",
-      title: "Boolean",
-      label: "Are you 21 or older?",
-    },
-    {
-      type: "image",
-      name: "banner",
-      imageHeight: "300px",
-      imageWidth: "450px",
-      imageLink:
-        "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
-    },
-    {
-      type: "file",
-      title: "File",
-      name: "image",
-      storeDataAsText: false,
-      showPreview: true,
-      imageWidth: 150,
-      maxSize: 102400
-    },
-  ]
-};
-
 frameworks.forEach((framework) => {
   fixture`${framework} a11y:${title}`.page`${url}${framework}`.beforeEach(
     async (t) => {
-      await initSurvey(framework, json);
     }
   );
 
-  test("axe check", async (t) => {
-    const axeContext = { include: [[".sv_p_root"]] };
-    const axeOptions = {
-      runOnly: {
-        type: "tag",
-        values: axeTags
-      },
-      rules: {
-        //https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
-        "color-contrast": {
-          enabled: false
+  test("axe check boolean", async (t) => {
+    await initSurvey(framework, {
+      "elements": [
+        {
+          type: "boolean",
+          name: "bool",
+          title: "Boolean",
+          label: "Are you 21 or older?",
         },
-        "document-title": {
-          enabled: false
+      ]
+    });
+    const { error, violations } = await axeCheck(t, axeContext, axeOptions);
+    await t.expect(violations.length === 0).ok(createReport(violations));
+  });
+  test("axe check image", async (t) => {
+    await initSurvey(framework, {
+      "elements": [
+        {
+          type: "image",
+          name: "banner",
+          imageHeight: "300px",
+          imageWidth: "450px",
+          imageLink:
+            "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
         },
-        "landmark-one-main": {
-          enabled: false
+      ]
+    });
+    const { error, violations } = await axeCheck(t, axeContext, axeOptions);
+    await t.expect(violations.length === 0).ok(createReport(violations));
+  });
+  test("axe check file", async (t) => {
+    await initSurvey(framework, {
+      "elements": [
+        {
+          type: "file",
+          title: "File",
+          name: "image",
+          storeDataAsText: false,
+          showPreview: true,
+          imageWidth: 150,
+          maxSize: 102400
         },
-        "page-has-heading-one": {
-          enabled: false
-        },
-        "region": {
-          enabled: false
-        }
-      }
-    };
+      ]
+    });
     const { error, violations } = await axeCheck(t, axeContext, axeOptions);
     await t.expect(violations.length === 0).ok(createReport(violations));
   });

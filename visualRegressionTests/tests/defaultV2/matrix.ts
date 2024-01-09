1ea11df360baa13f19d07323dd33047dc206259c
Aleksey Novikov <novikov82@gmail.com>
2024-01-09 09:32:07 +0300

    #7595 Dynamic matrix expand button has wrong focus. - reset focus in tests Fixes #7595

--- visualRegressionTests/tests/defaultV2/matrix.ts
+++ visualRegressionTests/tests/defaultV2/matrix.ts
@@ -656,7 +656,7 @@ frameworks.forEach(framework => {
       await takeElementScreenshot("question-matrix-dropdown-detail-no-header-expanded.png", questionRoot, t, comparer);
 
       await t.click(Selector(".sd-table__cell--detail-button").filterVisible().nth(1));
-      await t.hover(questionRoot, { offsetX: 1, offsetY: 1 });
+      await t.click(questionRoot, { offsetX: 1, offsetY: 1 });
 
       await takeElementScreenshot("question-matrix-dropdown-detail-no-header.png", questionRoot, t, comparer);
     });

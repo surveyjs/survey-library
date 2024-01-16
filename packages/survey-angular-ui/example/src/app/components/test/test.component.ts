54ca5adc2fcfa3b1cc7577535531cfd34654fa9b
Dmitry Kurmanov <kurmanov.work@gmail.com>
2024-01-16 15:36:57 +0400

    PR: PopupSurveyModel: Redesign  (#7576)
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668 (lint)
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668 (lint)
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668
    
    * work for the https://github.com/surveyjs/service/issues/1668

--- packages/survey-angular-ui/example/src/app/components/test/test.component.ts
+++ packages/survey-angular-ui/example/src/app/components/test/test.component.ts
@@ -11,13 +11,15 @@ import "survey-core/survey.i18n";
 export class TestComponent {
   public model?: SurveyModel | Survey.PopupSurveyModel;
   public isPopup: boolean = true;
+  public isExpanded: boolean = true;
   constructor(private changeDetectorRef: ChangeDetectorRef) {
     (window as any).Survey = Survey;
     Survey.defaultStandardCss.variables.mobileWidth = <any>undefined;
     Survey.modernCss.variables.mobileWidth = <any>undefined;
-    (<any>window).setSurvey = (survey: SurveyModel | Survey.PopupSurveyModel, isPopup: boolean) => {
+    (<any>window).setSurvey = (survey: SurveyModel | Survey.PopupSurveyModel, isPopup: boolean, isExpanded: boolean = true) => {
       this.model = survey;
       this.isPopup = isPopup;
+      this.isExpanded = isExpanded;
       this.changeDetectorRef.detectChanges();
     };
     (window as any).Survey.defaultBootstrapCss = defaultCss;

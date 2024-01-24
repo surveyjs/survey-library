c28400e2080943d74e156508b130a8f6c749367b
Andrew Telnov <andrew.telnov@gmail.com>
2024-01-24 13:54:56 +0200

    properties visibility for single component from inheritBaseProps don't work correctly fix #7725

--- src/jsonobject.ts
+++ src/jsonobject.ts
@@ -448,7 +448,12 @@ export class JsonObjectProperty implements IObject {
   public isVisible(layout: string, obj: any = null): boolean {
     let isLayout = !this.layout || this.layout == layout;
     if (!this.visible || !isLayout) return false;
-    if (!!this.visibleIf && !!obj) return this.visibleIf(obj);
+    if (!!this.visibleIf && !!obj) {
+      if (obj.getOriginalObj) {
+        obj = obj.getOriginalObj() || obj;
+      }
+      return this.visibleIf(obj);
+    }
     return true;
   }
   public get visible(): boolean {

423100b27b1bba39f0b876eb6d6692e1c783224c
tsv2013 <tsv2013@noreply.github.com>
2024-02-09 15:32:32 +0300

    File question - fixed duplicated files upload

--- src/question_file.ts
+++ src/question_file.ts
@@ -1018,8 +1018,8 @@ export class QuestionFileModel extends QuestionFileModelBase {
     }
   }
   doChange = (event: any) => {
-    var src = event.target || event.srcElement;
-    this.onChange(src);
+    // var src = event.target || event.srcElement;
+    // this.onChange(src);
   }
   doClean = () => {
     if (this.needConfirmRemoveFile) {

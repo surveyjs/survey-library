4017589f483a457e79f4bee919abb1569a1dfc46
OlgaLarina <olga.larina.developer@gmail.com>
2024-03-27 10:00:50 +0300

    Multipletext question add mask support (#8017)
    * work for #7996 Multipletext question add mask support
    
    * work for #7996 Multipletext question add mask support
    
    * work for #7996 Multipletext question add mask support
    
    ---------
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- tests/entries/test.ts
+++ tests/entries/test.ts
@@ -72,6 +72,7 @@ export * from "../mask/lexical_analyzer_tests";
 export * from "../mask/syntactic_analyzer_tests";
 export * from "../mask/mask_datetime_tests";
 export * from "../mask/mask_settings_tests";
+export * from "../mask/multipletext_mask_settings_tests";
 export * from "../stylesManagerTests";
 export * from "../headerTests";
 

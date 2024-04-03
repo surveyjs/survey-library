a7045b6a4cf4b66641fb1cef29e60077f53fe13b
tsv2013 <tsv2013@users.noreply.github.com>
2024-04-03 10:58:33 +0300

    Implemented #8043 - Support swapOrder option for radio render mode of the boolean question (#8044)
    Co-authored-by: tsv2013 <tsv2013@noreply.github.com>

--- src/react/boolean-radio.tsx
+++ src/react/boolean-radio.tsx
@@ -44,8 +44,13 @@ export class SurveyQuestionBooleanRadio extends SurveyQuestionBoolean {
     return (
       <div className={cssClasses.rootRadio}>
         <fieldset role="presentation" className= { cssClasses.radioFieldset }>
-          {this.renderRadioItem(false, this.question.locLabelFalse)}
-          {this.renderRadioItem(true, this.question.locLabelTrue)}
+          {!this.question.swapOrder ?
+            (<>{this.renderRadioItem(false, this.question.locLabelFalse)}
+              {this.renderRadioItem(true, this.question.locLabelTrue)}</>)
+            :
+            (<>{this.renderRadioItem(true, this.question.locLabelTrue)}
+              {this.renderRadioItem(false, this.question.locLabelFalse)}</>)
+          }
         </fieldset>
       </div>
     );

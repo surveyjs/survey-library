bbf60236ec6c7c218d8cb6a2ef6908f94af99b95
Aleksey Novikov <novikov82@gmail.com>
2024-04-04 15:54:27 +0300

    #6776 - fixed markup tests

--- src/defaultCss/cssstandard.ts
+++ src/defaultCss/cssstandard.ts
@@ -336,7 +336,7 @@ export var defaultStandardCss = {
   file: {
     root: "sv_q_file",
     placeholderInput: "sv-visuallyhidden",
-    previewItem: "sd-file__preview-item",
+    previewItem: "sv_q_file_preview",
     removeButton: "sv_q_file_remove_button",
     fileInput: "sv-visuallyhidden",
     removeFile: "sv_q_file_remove",

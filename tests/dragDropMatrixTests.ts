6b9f007922700aae988bacbce0aecc9a3fabf76b
Andrew Telnov <andrew.telnov@gmail.com>
2024-04-08 10:14:05 +0300

    Do not allow to drop into locked top rows #8070

--- tests/dragDropMatrixTests.ts
+++ tests/dragDropMatrixTests.ts
@@ -28,3 +28,26 @@ QUnit.test("Clear the isGhostRow marker", function (assert) {
   dd.clear();
   assert.equal(renderedTable.rows[1].isGhostRow, false, "Row 0 isGhostRow marker is reset");
 });
+
+QUnit.test("matrix.lockedRowCount", function (assert) {
+  const survey = new SurveyModel({
+    elements: [{
+      type: "matrixdynamic",
+      name: "test",
+      rowCount: 2,
+      allowRowsDragAndDrop: true,
+      columns: [
+        { cellType: "text", name: "value" },
+        { cellType: "text", name: "text" },
+      ],
+
+    }]
+  });
+  const matrixQuestion = survey.getAllQuestions()[0] as QuestionMatrixDynamicModel;
+  matrixQuestion.lockedRowCount = 1;
+  const rows = matrixQuestion.visibleRows;
+  const dd = matrixQuestion.dragDropMatrixRows;
+  dd.dragInit(<any>undefined, <any>undefined, matrixQuestion);
+  assert.equal(dd.canInsertIntoThisRow(<any>rows[0]), false, "row0");
+  assert.equal(dd.canInsertIntoThisRow(<any>rows[1]), true, "row1");
+});

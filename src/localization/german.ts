071590d70adc38b0e18c8a7e78a7a6f75120d579
AndOne1974 <andone@mfga.at>
2024-04-29 16:32:28 +0200

    Update german.ts

--- src/localization/german.ts
+++ src/localization/german.ts
@@ -9,7 +9,7 @@ export var germanSurveyStrings = {
   startSurveyText: "Start",
   otherItemText: "Sonstiges (Bitte angeben)",
   noneItemText: "Nichts trifft zu",
-  refuseItemText: "Verweigern Sie die Antwort",
+  refuseItemText: "Antwort verweigern",
   dontKnowItemText: "Weiß ich nicht",
   selectAllItemText: "Alles auswählen",
   progressText: "Seite {0} von {1}",
@@ -19,7 +19,7 @@ export var germanSurveyStrings = {
   questionsProgressText: "{0}/{1} Fragen beantwortet",
   emptySurvey: "Es sind keine Fragen vorhanden.",
   completingSurvey: "Vielen Dank, dass Sie die Umfrage abgeschlossen haben!",
-  completingSurveyBefore: "Aus unseren Unterlagen geht hervor, dass Sie diese Umfrage bereits abgeschlossen haben.",
+  completingSurveyBefore: "Sie haben diese Umfrage bereits abgeschlossen.",
   loadingSurvey: "Umfrage wird geladen...",
   placeholder: "Bitte auswählen...",
   ratingOptionsCaption: "Tippen Sie hier, um zu bewerten...",
@@ -69,7 +69,7 @@ export var germanSurveyStrings = {
   savingData: "Die Ergebnisse werden auf dem Server gespeichert...",
   savingDataError: "Es ist ein Fehler aufgetreten. Die Ergebnisse konnten nicht gespeichert werden.",
   savingDataSuccess: "Die Ergebnisse wurden gespeichert!",
-  savingExceedSize: "Ihre Antwort überschreitet 64 KB. Reduzieren Sie die Größe Ihrer Datei(en) und versuchen Sie es erneut, oder wenden Sie sich an einen Umfragebesitzer.",
+  savingExceedSize: "Ihre Antwort überschreitet 64 KB. Reduzieren Sie die Größe Ihrer Datei(en) und versuchen Sie es erneut, oder wenden Sie sich an den Umfragebesitzer.",
   saveAgainButton: "Erneut absenden",
   timerMin: "Min.",
   timerSec: "Sek.",
@@ -85,7 +85,7 @@ export var germanSurveyStrings = {
   chooseFileCaption: "Datei auswählen",
   takePhotoCaption: "Foto machen",
   photoPlaceholder: "Klicken Sie auf die Schaltfläche unten, um ein Foto mit der Kamera aufzunehmen.",
-  fileOrPhotoPlaceholder: "Ziehen Sie eine Datei per Drag & Drop oder wählen Sie sie aus, um sie hochzuladen oder ein Foto mit der Kamera aufzunehmen.",
+  fileOrPhotoPlaceholder: "Ziehen Sie eine Datei per Drag & Drop oder wählen Sie sie aus, um sie hochzuladen oder machen Sie ein Foto mit der Kamera.",
   replaceFileCaption: "Datei ersetzen",
   removeFileCaption: "Datei löschen",
   booleanCheckedLabel: "Ja",
@@ -95,15 +95,15 @@ export var germanSurveyStrings = {
   questionTitlePatternText: "Fragentitel",
   modalCancelButtonText: "Abbrechen",
   modalApplyButtonText: "Anwenden",
-  filterStringPlaceholder: "Tippe um zu suchen...",
+  filterStringPlaceholder: "Tippen Sie, um zu suchen...",
   emptyMessage: "Es gibt noch keine Daten.",
   noEntriesText: "Es gibt noch keine Einträge.\nKlicken Sie auf die Schaltfläche unten, um einen neuen Eintrag hinzuzufügen.",
   noEntriesReadonlyText: "Es gibt keine Einträge.",
   more: "Mehr",
-  tagboxDoneButtonCaption: "OKAY",
+  tagboxDoneButtonCaption: "OK",
   selectToRankEmptyRankedAreaText: "Alle Auswahlmöglichkeiten sind in einer Rangfolge angeordnet",
   selectToRankEmptyUnrankedAreaText: "Ziehen Sie die Auswahl hierher, um sie zu ordnen",
-  ok: "OKAY",
+  ok: "OK",
   cancel: "Abbrechen"
 };
 
@@ -112,20 +112,3 @@ surveyLocalization.localeNames["de"] = "deutsch";
 
 // The following strings have been translated by a machine translation service
 // Remove those strings that you have corrected manually
-// panelDynamicTabTextFormat: "Panel {panelIndex}" => "Bereich {panelIndex}"
-// noEntriesReadonlyText: "There are no entries." => "Es gibt keine Einträge."
-// tagboxDoneButtonCaption: "OK" => "OKAY"
-// selectToRankEmptyRankedAreaText: "All choices are ranked" => "Alle Auswahlmöglichkeiten sind in einer Rangfolge angeordnet"
-// selectToRankEmptyUnrankedAreaText: "Drag and drop choices here to rank them" => "Ziehen Sie die Auswahl hierher, um sie zu ordnen"// takePhotoCaption: "Take Photo" => "Foto machen"
-// photoPlaceholder: "Click the button below to take a photo using the camera." => "Klicken Sie auf die Schaltfläche unten, um ein Foto mit der Kamera aufzunehmen."
-// fileOrPhotoPlaceholder: "Drag and drop or select a file to upload or take a photo using the camera." => "Ziehen Sie eine Datei per Drag & Drop oder wählen Sie sie aus, um sie hochzuladen oder ein Foto mit der Kamera aufzunehmen."
-// replaceFileCaption: "Replace file" => "Datei ersetzen"// eachRowUniqueError: "Each row must have a unique value." => "Jede Zeile muss einen eindeutigen Wert haben."
-// noUploadFilesHandler: "Files cannot be uploaded. Please add a handler for the 'onUploadFiles' event." => "Dateien können nicht hochgeladen werden. Fügen Sie einen Handler für das Ereignis 'onUploadFiles' hinzu."
-// showDetails: "Show Details" => "Details anzeigen"
-// hideDetails: "Hide Details" => "Details ausblenden"
-// ok: "OK" => "OKAY"
-// cancel: "Cancel" => "Abbrechen"
-// refuseItemText: "Refuse to answer" => "Verweigern Sie die Antwort"
-// dontKnowItemText: "Don't know" => "Weiß ich nicht"
-// savingExceedSize: "Your response exceeds 64KB. Please reduce the size of your file(s) and try again or contact a survey owner." => "Ihre Antwort überschreitet 64 KB. Reduzieren Sie die Größe Ihrer Datei(en) und versuchen Sie es erneut, oder wenden Sie sich an einen Umfragebesitzer."
-// signaturePlaceHolderReadOnly: "No signature" => "Keine Unterschrift"

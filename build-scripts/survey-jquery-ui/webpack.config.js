ef661e87f7ec243423727b5c61d1f99350672ed7
dmitrykurmanov <kurmanov.work@gmail.com>
2024-05-10 17:51:17 +0400

    work for the https://github.com/surveyjs/survey-library/issues/8245

---
+++ build-scripts/survey-jquery-ui/webpack.config.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const webpackCommonConfigCreator = require("../webpack.common");
+const DtsGeneratorPlugin = require("../webpack-dts-generator");
+const { merge } = require("webpack-merge");
+var packageJson = require("./package.json");
+var path = require("path");
+
+const config = {
+  entry: {
+    survey: path.resolve(__dirname, "../../src/main.scss"),
+    modern: path.resolve(__dirname, "../../src/modern.scss"),
+    defaultV2: path.resolve(__dirname, "../../src/defaultV2-theme/defaultV2.scss")
+  },
+  resolve: {
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",     // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime"
+    },
+    extensions: ['.ts', '.tsx']
+  },
+  externals: {
+    jquery: {
+      root: "jQuery",
+      commonjs2: "jquery",
+      commonjs: "jquery",
+      amd: "jquery"
+    }
+  },
+  plugins: [
+    new DtsGeneratorPlugin({
+      tsConfigPath: "./build-scripts/survey-jquery-ui/tsconfig.typing.jquery.json",
+      filePath: "build/survey-jquery/survey.jquery-ui.d.ts",
+      moduleName: "survey-jquery-ui",
+      importName: "entries/jquery-ui"
+    }),
+  ],
+};
+
+module.exports = function (options) {
+  options.platform = "jquery-ui";
+  options.libraryName = "SurveyJquery";
+  return merge(webpackCommonConfigCreator(options, packageJson, "survey.jquery.ui"), config);
+}

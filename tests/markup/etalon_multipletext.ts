3fb9a42256194488b908f6b3c767b6283689dd56
Aleksey Novikov <novikov82@gmail.com>
2024-05-16 10:45:11 +0300

    #8209 - add multipletext tests

--- tests/markup/etalon_multipletext.ts
+++ tests/markup/etalon_multipletext.ts
@@ -1,5 +1,5 @@
 import { registerMarkupTests } from "./helper";
-import { StylesManager } from "survey-core";
+import { StylesManager, settings } from "survey-core";
 
 registerMarkupTests(
   [
@@ -35,6 +35,76 @@ registerMarkupTests(
         ]
       },
       snapshot: "multipletext",
+    },
+    {
+      name: "Test mutlipletext readonly",
+      json: {
+        questions: [
+          {
+            type: "multipletext",
+            name: "multipletext",
+            title: "Multipletext",
+            titleLocation: "hidden",
+            readOnly: true,
+            colCount: 2,
+            items: [
+              {
+                name: "item1",
+                title: "Text 1"
+              },
+              {
+                name: "item2",
+                title: "Text 2"
+              },
+              {
+                name: "item3",
+                title: "Text 3"
+              },
+              {
+                name: "item4",
+                title: "Text 4"
+              },
+            ],
+          },
+        ]
+      },
+      snapshot: "multipletext-readonly",
+    },
+    {
+      name: "Test mutlipletext disabled",
+      json: {
+        questions: [
+          {
+            type: "multipletext",
+            name: "multipletext",
+            title: "Multipletext",
+            titleLocation: "hidden",
+            colCount: 2,
+            items: [
+              {
+                name: "item1",
+                title: "Text 1"
+              },
+              {
+                name: "item2",
+                title: "Text 2"
+              },
+              {
+                name: "item3",
+                title: "Text 3"
+              },
+              {
+                name: "item4",
+                title: "Text 4"
+              },
+            ],
+          },
+        ]
+      },
+      initSurvey: (survey) => survey.setDesignMode(true),
+      before: () => { settings.supportCreatorV2 = true; },
+      after: () => { settings.supportCreatorV2 = false; },
+      snapshot: "multipletext-disabled",
     },
     {
       name: "Test mutlipletext question markup error top",

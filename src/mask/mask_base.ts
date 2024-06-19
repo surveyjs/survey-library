029c171e5f46c29d27b9ad0ce27a3a6d636acfff
tsv2013 <tsv2013@noreply.github.com>
2024-06-19 11:29:21 +0300

    Fixed build

--- src/mask/mask_base.ts
+++ src/mask/mask_base.ts
@@ -1,4 +1,5 @@
 import { Base } from "../base";
+import { ISurvey } from "../base-interfaces";
 import { JsonObject, Serializer, property } from "../jsonobject";
 import { IInputMask, IMaskedInputResult, ITextInputParams } from "./mask_utils";
 
@@ -40,7 +41,7 @@ export class InputMaskBase extends Base implements IInputMask {
     const properties = Serializer.getProperties(this.getType());
     properties.forEach(property => {
       const currentValue = (this as any)[property.name];
-      if(!property.isDefaultValue(currentValue)) {
+      if (!property.isDefaultValue(currentValue)) {
         res[property.name] = currentValue;
       }
     });
@@ -62,7 +63,7 @@ Serializer.addClass(
   [
     {
       name: "saveMaskedValue:boolean",
-      visibleIf: function(obj: any) {
+      visibleIf: function (obj: any) {
         if (!obj) return false;
         return obj.getType() !== "masksettings";
       },

4355d2a1545adaa1f51d3690fb194a2aabae9827
OlgaLarina <olga.larina.dev@gmail.com>
2024-07-09 18:33:00 +0300

    work for #8526 The value is reset when using the input type "phone" and the pattern mask

--- src/mask/input_element_adapter.ts
+++ src/mask/input_element_adapter.ts
@@ -40,6 +40,11 @@ export class InputElementAdapter {
     }
   };
 
+  changeHandler = (event: any) => {
+    const result = this.inputMaskInstance.processInput({ prevValue: "", insertedChars: event.target.value, selectionStart: 0, selectionEnd: 0 });
+    this.inputElement.value = result.value;
+  }
+
   public createArgs(event: any): ITextInputParams {
     const args: ITextInputParams = {
       insertedChars: event.data,
@@ -67,6 +72,7 @@ export class InputElementAdapter {
       this.inputElement.addEventListener("beforeinput", this.beforeInputHandler);
       this.inputElement.addEventListener("click", this.clickHandler);
       this.inputElement.addEventListener("focus", this.clickHandler);
+      this.inputElement.addEventListener("change", this.changeHandler);
     }
   }
   public removeInputEventListener(): void {
@@ -74,6 +80,7 @@ export class InputElementAdapter {
       this.inputElement.removeEventListener("beforeinput", this.beforeInputHandler);
       this.inputElement.removeEventListener("click", this.clickHandler);
       this.inputElement.removeEventListener("focus", this.clickHandler);
+      this.inputElement.removeEventListener("change", this.changeHandler);
     }
   }
   public dispose(): void {

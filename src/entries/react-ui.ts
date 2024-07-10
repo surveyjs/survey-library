0d22b178fac0e2847ed40d2046b0a4148f58d612
tsv2013 <tsv2013@noreply.github.com>
2024-07-10 09:35:58 +0300

    Fixed entry files

--- src/entries/react-ui.ts
+++ src/entries/react-ui.ts
@@ -2,7 +2,4 @@ export * from "./react-ui-model";
 export * from "./core-export";
 export { SurveyModel as Model } from "survey-core";
 
-export * from "../utils/responsivity-manager";
-export { unwrap } from "../utils/utils";
-
 import { checkLibraryVersion } from "survey-core";

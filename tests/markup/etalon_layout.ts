138b29ba9b76db7f335244bcb063c152b900639a
OlgaLarina <olga.larina.dev@gmail.com>
2024-07-18 16:55:22 +0300

    work for #5178 fix markup tests

---
+++ tests/markup/etalon_layout.ts
@@ -0,0 +1,120 @@
+import { StylesManager } from "survey-core";
+import { registerMarkupTest } from "./helper";
+
+registerMarkupTest(
+  {
+    name: "Layout mode - start with new line with panel",
+    json: {
+      "elements": [
+        {
+          type: "html",
+          name: "question0",
+          html: "HTML1",
+          title: "Question title",
+          titleLocation: "hidden"
+        },
+        {
+          type: "panel",
+          name: "name",
+          showQuestionNumbers: "off",
+          startWithNewLine: false,
+          elements: [
+            {
+              type: "html",
+              name: "question1",
+              html: "HTML2",
+              title: "Question title",
+              titleLocation: "hidden"
+            }
+          ]
+        }
+      ]
+    },
+    before: () => StylesManager.applyTheme("defaultV2"),
+    after: () => StylesManager.applyTheme("default"),
+    event: "onAfterRenderPage",
+    snapshot: "layout-panel-page-swnl-v2",
+  });
+
+registerMarkupTest(
+  {
+    name: "Layout mode - start with new line",
+    json: {
+      "elements": [
+        {
+          type: "html",
+          name: "question0",
+          html: "HTML1",
+          title: "Question title",
+          titleLocation: "hidden"
+        },
+        {
+          type: "html",
+          name: "question1",
+          html: "HTML2",
+          title: "Question title",
+          titleLocation: "hidden",
+          startWithNewLine: false
+        }
+      ]
+    },
+    before: () => StylesManager.applyTheme("defaultV2"),
+    after: () => StylesManager.applyTheme("default"),
+    event: "onAfterRenderPage",
+    snapshot: "layout-page-swnl-v2",
+  });
+
+registerMarkupTest(
+  {
+    name: "Layout mode - start with new line with panel and titles",
+    json: {
+      "elements": [
+        {
+          type: "text",
+          name: "question0",
+          title: "Question title",
+        },
+        {
+          type: "panel",
+          name: "name",
+          showQuestionNumbers: "off",
+          startWithNewLine: false,
+          elements: [
+            {
+              type: "text",
+              name: "question1",
+              title: "Question title",
+            }
+          ]
+        }
+      ]
+    },
+    before: () => StylesManager.applyTheme("defaultV2"),
+    after: () => StylesManager.applyTheme("default"),
+    event: "onAfterRenderPage",
+    snapshot: "layout-panel-page-swnl-title-v2",
+  });
+
+registerMarkupTest(
+  {
+    name: "Layout mode - start with new line Title",
+    json: {
+      "elements": [
+        {
+          type: "text",
+          name: "question0",
+          title: "Question title"
+        },
+        {
+          type: "text",
+          name: "question1",
+          title: "Question title",
+          startWithNewLine: false
+        }
+      ]
+    },
+    before: () => StylesManager.applyTheme("defaultV2"),
+    after: () => StylesManager.applyTheme("default"),
+    event: "onAfterRenderPage",
+    snapshot: "layout-page-swnl-title-v2",
+  });

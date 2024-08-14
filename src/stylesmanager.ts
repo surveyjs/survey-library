e3ff67970c3408b14937bd52b7e1ec3aa0309d6e
Dmitry Kuzin <dk981234@gmail.com>
2024-08-14 12:22:43 +0400

    Fix auto apply theme doesnt work

--- src/stylesmanager.ts
+++ src/stylesmanager.ts
@@ -1,5 +1,6 @@
+import { SurveyModel } from "../packages/survey-core/src/survey";
 import { surveyCss } from "../packages/survey-core/src/defaultCss/defaultV2Css";
-import { DomDocumentHelper } from "../packages/survey-core/src/global_variables_utils";
+import { DomDocumentHelper, DomWindowHelper } from "../packages/survey-core/src/global_variables_utils";
 import { settings, ISurveyEnvironment } from "../packages/survey-core/src/settings";
 import { getElement, isShadowDOM, Logger } from "../packages/survey-core/src/utils/utils";
 
@@ -414,4 +415,9 @@ export class StylesManager {
       }
     }
   }
+}
+if(DomWindowHelper.isAvailable()) {
+  SurveyModel.prototype["onBeforeCreating"] = () => {
+    StylesManager.autoApplyTheme();
+  };
 }

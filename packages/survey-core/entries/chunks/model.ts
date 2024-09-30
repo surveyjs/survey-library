4f1da163a7e1273d50c0e0decfdbef7aa6a37ed4
dmitrykurmanov <kurmanov.work@gmail.com>
2024-09-30 13:00:54 +0400

    work for the https://github.com/surveyjs/private-tasks/issues/386

--- packages/survey-core/entries/chunks/model.ts
+++ packages/survey-core/entries/chunks/model.ts
@@ -287,7 +287,8 @@ export {
   prepareElementForVerticalAnimation,
   cleanHtmlElementAfterAnimation,
   classesToSelector,
-  IAttachKey2clickOptions
+  IAttachKey2clickOptions,
+  renamedIcons
 } from "../../src/utils/utils";
 export { InputMaskBase } from "../../src/mask/mask_base";
 export { InputMaskPattern } from "../../src/mask/mask_pattern";

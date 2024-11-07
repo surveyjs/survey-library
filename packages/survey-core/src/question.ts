e06cb1db22a493a6682f585dff00430aeea294f7
Andrew Telnov <andrew.telnov@gmail.com>
2024-11-07 11:15:37 +0200

    Perform optimizations for loading survey with a lot of dynamic panels

--- packages/survey-core/src/question.ts
+++ packages/survey-core/src/question.ts
@@ -1534,12 +1534,13 @@ export class Question extends SurveyElement<Question>
   protected onSetData(): void {
     super.onSetData();
     if (!this.survey) return;
-    this.initDataFromSurvey();
-    this.onSurveyValueChanged(this.value);
-    this.updateValueWithDefaults();
     this.onIndentChanged();
-    this.updateQuestionCss();
-    this.updateIsAnswered();
+    if(!this.isDesignMode) {
+      this.initDataFromSurvey();
+      this.onSurveyValueChanged(this.value);
+      this.updateValueWithDefaults();
+      this.updateIsAnswered();
+    }
   }
   protected initDataFromSurvey(): void {
     if (!!this.data) {

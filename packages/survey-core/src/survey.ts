c96dbdc99ad070ff41261a276c612b13acc55a21
Aleksey Novikov <novikov82@gmail.com>
2024-11-13 15:21:03 +0300

    #9004 do not raise undefined callback Fixes #9004

--- packages/survey-core/src/survey.ts
+++ packages/survey-core/src/survey.ts
@@ -5370,7 +5370,7 @@ export class SurveyModel extends SurveyElementCore
             SurveyElement.ScrollElementToViewCore(this.rootElement as any, false, scrollIfVisible, scrollIntoViewOptions, onScolledCallback);
           } else {
             this.rootElement.scrollTop = 0;
-            onScolledCallback();
+            if (onScolledCallback) onScolledCallback();
           }
         } else {
           SurveyElement.ScrollElementToTop(options.elementId, scrollIfVisible, scrollIntoViewOptions, onScolledCallback);

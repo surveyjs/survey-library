e7df8515600ef82b1415589b06c0cd80ef36d649
Andrew Telnov <andrew.telnov@gmail.com>
2024-11-23 12:53:15 +0200

    Add unit test

--- packages/survey-core/tests/helperstests.ts
+++ packages/survey-core/tests/helperstests.ts
@@ -1,4 +1,4 @@
-import { Helpers } from "../src/helpers";
+import { Helpers, createDate } from "../src/helpers";
 import { EmailValidator } from "../src/validator";
 import { SurveyModel } from "../src/survey";
 import { ProcessValue } from "../src/conditionProcessValue";
@@ -584,4 +584,22 @@ QUnit.test("compareVersions", function(assert) {
   assert.equal(Helpers.compareVerions("1.2.3", "1"), 1, "#9");
   assert.equal(Helpers.compareVerions("1.2", "1.2.3"), -1, "#10");
   assert.equal(Helpers.compareVerions("1.2.3", "1.2"), 1, "#11");
+});
+QUnit.test("createDate & settings.onDateCreated", function(assert) {
+  assert.equal(createDate("#1", "2024-10-10").getDate(), 10, "#1");
+  let func_val: any = "";
+  let func_reason = "";
+  settings.onDateCreated = (newDate, reason, val) => {
+    func_val = val;
+    func_reason = reason;
+    newDate.setDate(newDate.getDate() + 1);
+    return newDate;
+  };
+  assert.equal(createDate("#2", "2024-10-10").getDate(), 11, "#2");
+  assert.equal(func_val, "2024-10-10", "val");
+  assert.equal(func_reason, "#2", "reason");
+  settings.onDateCreated = (date, reason, val) => {
+    return date;
+  };
+  assert.equal(createDate("#3", "2024-10-10").getDate(), 10, "#3");
 });

b75b8e38edf383148504b4e194b760fbaef12ea4
Dmitry Kuzin <dk981234@gmail.com>
2024-12-02 19:29:15 +0400

    Fix js ui functional tests

--- testCafe/questions/dropdown.js
+++ testCafe/questions/dropdown.js
@@ -2267,6 +2267,9 @@ frameworks.forEach((framework) => {
     let surveyResult = await getSurveyResult();
     await t.expect(surveyResult).eql({ q1: { row1: { col1: "other", "col1-Comment": "ABC" } } });
   });
+});
+frameworks.forEach((framework) => {
+  fixture`${framework} ${title}`.page`${url}${framework}`;
   test("Check item title changes when locale is changed", async (t) => {
     await initSurvey(framework, {
       locale: "de",

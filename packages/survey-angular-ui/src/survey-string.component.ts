1c3908d6a91af4225dce05815e921afb86c3758b
OlgaLarina <olga.larina.dev@gmail.com>
2024-12-19 12:35:49 +0300

    refactoring list item template

--- packages/survey-angular-ui/src/survey-string.component.ts
+++ packages/survey-angular-ui/src/survey-string.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from "@angular/core";
 import { LocalizableString } from "survey-core";
+import { EmbeddedViewContentComponent } from "./embedded-view-content.component";
 
 @Component(
   {
     selector: "sv-ng-string, '[sv-ng-string]'",
-    template: "<ng-template [component]='{ name: model.renderAs, data: { model: model.renderAsData } }'></ng-template>"
+    template: "<ng-template [component]='{ name: model.renderAs, data: { model: model.renderAsData } }'></ng-template>",
+    styleUrls: ["./hide-host.scss"]
   }
 )
-export class SurveyStringComponent {
+export class SurveyStringComponent extends EmbeddedViewContentComponent {
   @Input() model!: LocalizableString;
 }

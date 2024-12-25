316b17c7c6611ee9ae3f8f4a009cddc7c21db94a
Dmitry Kuzin <dk981234@gmail.com>
2024-12-25 18:30:08 +0400

    Remove src dir from root

--- src/entries/react-ui.ts
+++
@@ -1,6 +0,0 @@
-import { checkLibraryVersion } from "survey-core";
-
-export * from "../../packages/survey-react-ui/entries/react-ui-model";
-export { SurveyModel, SurveyWindowModel, SurveyModel as Model, settings, ISurveyEnvironment, surveyLocalization, surveyStrings } from "survey-core";
-
-checkLibraryVersion(`${process.env.VERSION}`, "survey-react-ui");

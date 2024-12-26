588a1a80e8624808ef1571cf0c1441c628a822f0
Dmitry Kuzin <dk981234@gmail.com>
2024-12-26 16:41:09 +0400

    Fix all types f test

--- testCafe/questions/allTypes.js
+++ testCafe/questions/allTypes.js
@@ -1,4 +1,4 @@
-import { frameworks, url_test, initSurvey, getSurveyResult, getDynamicPanelRemoveButton, applyTheme, getListItemByText } from "../helper";
+import { frameworks, url, initSurvey, getSurveyResult, getDynamicPanelRemoveButton, getListItemByText } from "../helper";
 import { Selector, fixture, test } from "testcafe";
 const title = "allTypes";
 
@@ -148,159 +148,155 @@ var json = {
   ],
 };
 
-["modern", "bootstrap"].forEach(theme => {
-  frameworks.forEach(framework => {
-    fixture`${framework} ${title} ${theme}`
-      .page`${url_test}${theme}/${framework}`.beforeEach(async t => {
-      await applyTheme(theme);
-      await initSurvey(framework, json);
-    });
-    test("check survey will all types", async t => {
-      await t
-        .expect(Selector(".sv-string-editor").exists)
-        .notOk("There should not be any editable elements outside design mode");
+frameworks.forEach(framework => {
+  fixture`${framework} ${title}`
+    .page`${url}${framework}`.beforeEach(async t => {
+    await initSurvey(framework, json);
+  });
+  test("check survey will all types", async t => {
+    await t
+      .expect(Selector(".sv-string-editor").exists)
+      .notOk("There should not be any editable elements outside design mode");
 
-      await t.typeText(
-        Selector("span").withText("text_question")
-          .parent("[data-name]")
-          .find("input"),
-        "test text"
-      );
-
-      await t.click(
-        Selector("span").withText("checkbox_question")
-          .parent("[data-name]")
-          .find("span")
-          .withText("item1")
-      );
-
-      await t.click(
-        Selector("span").withText("radiogroup_question")
-          .parent("[data-name]")
-          .find("span")
-          .withText("item1")
-      );
-
-      await t
-        .click(Selector("div[aria-label='dropdown_question']"))
-        .click(getListItemByText("item1"));
-
-      await t.typeText(
-        Selector("span").withText("comment_question")
-          .parent("[data-name]")
-          .find("textarea"),
-        "test comment"
-      );
-
-      await t.click(
-        Selector("span").withText("rating_question")
-          .parent("[data-name]")
-          .find("span")
-          .withText("3")
-      );
-
-      await t.click(
-        Selector("span").withText("imagepicker_question")
-          .parent("[data-name]")
-          .find("img")
-          .withAttribute("alt", "item1")
-          .parent()
-      );
-
-      await t.click(
-        Selector("span").withText("boolean_question")
-          .parent("[data-name]")
-          .find("span")
-          .withText("Yes")
-      );
-
-      await t.hover(Selector("[data-name='image_question']").find("img"));
-
-      await t.expect(Selector(".sjs-html-question").innerText).eql("html text");
-
-      await t.click(
-        Selector("span").withText("signature_question")
-          .parent("[data-name]")
-          .find("button")
-          .withAttribute("title", "Clear")
-      );
-
-      await t.hover(
-        Selector("span").withText("expression_question")
-          .parent("[data-name]")
-          .find("div")
-          .withText("1")
-      );
-
-      await t.click(
-        Selector("span").withText("file_question")
-          .parent("[data-name]")
-          .find("button")
-          .withText("Clear")
-          .filterVisible()
-      );
-
-      await t.click(
-        Selector("span").withText("matrix_question")
-          .parent("[data-name]")
-          .find("tr")
-          .withText("Row 1")
-          .find("input[type='radio']")
-          .parent()
-      );
-
-      const matrixDropdownRow = Selector("span").withText("matrixdropdown_question")
+    await t.typeText(
+      Selector("span").withText("text_question")
         .parent("[data-name]")
-        .find("tr").withText("Row 1");
-      await t
-        .click(matrixDropdownRow.find("div[aria-label='row Row 1, column Column 1']"))
-        .click(getListItemByText("1"));
+        .find("input"),
+      "test text"
+    );
 
-      await t.click(
-        Selector("span").withText("matrixdynamic_question")
-          .parent("[data-name]")
-          .find("span")
-          .withText("Remove")
-      );
+    await t.click(
+      Selector("span").withText("checkbox_question")
+        .parent("[data-name]")
+        .find("span")
+        .withText("item1")
+    );
 
-      await t.typeText(
-        Selector("span").withText("multipletext_question")
-          .parent("[data-name]")
-          .find("input"),
-        "test multiple text"
-      );
+    await t.click(
+      Selector("span").withText("radiogroup_question")
+        .parent("[data-name]")
+        .find("span")
+        .withText("item1")
+    );
 
-      await t.click(Selector("span").withText("panel_title"));
+    await t
+      .click(Selector("div[aria-label='dropdown_question']"))
+      .click(getListItemByText("item1"));
 
-      await t.click(getDynamicPanelRemoveButton("paneldynamic", "Remove"));
+    await t.typeText(
+      Selector("span").withText("comment_question")
+        .parent("[data-name]")
+        .find("textarea"),
+      "test comment"
+    );
 
-      // const rankItem1 = Selector("span").withText('ranking_question')
-      //   .parent("[data-name]")
-      //   .find("div")W
-      //   .withText("item1");
+    await t.click(
+      Selector("span").withText("rating_question")
+        .parent("[data-name]")
+        .find("span")
+        .withText("3")
+    );
 
-      // const rankItem2 = Selector("span").withText('ranking_question')
-      //   .parent("[data-name]")
-      //   .find("div")
-      //   .withText("item2");
-      // await t.dragToElement(rankItem1, rankItem2, { destinationOffsetY: -1, speed: 0.1 });
+    await t.click(
+      Selector("span").withText("imagepicker_question")
+        .parent("[data-name]")
+        .find("img")
+        .withAttribute("alt", "item1")
+        .parent()
+    );
 
-      await t.click("input[value=Complete]");
+    await t.click(
+      Selector("span").withText("boolean_question")
+        .parent("[data-name]")
+        .find("span")
+        .withText("Yes")
+    );
 
-      const surveyResult = await getSurveyResult();
-      await t.expect(surveyResult.text_question).eql("test text");
-      await t.expect(surveyResult.checkbox_question).eql(["item1"]);
-      await t.expect(surveyResult.radiogroup_question).eql("item1");
-      await t.expect(surveyResult.dropdown_question).eql("item1");
-      await t.expect(surveyResult.comment_question).eql("test comment");
-      await t.expect(surveyResult.rating_question).eql(3);
-      await t.expect(surveyResult.imagepicker_question).eql("item1");
-      await t.expect(surveyResult.boolean_question).eql(true);
-      await t.expect(surveyResult.expression_question).eql(1);
-      await t.expect(surveyResult.matrix_question).eql({ "Row 1": "Column 1", });
-      await t.expect(surveyResult.matrixdropdown_question).eql({ "Row 1": { "Column 1": 1, }, });
-      await t.expect(surveyResult.multipletext_question).eql({ text1: "test multiple text", });
-      // await t.expect(surveyResult.ranking_question).eql(["item2", "item1"]);
-    });
+    await t.hover(Selector("[data-name='image_question']").find("img"));
+
+    await t.expect(Selector(".sjs-html-question").innerText).eql("html text");
+
+    await t.click(
+      Selector("span").withText("signature_question")
+        .parent("[data-name]")
+        .find("button")
+        .withAttribute("title", "Clear")
+    );
+
+    await t.hover(
+      Selector("span").withText("expression_question")
+        .parent("[data-name]")
+        .find("div")
+        .withText("1")
+    );
+
+    await t.click(
+      Selector("span").withText("file_question")
+        .parent("[data-name]")
+        .find("button")
+        .withText("Clear")
+        .filterVisible()
+    );
+
+    await t.click(
+      Selector("span").withText("matrix_question")
+        .parent("[data-name]")
+        .find("tr")
+        .withText("Row 1")
+        .find("input[type='radio']")
+        .parent()
+    );
+
+    const matrixDropdownRow = Selector("span").withText("matrixdropdown_question")
+      .parent("[data-name]")
+      .find("tr").withText("Row 1");
+    await t
+      .click(matrixDropdownRow.find("div[aria-label='row Row 1, column Column 1']"))
+      .click(getListItemByText("1"));
+    await t.click(
+      Selector("span").withText("matrixdynamic_question")
+        .parent("[data-name]")
+        .find("button")
+        .withAttribute("title", "Remove")
+    );
+
+    await t.typeText(
+      Selector("span").withText("multipletext_question")
+        .parent("[data-name]")
+        .find("input"),
+      "test multiple text"
+    );
+
+    await t.click(Selector("span").withText("panel_title"));
+
+    await t.click(getDynamicPanelRemoveButton("paneldynamic", "Remove"));
+
+    // const rankItem1 = Selector("span").withText('ranking_question')
+    //   .parent("[data-name]")
+    //   .find("div")W
+    //   .withText("item1");
+
+    // const rankItem2 = Selector("span").withText('ranking_question')
+    //   .parent("[data-name]")
+    //   .find("div")
+    //   .withText("item2");
+    // await t.dragToElement(rankItem1, rankItem2, { destinationOffsetY: -1, speed: 0.1 });
+
+    await t.click("input[value=Complete]");
+
+    const surveyResult = await getSurveyResult();
+    await t.expect(surveyResult.text_question).eql("test text");
+    await t.expect(surveyResult.checkbox_question).eql(["item1"]);
+    await t.expect(surveyResult.radiogroup_question).eql("item1");
+    await t.expect(surveyResult.dropdown_question).eql("item1");
+    await t.expect(surveyResult.comment_question).eql("test comment");
+    await t.expect(surveyResult.rating_question).eql(3);
+    await t.expect(surveyResult.imagepicker_question).eql("item1");
+    await t.expect(surveyResult.boolean_question).eql(true);
+    await t.expect(surveyResult.expression_question).eql(1);
+    await t.expect(surveyResult.matrix_question).eql({ "Row 1": "Column 1", });
+    await t.expect(surveyResult.matrixdropdown_question).eql({ "Row 1": { "Column 1": 1, }, });
+    await t.expect(surveyResult.multipletext_question).eql({ text1: "test multiple text", });
+    // await t.expect(surveyResult.ranking_question).eql(["item2", "item1"]);
   });
 });

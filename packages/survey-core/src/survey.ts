56252ab782cb9cacb3ad181641ed605eeeaf8826
Andrew <andrew.telnov@gmail.com>
2025-01-09 08:50:22 +0000

    "questionsOnPageMode": "questionPerPage" doesn't work correctly with skip trigger fix #9276 (#9277)

--- packages/survey-core/src/survey.ts
+++ packages/survey-core/src/survey.ts
@@ -7933,6 +7933,9 @@ export class SurveyModel extends SurveyElementCore
     const isNeedWaitForPageRendered = this.activePage !== question.page && !question.page.isStartPage;
     if (isNeedWaitForPageRendered) {
       this.currentPage = <PageModel>question.page;
+      if(this.isSingleVisibleQuestion && !this.isDesignMode) {
+        this.currentSingleQuestion = question;
+      }
     }
     if (!isNeedWaitForPageRendered) {
       this.focusQuestionInfo();

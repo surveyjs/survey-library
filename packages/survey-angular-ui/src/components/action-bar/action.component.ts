02135dc6ce472935edc8953cdddef3dbac1b2b0e
Dmitry <dk981234@gmail.com>
2025-01-13 20:32:45 +0400

    Refactor adaptive action container (#9298)
    * Start work for new responsivity behavior
    
    * Work
    
    * Implement new way of calc action dimensions
    
    * Implement in vue3
    
    * Small refactor
    
    * Implement in angular + refactor container refs
    
    * reset dimension for only one action which title has changed
    
    * Revert style changes for a while
    
    * Remove test.only
    
    * Fix unit tests
    
    * Small refactor
    
    * Fix incorrect keys for actions in angular
    
    * Fix action mode is not updated correctly
    
    * Fix react super.componentWillUnmount is not called for action
    
    * Fix action bar item in React <= 17
    
    * Reset sv-action--hidden class before measure
    
    * Refactor disableShrink and removePriority behavior
    
    * Remove removePriority from action
    
    * Add tests on fit method with gap
    
    * Unify markup tests between frameworks
    
    * Fix js-ui item update dimension works incorrectly
    
    * Update vrt etalon

--- packages/survey-angular-ui/src/components/action-bar/action.component.ts
+++ packages/survey-angular-ui/src/components/action-bar/action.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input, ViewChild, ViewContainerRef } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Input, ViewChild, ViewContainerRef } from "@angular/core";
 import { BaseAngular } from "../../base-angular";
 import { AngularComponentFactory } from "../../component-factory";
+import { Action } from "survey-core";
 
 @Component({
   selector: "sv-ng-action",
   templateUrl: "./action.component.html",
   styles: [":host { display: none; }"],
 })
-export class ActionComponent extends BaseAngular {
-  @Input() model: any;
-  @ViewChild("actionContent", { read: ViewContainerRef, static: true }) actionContent!: ViewContainerRef;
+export class ActionComponent extends BaseAngular implements AfterViewInit {
+  @Input() model!: Action;
+  @ViewChild("container") containerRef!: ElementRef<HTMLElement>;
   getModel() {
     return this.model;
   }
@@ -18,6 +19,17 @@ export class ActionComponent extends BaseAngular {
   }
   public get id() {
     return this.model.id || '';
+  }
+  public override ngOnDestroy(): void {
+    super.ngOnDestroy();
+    this.model.updateModeCallback = undefined as any;
+  }
+  public ngAfterViewInit(): void {
+    this.model.updateModeCallback = (mode, callback) => {
+      this.model.mode = mode;
+      callback(mode, this.containerRef?.nativeElement)
+    }
+    this.model.afterRender();
   }
 }
 

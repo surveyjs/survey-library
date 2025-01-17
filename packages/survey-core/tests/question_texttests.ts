e4669cfb398e7c267fb6998cb55c9ce64167fa8e
Andrew Telnov <andrew.telnov@gmail.com>
2025-01-17 17:19:42 +0200

    Add test cases #9268

--- packages/survey-core/tests/question_texttests.ts
+++ packages/survey-core/tests/question_texttests.ts
@@ -585,11 +585,16 @@ QUnit.test("Support mask in displayValue, #9268", function (assert) {
       { type: "text", name: "q2", title: "Title: {q1}" }
     ]
   });
-  const q1 = survey.getQuestionByName("q1");
+  const q1 = <QuestionTextModel>survey.getQuestionByName("q1");
   const q2 = survey.getQuestionByName("q2");
   assert.equal(q1.displayValue, "", "displayValue #1");
   assert.equal(q2.locTitle.textOrHtml, "Title: ", "title #1");
   q1.value = 1234567.3;
   assert.equal(q1.displayValue, "1 234 567,3", "displayValue #2");
   assert.equal(q2.locTitle.textOrHtml, "Title: 1 234 567,3", "title #2");
+  assert.equal(q1.getDisplayValue(false, 1234), "1 234", "getDisplayValue with value #1");
+  q1.maskSettings.saveMaskedValue = true;
+  assert.equal(q1.getDisplayValue(false, 1234), "1 234", "getDisplayValue with value #2");
+  assert.equal(q1.value, 1234567.3, "Have the same value");
+  assert.equal(q1.inputValue, "1 234 567,3", "Have the same input value");
 });

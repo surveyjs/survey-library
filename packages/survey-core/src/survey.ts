a4e7959d8b7e654dfed743b0b27a30c629339189
Dmitry Kurmanov <kurmanov.work@gmail.com>
2025-01-27 18:22:18 +0400

    work for the https://github.com/surveyjs/survey-library/issues/9349 (#9355)

--- packages/survey-core/src/survey.ts
+++ packages/survey-core/src/survey.ts
@@ -5333,7 +5333,7 @@ export class SurveyModel extends SurveyElementCore
     if (this.onAfterRenderQuestionInput.isEmpty) return;
     let id = (<Question>question).inputId;
     const { root } = settings.environment;
-    if (!!id && htmlElement.id !== id && typeof root !== "undefined") {
+    if (!!id && (!htmlElement || htmlElement.id !== id) && typeof root !== "undefined") {
       let el = root.getElementById(id);
       if (!!el) {
         htmlElement = el;

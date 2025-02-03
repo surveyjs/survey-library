4a5e52477ddbd4ff0fac0dfa0e7681f75e3d638d
Andrew Telnov <andrew.telnov@gmail.com>
2025-02-03 12:58:33 +0200

    The other value and comment in a Composite Question stores incorrectly fix #9378

--- packages/survey-core/src/question_custom.ts
+++ packages/survey-core/src/question_custom.ts
@@ -22,6 +22,7 @@ import { LocalizableString } from "./localizablestring";
 import { SurveyError } from "./survey-error";
 import { CustomError } from "./error";
 import { ConsoleWarnings } from "./console-warnings";
+import { settings } from "./settings";
 
 /**
  * An interface used to create custom question types.
@@ -1244,6 +1245,31 @@ export class QuestionCompositeModel extends QuestionCustomModelBase {
     this.settingNewValue = false;
     this.runPanelTriggers(QuestionCompositeModel.ItemVariableName + "." + name, newValue);
   }
+  setComment(name: string, newValue: string, locNotification: any): any {
+    let val = this.getUnbindValue(this.value);
+    const commentName = this.getCommentName(name);
+    if(!val && !newValue || !!newValue && !!val && val[commentName] === newValue) return;
+    if(!!newValue) {
+      if(!val) { val = {}; }
+      val[commentName] = newValue;
+    } else {
+      delete val[commentName];
+    }
+    const q = <Question>this.getQuestionByName(name);
+    if(!!q && q.comment !== newValue) {
+      q.comment = newValue;
+    }
+    this.value = val;
+  }
+  getComment(name: string): string {
+    const q = <Question>this.getQuestionByName(name);
+    if(!!q) return q.comment;
+    const val = this.value;
+    return !!val && val[this.getCommentName(name)] || "";
+  }
+  private getCommentName(name: string): string {
+    return name + settings.commentSuffix;
+  }
   private runPanelTriggers(name: string, value: any): void {
     if(!!this.contentPanel) {
       this.contentPanel.questions.forEach(q => {
@@ -1321,17 +1347,22 @@ export class QuestionCompositeModel extends QuestionCustomModelBase {
     super.setQuestionValue(newValue, updateIsAnswered);
   }
   private setValuesIntoQuestions(newValue: any): void {
-    if(!this.contentPanel) return;
+    if(!this.contentPanel || this.settingNewValue) return;
     newValue = this.getValueForContentPanel(newValue);
     const oldSettingNewValue = this.settingNewValue;
     this.settingNewValue = true;
     const questions = this.contentPanel.questions;
     for (var i = 0; i < questions.length; i++) {
-      const key = questions[i].getValueName();
-      const val = !!newValue ? newValue[key] : undefined;
       const q = questions[i];
+      const key = q.getValueName();
+      const commentKey = this.getCommentName(key);
+      const val = !!newValue ? newValue[key] : undefined;
+      const commentVal = !!newValue && newValue[commentKey] || "";
       if(!this.isTwoValueEquals(q.value, val) && (val !== undefined || !q.isEmpty())) {
         q.value = val;
+      }
+      if(q.comment !== commentVal) {
+        q.comment = commentVal;
       }
     }
     this.settingNewValue = oldSettingNewValue;

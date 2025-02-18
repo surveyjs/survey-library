84405899ce7d540dc14cbce9941b4ead7f4d61ea
Aleksey Novikov <novikov82@gmail.com>
2025-02-18 14:17:29 +0300

    #9465 After creating from JSON (duplicate in Creator) renderedIsCollapsed does not update Fixes #9465 (#9470)
    * #9465 After creating from JSON (duplicate in Creator) renderedIsCollapsed does not update Fixes #9465
    
    * fix preact #9465

--- packages/survey-core/src/survey-element.ts
+++ packages/survey-core/src/survey-element.ts
@@ -389,7 +389,7 @@ export class SurveyElement<E = any> extends SurveyElementCore implements ISurvey
   }
   public set state(val: string) {
     this.setPropertyValue("state", val);
-    this.renderedIsExpanded = !(this.state === "collapsed" && !this.isDesignMode);
+    this.renderedIsExpanded = !this.isCollapsed;
   }
   protected notifyStateChanged(prevState: string): void {
     if (this.survey) {
@@ -549,7 +549,9 @@ export class SurveyElement<E = any> extends SurveyElementCore implements ISurvey
       this.resetDescriptionVisibility();
       this.clearCssClasses();
     }
-    this._renderedIsExpanded = !this.isCollapsed;
+    this.blockAnimations();
+    this.renderedIsExpanded = !this.isCollapsed;
+    this.releaseAnimations();
   }
   protected canRunConditions(): boolean {
     return super.canRunConditions() && !!this.data;

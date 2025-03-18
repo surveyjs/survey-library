3d41366dedea657424d88cecb0a9fb19db2c09b1
dmitrykurmanov <kurmanov.work@gmail.com>
2025-03-18 13:23:30 +0400

    work for the https://github.com/surveyjs/survey-library/issues/9023

--- packages/survey-react-ui/src/reactquestion_range_slider.tsx
+++ packages/survey-react-ui/src/reactquestion_range_slider.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { SurveyQuestionElementBase } from "./reactquestion_element";
-import { QuestionRangeSliderModel } from "survey-core";
+import { Base, QuestionRangeSliderModel } from "survey-core";
 import { ReactQuestionFactory } from "./reactquestion_factory";
 
 export class SurveyQuestionRangeSlider extends SurveyQuestionElementBase {
@@ -8,10 +8,25 @@ export class SurveyQuestionRangeSlider extends SurveyQuestionElementBase {
     return this.questionBase as QuestionRangeSliderModel;
   }
 
+  protected getStateElement(): Base {
+    return this.question;
+  }
+
+  protected handleOnChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const newValue: number = +event.target.value;
+    if (newValue >= 70) return;
+    this.question.value = [newValue, 100];
+  }
+
+  private getValue() {
+    const value = this.question.value[0];
+    return value ?? 0;
+  }
+
   protected renderElement(): React.JSX.Element {
     return (
       <div className={this.question.rootCss} ref={(div) => (this.setControl(div))}>
-        Range Slider
+        <input type="range" min="0" max="100" value={this.getValue()} step="1" onChange={this.handleOnChange}/>
       </div>
     );
   }

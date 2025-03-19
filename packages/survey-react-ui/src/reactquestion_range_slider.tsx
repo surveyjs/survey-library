64dfdba3bca2582ccbc98034609e98b95ad820cb
dmitrykurmanov <kurmanov.work@gmail.com>
2025-03-19 13:03:36 +0400

    work for the https://github.com/surveyjs/survey-library/issues/9023

--- packages/survey-react-ui/src/reactquestion_range_slider.tsx
+++ packages/survey-react-ui/src/reactquestion_range_slider.tsx
@@ -12,21 +12,32 @@ export class SurveyQuestionRangeSlider extends SurveyQuestionElementBase {
     return this.question;
   }
 
-  protected handleOnChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  protected handleOnChange1 = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const newValue: number = +event.target.value;
-    if (newValue >= 70) return;
-    this.question.value = [newValue, 100];
+    this.question.value.splice(0, 1, newValue);
   }
 
-  private getValue() {
+  protected handleOnChange2 = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const newValue: number = +event.target.value;
+    this.question.value.splice(1, 1, newValue);
+  }
+
+  private getValue1() {
     const value = this.question.value[0];
+    return value ?? 0;
+  }
+
+  private getValue2() {
+    const value = this.question.value[1];
     return value ?? 0;
   }
 
   protected renderElement(): React.JSX.Element {
     return (
       <div className={this.question.rootCss} ref={(div) => (this.setControl(div))}>
-        <input type="range" min="0" max="100" value={this.getValue()} step="1" onChange={this.handleOnChange}/>
+        {/* <input type="range" min="0" max="100" value={this.getValue1()} step="1" onInput={this.handleOnInput1}/> */}
+        <input type="range" value={this.getValue1()} min={0} max={100} step="1" onChange={this.handleOnChange1} />
+        <input type="range" value={this.getValue2()} min={0} max={100} step="1" onChange={this.handleOnChange2} />
       </div>
     );
   }

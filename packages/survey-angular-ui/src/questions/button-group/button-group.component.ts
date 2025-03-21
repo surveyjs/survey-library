fce52cb7877a9c9648fde5d3fd29eab005c85580
Dmitry Kuzin <dk981234@gmail.com>
2025-03-21 20:11:04 +0400

    Fix buttongroup and svg icons a11y errors

--- packages/survey-angular-ui/src/questions/button-group/button-group.component.ts
+++ packages/survey-angular-ui/src/questions/button-group/button-group.component.ts
@@ -2,6 +2,7 @@
 import { Component } from "@angular/core";
 import { QuestionAngular } from "../../question";
 import { QuestionButtonGroupModel } from "survey-core";
+import { AngularComponentFactory } from "../../component-factory";
 
 @Component({
   selector: "sv-ng-buttongroup-question",
@@ -9,3 +10,4 @@ import { QuestionButtonGroupModel } from "survey-core";
 })
 export class ButtonGroupQuestionComponent extends QuestionAngular<QuestionButtonGroupModel> {
 }
+AngularComponentFactory.Instance.registerComponent("buttongroup-question", ButtonGroupQuestionComponent);

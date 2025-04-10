57eede8c4ab9ca3ed18cc0b8aba8f4ac3e0b36b2
OlgaLarina <olga.larina.dev@gmail.com>
2025-04-10 16:45:51 +0300

    lint improvement

--- packages/survey-vue3-ui/.eslintrc.cjs
+++ packages/survey-vue3-ui/.eslintrc.cjs
@@ -5,7 +5,6 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
   ],
   rules: {
     "vue/multi-word-component-names": "off",

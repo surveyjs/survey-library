8932626cdb63c3ee35509bd4410f76a368ac9966
dmitrykurmanov <kurmanov.work@gmail.com>
2025-05-03 19:17:57 +0400

    work for the https://github.com/surveyjs/survey-library/issues/9023

---
+++ packages/survey-vue3-ui/src/Slider.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :class="question.rootCss" ref="root">
+        <input name="range-input" ref="rangeInputRef" :class="question.cssClasses.input" type="range" :min="question.min" 
+        :max="question.max" :step="question.step" tabIndex="-1" 
+        @change="(e)=>{question.handleRangeOnChange(e as InputEvent)}"
+        @pointerdown="(e)=>{question.handleRangePointerDown(e as PointerEvent)}"
+        @pointerup="(e)=>{question.handleRangePointerUp(e as PointerEvent, root as HTMLElement)}" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type { QuestionSliderModel } from "survey-core";
+import { useQuestion } from "./base";
+import { ref } from "vue";
+defineOptions({
+  inheritAttrs: false,
+});
+const props = defineProps<{ question: QuestionSliderModel }>();
+const root = ref<HTMLElement | null>(null);
+useQuestion<QuestionSliderModel>(props, root);
+</script>

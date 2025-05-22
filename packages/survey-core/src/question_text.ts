d012c54dbb857024fef59f42c44aeb4336fed4e7
dmitrykurmanov <kurmanov.work@gmail.com>
2025-05-22 12:00:09 +0400

    work for the https://github.com/surveyjs/private-tasks/issues/490

--- packages/survey-core/src/question_text.ts
+++ packages/survey-core/src/question_text.ts
@@ -256,7 +256,7 @@ export class QuestionTextModel extends QuestionTextBase {
    * A value passed on to the [`autocomplete`](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete) attribute of the underlying `<input>` element.
    */
   public get autocomplete(): string {
-    return this.getPropertyValue("autocomplete", null);
+    return this.getPropertyValue("autocomplete");
   }
   public set autocomplete(val: string) {
     this.setPropertyValue("autocomplete", val);

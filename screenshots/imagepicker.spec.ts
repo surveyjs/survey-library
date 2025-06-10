e186895f5ae6f3b00b9129130493f9ae249da13b
OlgaLarina <olga.larina.developer@gmail.com>
2025-06-10 14:11:35 +0300

    playwriпре Image Screenshot (#10018)
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

---
+++ screenshots/imagepicker.spec.ts
@@ -0,0 +1,280 @@
+import { test, expect } from "@playwright/test";
+import { frameworks, url, initSurvey, setRowItemFlowDirection, compareScreenshot, resetFocusToBody } from "../e2e/helper";
+import { imageSource } from "../visualRegressionTests/constants";
+
+const title = "Imagepicker Screenshot";
+
+frameworks.forEach(framework => {
+  test.describe(`${framework} ${title}`, () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(`${url}${framework}`);
+    });
+
+    test("Check imagepicker checked item", async ({ page }) => {
+      await page.goto(`${url}${framework}`);
+      await page.setViewportSize({ width: 1920, height: 1500 });
+      await initSurvey(page, framework, {
+        showQuestionNumbers: "off",
+        widthMode: "static",
+        questions: [
+          {
+            type: "imagepicker",
+            name: "choosepicture",
+            title: "Imagepicker",
+            defaultValue: "lion",
+            choices: [{
+              value: "lion",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+            },
+            {
+              value: "giraffe",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
+            },
+            {
+              value: "panda",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+            },
+            {
+              value: "camel",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+            },
+            ]
+          }
+        ]
+      });
+      // await resetFocusToBody();
+      await page.waitForLoadState("networkidle");
+      await compareScreenshot(page, page.locator(".sd-question"), "imagepicker-checked-item.png");
+    });
+
+    test("Check responsive imagepicker", async ({ page }) => {
+      await page.goto(`${url}${framework}`);
+      await page.setViewportSize({ width: 1920, height: 1500 });
+      await initSurvey(page, framework, {
+        showQuestionNumbers: "off",
+        widthMode: "responsive",
+        questions: [
+          {
+            type: "imagepicker",
+            name: "choosepicture",
+            title: "Imagepicker",
+            choices: [{
+              value: "lion",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+            },
+            {
+              value: "giraffe",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
+            },
+            {
+              value: "panda",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+            },
+            {
+              value: "camel",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+            },
+            ]
+          }
+        ]
+      });
+      // await resetFocusToBody();
+      await page.waitForLoadState("networkidle");
+      await compareScreenshot(page, page.locator(".sd-question"), "imagepicker-responsive-max.png");
+      await page.setViewportSize({ width: 700, height: 1500 });
+      await compareScreenshot(page, page.locator(".sd-question"), "imagepicker-responsive-medium.png");
+      await page.setViewportSize({ width: 500, height: 1500 });
+      await compareScreenshot(page, page.locator(".sd-question"), "imagepicker-responsive-min.png");
+    });
+
+    test("Check image picker responsive, colCount !== 0", async ({ page }) => {
+      await page.goto(`${url}${framework}`);
+      await setRowItemFlowDirection(page);
+      await page.setViewportSize({ width: 1920, height: 1500 });
+      await initSurvey(page, framework, {
+        showQuestionNumbers: "off",
+        widthMode: "responsive",
+        questions: [
+          {
+            type: "imagepicker",
+            name: "choosepicture",
+            title: "Imagepicker",
+            colCount: 3,
+            choices: [{
+              value: "lion",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+            },
+            {
+              value: "giraffe",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
+            },
+            {
+              value: "panda",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+            },
+            {
+              value: "camel",
+              imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+            },
+            ]
+          }
+        ]
+      });
+      // await resetFocusToBody();
+      await page.waitForLoadState("networkidle");
+      await compareScreenshot(page, page.locator(".sd-question"), "imagepicker-responsive-col-count-3.png");
+      await page.evaluate(() => { (window as any).survey.getAllQuestions()[0].colCount = 1; });
+      await compareScreenshot(page, page.locator(".sd-question"), "imagepicker-responsive-col-count-1.png");
+    });
+
+    test("Check image picker question", async ({ page }) => {
+      await page.goto(`${url}${framework}`);
+      await page.setViewportSize({ width: 1920, height: 1080 });
+      await initSurvey(page, framework, {
+        showQuestionNumbers: "on",
+        "elements": [
+          {
+            "type": "imagepicker",
+            "name": "question2",
+            "choices": [
+              {
+                "value": "lion",
+                "imageLink": imageSource
+              },
+              "item1"
+            ],
+            imageWidth: 200,
+            imageHeight: 150,
+            "imageFit": "cover"
+          }
+        ]
+      });
+      await page.waitForLoadState("networkidle");
+
+      const questionRoot = page.locator(".sd-imagepicker");
+      await compareScreenshot(page, questionRoot, "imagepicker-question.png");
+    });
+
+    test("Check image picker loading is broken", async ({ page }) => {
+      await page.goto(`${url}${framework}`);
+      await page.setViewportSize({ width: 800, height: 600 });
+      await initSurvey(page, framework, {
+        showQuestionNumbers: "on",
+        questions: [
+          {
+            "type": "imagepicker",
+            "name": "question1",
+            "choices": [
+              {
+                "value": "Image 1",
+                "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+              },
+              {
+                "value": "Image 2",
+              },
+              {
+                "value": "Image 3",
+                "imageLink": "https://surveyjs.io/Cos/image-picker/panda.jpg"
+              },
+              {
+                "value": "Image 4",
+                "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+              }
+            ]
+          }
+        ]
+      });
+      await page.waitForLoadState("networkidle");
+
+      const questionRoot = page.locator(".sd-imagepicker");
+      await compareScreenshot(page, questionRoot, "imagepicker-not-load.png");
+    });
+
+    test("Check image picker columns and long label text", async ({ page }) => {
+      await page.goto(`${url}${framework}`);
+      await setRowItemFlowDirection(page);
+      await page.setViewportSize({ width: 1920, height: 1080 });
+      await initSurvey(page, framework, {
+        "logoPosition": "right",
+        "pages": [
+          {
+            "name": "page1",
+            "elements": [
+              {
+                "type": "imagepicker",
+                "name": "best_picture",
+                "title": "Which movie do you believe should have won the Academy Award for Best Picture?",
+                "hideNumber": true,
+                "choices": [
+                  {
+                    "value": "movie_1",
+                    "text": "The Holdovers",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_2",
+                    "text": "American Fiction",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_3",
+                    "text": "Oppenheimer",
+                    "enableIf": "{trailer-follow-up} = 3",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_4",
+                    "text": "Barbie",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_5",
+                    "text": "Poor Things",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_6",
+                    "text": "Past Lives",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_7",
+                    "text": "Killers of the Flower Moon",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_8",
+                    "text": "Maestro",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_9",
+                    "text": "Anatomy of a Fall",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "movie_10",
+                    "text": "The Zone of Interest",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  }
+                ],
+                "colCount": 5,
+                "imageHeight": 220,
+                "imageWidth": 220,
+                "showLabel": true,
+                "multiSelect": true
+              },
+            ]
+          }
+        ],
+        "showQuestionNumbers": "off",
+        "width": "1000",
+        "widthMode": "static"
+      });
+      await page.waitForLoadState("networkidle");
+
+      const questionRoot = page.locator(".sd-imagepicker");
+      await compareScreenshot(page, questionRoot, "imagepicker-question-columns-long-label.png");
+    });
+  });
+});
--- visualRegressionTests/tests/defaultV2/image.ts
+++ visualRegressionTests/tests/defaultV2/image.ts
@@ -1,103 +1,103 @@
-import { Selector, ClientFunction } from "testcafe";
-import { url, frameworks, initSurvey, wrapVisualTest, takeElementScreenshot } from "../../helper";
-import { imageSource } from "../../constants";
+// import { Selector, ClientFunction } from "testcafe";
+// import { url, frameworks, initSurvey, wrapVisualTest, takeElementScreenshot } from "../../helper";
+// import { imageSource } from "../../constants";
 
-const title = "Image Screenshot";
+// const title = "Image Screenshot";
 
-fixture`${title}`.page`${url}`.beforeEach(async (t) => {
+// fixture`${title}`.page`${url}`.beforeEach(async (t) => {
 
-});
+// });
 
-frameworks.forEach(framework => {
-  fixture`${framework} ${title}`
-    .page`${url}${framework}`;
-  test("Check image question", async (t) => {
-    await wrapVisualTest(t, async (t, comparer) => {
-      await t.resizeWindow(1920, 1080);
-      await initSurvey(framework, {
-        showQuestionNumbers: "on",
-        widthMode: "responsive",
-        questions: [
-          {
-            type: "image",
-            name: "image_question",
-            imageWidth: "1024px",
-            imageHeight: "465px",
-            minWidth: "1024px",
-            maxWidth: "1024px",
-            width: "1024px",
-            imageLink: imageSource
-          },
-        ]
-      });
-      const questionRoot = Selector(".sd-question--image");
-      await takeElementScreenshot("image-question.png", questionRoot, t, comparer);
-    });
-  });
-  test("Check image in-row question", async (t) => {
-    await wrapVisualTest(t, async (t, comparer) => {
-      await t.resizeWindow(800, 600);
-      await initSurvey(framework, {
-        showQuestionNumbers: "on",
-        questions: [
-          {
-            type: "image",
-            name: "image_question",
-            imageWidth: "200px",
-            imageHeight: "100px",
-            imageLink: imageSource
-          },
-          {
-            type: "image",
-            name: "image_question",
-            imageWidth: "200px",
-            imageHeight: "100px",
-            imageLink: imageSource,
-            startWithNewLine: false
-          },
-        ]
-      });
-      const questionRoot = Selector(".sd-row");
-      await takeElementScreenshot("image-question-row.png", questionRoot, t, comparer);
-    });
-  });
-  test("Check image loading is broken", async (t) => {
-    await wrapVisualTest(t, async (t, comparer) => {
-      await t.resizeWindow(800, 600);
-      await initSurvey(framework, {
-        showQuestionNumbers: "on",
-        questions: [
-          {
-            "type": "image",
-            "name": "noimage",
-            "imageLink": "https://surveyjs.io/Cos/image-picker/panda.jpg"
-          }
-        ]
-      });
-      const questionRoot = Selector(".sd-question--image");
-      await takeElementScreenshot("image-not-load.png", questionRoot, t, comparer);
-    });
-  });
-  test("Check image question responsive width when image is located in row with multiple elements", async (t) => {
-    await wrapVisualTest(t, async (t, comparer) => {
-      await t.resizeWindow(800, 600);
-      await initSurvey(framework, {
-        showQuestionNumbers: "on",
-        "questions": [
-          {
-            "type": "text",
-            "name": "q1",
-          },
-          {
-            "type": "image",
-            "name": "q2",
-            "imageLink": "http://localhost:8080/testCafe/resources/small_Dashka.jpg",
-            "startWithNewLine": false
-          }
-        ]
-      });
-      const questionRoot = Selector(".sd-row");
-      await takeElementScreenshot("image-in-row.png", questionRoot, t, comparer);
-    });
-  });
-});
+// frameworks.forEach(framework => {
+//   fixture`${framework} ${title}`
+//     .page`${url}${framework}`;
+//   test("Check image question", async (t) => {
+//     await wrapVisualTest(t, async (t, comparer) => {
+//       await t.resizeWindow(1920, 1080);
+//       await initSurvey(framework, {
+//         showQuestionNumbers: "on",
+//         widthMode: "responsive",
+//         questions: [
+//           {
+//             type: "image",
+//             name: "image_question",
+//             imageWidth: "1024px",
+//             imageHeight: "465px",
+//             minWidth: "1024px",
+//             maxWidth: "1024px",
+//             width: "1024px",
+//             imageLink: imageSource
+//           },
+//         ]
+//       });
+//       const questionRoot = Selector(".sd-question--image");
+//       await takeElementScreenshot("image-question.png", questionRoot, t, comparer);
+//     });
+//   });
+//   test("Check image in-row question", async (t) => {
+//     await wrapVisualTest(t, async (t, comparer) => {
+//       await t.resizeWindow(800, 600);
+//       await initSurvey(framework, {
+//         showQuestionNumbers: "on",
+//         questions: [
+//           {
+//             type: "image",
+//             name: "image_question",
+//             imageWidth: "200px",
+//             imageHeight: "100px",
+//             imageLink: imageSource
+//           },
+//           {
+//             type: "image",
+//             name: "image_question",
+//             imageWidth: "200px",
+//             imageHeight: "100px",
+//             imageLink: imageSource,
+//             startWithNewLine: false
+//           },
+//         ]
+//       });
+//       const questionRoot = Selector(".sd-row");
+//       await takeElementScreenshot("image-question-row.png", questionRoot, t, comparer);
+//     });
+//   });
+//   test("Check image loading is broken", async (t) => {
+//     await wrapVisualTest(t, async (t, comparer) => {
+//       await t.resizeWindow(800, 600);
+//       await initSurvey(framework, {
+//         showQuestionNumbers: "on",
+//         questions: [
+//           {
+//             "type": "image",
+//             "name": "noimage",
+//             "imageLink": "https://surveyjs.io/Cos/image-picker/panda.jpg"
+//           }
+//         ]
+//       });
+//       const questionRoot = Selector(".sd-question--image");
+//       await takeElementScreenshot("image-not-load.png", questionRoot, t, comparer);
+//     });
+//   });
+//   test("Check image question responsive width when image is located in row with multiple elements", async (t) => {
+//     await wrapVisualTest(t, async (t, comparer) => {
+//       await t.resizeWindow(800, 600);
+//       await initSurvey(framework, {
+//         showQuestionNumbers: "on",
+//         "questions": [
+//           {
+//             "type": "text",
+//             "name": "q1",
+//           },
+//           {
+//             "type": "image",
+//             "name": "q2",
+//             "imageLink": "http://localhost:8080/testCafe/resources/small_Dashka.jpg",
+//             "startWithNewLine": false
+//           }
+//         ]
+//       });
+//       const questionRoot = Selector(".sd-row");
+//       await takeElementScreenshot("image-in-row.png", questionRoot, t, comparer);
+//     });
+//   });
+// });

fa5471e6660781eb1b482d4401200e443d13b2df
OlgaLarina <olga.larina.developer@gmail.com>
2025-06-10 21:54:18 +0300

    playwright Boolean Screenshot (#10024)
    * playwright Boolean Screenshot
    
    * playwright Boolean Screenshot
    
    ---------
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- screenshots/boolean.spec.ts
+++ screenshots/boolean.spec.ts
@@ -1,76 +1,79 @@
-import { frameworks, url, initSurvey } from "../e2e/helper";
-import { QuestionBoolean, QuestionRadiogroup } from "../e2e/questionHelper";
 import { test, expect } from "@playwright/test";
-import { Survey } from "../e2e/surveyHelper";
+import { frameworks, url, initSurvey, compareScreenshot, resetFocusToBody } from "../e2e/helper";
 
 const title = "Boolean Screenshot";
 
-frameworks.forEach((framework) => {
-  test.describe(title + " - " + framework, () => {
+frameworks.forEach(framework => {
+  test.describe(`${framework} ${title}`, () => {
     test.beforeEach(async ({ page }) => {
       await page.goto(`${url}${framework}`);
     });
+
     test("Check boolean question", async ({ page }) => {
       await page.setViewportSize({ width: 1920, height: 1080 });
       await initSurvey(page, framework, {
-        questions: [
-          {
-            type: "boolean",
-            name: "boolean_question",
-          },
-        ]
+        showQuestionNumbers: "on",
+        questions: [{ type: "boolean", name: "boolean_question", },]
       });
-      const question = new QuestionBoolean(page, "boolean_question");
-      await question.resetFocusToBody();
-      await question.toHaveScreenshot("boolean-question-indeterminate.png");
+      const questionRoot = page.locator(".sd-question--boolean");
+      await page.waitForTimeout(1000);
+      await resetFocusToBody(page);
+      await compareScreenshot(page, questionRoot, "boolean-question-indeterminate.png");
 
-      await question.hover(".sd-boolean__thumb-ghost");
-      await question.toHaveScreenshot("boolean-question-indeterminate-hovered.png");
+      await page.locator(".sd-boolean__thumb-ghost").first().hover();
+      await compareScreenshot(page, questionRoot, "boolean-question-indeterminate-hovered.png");
 
-      question.clickThumb(false);
-      await question.toHaveScreenshot("boolean-question-clicked.png");
+      await page.locator(".sv-string-viewer").filter({ hasText: "No" }).click();
+      await compareScreenshot(page, questionRoot, "boolean-question-clicked.png");
 
-      await question.hover(question.question.locator(".sd-boolean__thumb-ghost").nth(1));
-      await question.toHaveScreenshot("boolean-question-clicked-hovered.png");
+      await page.locator("span").filter({ hasText: "Yes" }).first().hover();
+      await compareScreenshot(page, questionRoot, "boolean-question-clicked-hovered.png");
 
-      await question.setPropertyValue("readOnly", true);
-      await question.toHaveScreenshot("boolean-question-clicked-disabled.png");
+      await page.evaluate(() => {
+        window["survey"].getQuestionByName("boolean_question").readOnly = true;
+      });
+      await compareScreenshot(page, questionRoot, "boolean-question-clicked-disabled.png");
 
-      await question.setPropertyValue("value", undefined);
-      await question.toHaveScreenshot("boolean-question-disabled.png");
+      await page.evaluate(() => {
+        window["survey"].getQuestionByName("boolean_question").value = null;
+      });
+      await compareScreenshot(page, questionRoot, "boolean-question-disabled.png");
     });
+
     test("Check boolean question - interchange buttons", async ({ page }) => {
       await page.setViewportSize({ width: 1600, height: 800 });
       await initSurvey(page, framework, {
-        questions: [
-          {
-            type: "boolean",
-            name: "boolean_question",
-            swapOrder: true
-          }
-        ]
+        showQuestionNumbers: "on",
+        questions: [{ type: "boolean", name: "boolean_question", swapOrder: true },] });
+      const questionRoot = page.locator(".sd-question--boolean");
+      await page.waitForTimeout(1000);
+      await resetFocusToBody(page);
+      await compareScreenshot(page, questionRoot, "boolean-question-exch-indeterminate.png");
+
+      await page.locator(".sd-boolean__thumb-ghost").first().hover();
+      await compareScreenshot(page, questionRoot, "boolean-question-exch-indeterminate-hovered.png");
+
+      await page.locator(".sv-string-viewer").filter({ hasText: "No" }).click();
+      await compareScreenshot(page, questionRoot, "boolean-question-exch-clicked.png");
+
+      await page.locator("span").filter({ hasText: "Yes" }).first().hover();
+      await compareScreenshot(page, questionRoot, "boolean-question-exch-clicked-hovered.png");
+
+      await page.evaluate(() => {
+        window["survey"].getQuestionByName("boolean_question").readOnly = true;
       });
-      const question = new QuestionBoolean(page, "boolean_question");
-      await question.resetFocusToBody();
-      await question.toHaveScreenshot("boolean-question-exch-indeterminate.png");
+      await compareScreenshot(page, questionRoot, "boolean-question-exch-clicked-disabled.png");
 
-      await question.hover(".sd-boolean__thumb-ghost");
-      await question.toHaveScreenshot("boolean-question-exch-indeterminate-hovered.png");
-      question.clickThumb(true);
-      await question.toHaveScreenshot("boolean-question-exch-clicked.png");
-
-      await question.hover(question.question.locator(".sd-boolean__thumb-ghost").nth(0));
-      await question.toHaveScreenshot("boolean-question-exch-clicked-hovered.png");
-
-      await question.setPropertyValue("readOnly", true);
-      await question.toHaveScreenshot("boolean-question-exch-clicked-disabled.png");
-
-      await question.setPropertyValue("value", undefined);
-      await question.toHaveScreenshot("boolean-question-exch-disabled.png");
+      await page.evaluate(() => {
+        window["survey"].getQuestionByName("boolean_question").value = null;
+      });
+      await compareScreenshot(page, questionRoot, "boolean-question-exch-disabled.png");
     });
+
     test("Check radio boolean question", async ({ page }) => {
       await page.setViewportSize({ width: 1920, height: 1080 });
       await initSurvey(page, framework, {
+        showQuestionNumbers: "on",
         width: "900px",
         questions: [
           {
@@ -83,20 +86,23 @@ frameworks.forEach((framework) => {
           },
         ]
       });
-      const question = new QuestionBoolean(page, "boolean_question");
-      question.screenShortOptions = { maxDiffPixels: 50 };
-      await question.resetFocusToBody();
-      await question.toHaveScreenshot("boolean-radio-question.png");
+      const questionRoot = page.locator(".sd-question--boolean");
+      await page.waitForTimeout(1000);
+      await compareScreenshot(page, questionRoot, "boolean-radio-question.png");
 
-      await question.clickItemByText("No");
-      await question.toHaveScreenshot("boolean-radio-question-clicked.png");
+      await page.locator(".sv-string-viewer").filter({ hasText: "No" }).click();
+      await expect(page.locator("input[type=radio]").first()).toBeChecked();
+      await compareScreenshot(page, questionRoot, "boolean-radio-question-clicked.png");
 
-      await page.locator("body").click({ position: { x: 1, y: 1 } });
-      await question.toHaveScreenshot("boolean-radio-question-unfocused.png");
+      await page.click("body", { position: { x: 0, y: 400 } });
+      await expect(page.locator("input[type=radio]").first()).not.toBeFocused();
+      await compareScreenshot(page, questionRoot, "boolean-radio-question-unfocused.png");
     });
+
     test("Check boolean question word-wrap", async ({ page }) => {
       await page.setViewportSize({ width: 1920, height: 1080 });
       await initSurvey(page, framework, {
+        showQuestionNumbers: "on",
         focusFirstQuestionAutomatic: true,
         questions: [
           {
@@ -105,16 +111,18 @@ frameworks.forEach((framework) => {
             "labelTrue": "On-site",
             "labelFalse": "Remote",
             defaultValue: true
-          }
+          },
         ]
       });
-      const question = new QuestionBoolean(page, "boolean_question");
-      await question.resetFocusToBody();
-      await question.toHaveScreenshot("boolean-question-word-wrap.png");
+      const questionRoot = page.locator(".sd-question--boolean");
+      await page.waitForTimeout(1000);
+      await compareScreenshot(page, questionRoot, "boolean-question-word-wrap.png");
     });
+
     test("Check boolean thumb position", async ({ page }) => {
       await page.setViewportSize({ width: 1400, height: 800 });
       await initSurvey(page, framework, {
+        showQuestionNumbers: "on",
         "logoPosition": "right",
         "pages": [
           {
@@ -138,14 +146,16 @@ frameworks.forEach((framework) => {
           }
         ]
       });
-      const question1 = new QuestionBoolean(page, "boolean1");
-      const question2 = new QuestionBoolean(page, "boolean2");
-      await question1.toHaveScreenshot("boolean-switch-thumb-right.png");
-      await question2.toHaveScreenshot("boolean-switch-thumb-right-swapped.png");
+      const questionRoot = page.locator(".sd-question--boolean");
+      await page.waitForTimeout(1000);
+      await compareScreenshot(page, questionRoot, "boolean-switch-thumb-right.png");
+      await compareScreenshot(page, questionRoot.nth(1), "boolean-switch-thumb-right-swapped.png");
     });
+
     test("Check boolean swapOrder thumb position", async ({ page }) => {
       await page.setViewportSize({ width: 1400, height: 800 });
       await initSurvey(page, framework, {
+        showQuestionNumbers: "on",
         "elements": [
           {
             type: "boolean",
@@ -154,12 +164,42 @@ frameworks.forEach((framework) => {
           },
         ]
       });
-      const question = new QuestionBoolean(page, "boolean1");
-      await question.toHaveScreenshot("boolean-switch-thumb-swapped-indeterminate.png");
-      question.clickItemByText("No");
-      await question.toHaveScreenshot("boolean-switch-thumb-swapped-no.png");
-      question.clickItemByText("Yes");
-      await question.toHaveScreenshot("boolean-switch-thumb-swapped-yes.png");
+      const questionRoot = page.locator(".sd-question--boolean");
+      await page.waitForTimeout(1000);
+      await compareScreenshot(page, questionRoot, "boolean-switch-thumb-swapped-indeterminate.png");
+      await page.locator(".sv-string-viewer").filter({ hasText: "No" }).click();
+      await compareScreenshot(page, questionRoot, "boolean-switch-thumb-swapped-no.png");
+      await page.locator(".sv-string-viewer").filter({ hasText: "Yes" }).click();
+      await compareScreenshot(page, questionRoot, "boolean-switch-thumb-swapped-yes.png");
+    });
+
+    test("Check boolean rtl", async ({ page }) => {
+      await page.setViewportSize({ width: 1920, height: 1080 });
+      await page.evaluate(() => {
+        document.body.setAttribute("dir", "rtl");
+      });
+
+      await initSurvey(page, framework, {
+        showQuestionNumbers: "on",
+        "elements": [
+          {
+            "type": "boolean",
+            "name": "slider",
+            "title": "Are you 21 or older?",
+            "description": "Display mode = Default (Slider)",
+            "valueTrue": "Yes",
+            "valueFalse": "No",
+            "defaultValue": "No"
+          }
+        ]
+      });
+
+      const questionRoot = page.locator(".sd-question--boolean");
+      await compareScreenshot(page, questionRoot, "boolean-question-rtl.png");
+
+      await page.evaluate(() => {
+        document.body.setAttribute("dir", "ltr");
+      });
     });
   });
 });
--- visualRegressionTests/tests/defaultV2/complex.ts
+++ visualRegressionTests/tests/defaultV2/complex.ts
@@ -1,104 +1,104 @@
-import { Selector, ClientFunction } from "testcafe";
-import { url, frameworks, initSurvey, wrapVisualTest, takeElementScreenshot } from "../../helper";
+// import { Selector, ClientFunction } from "testcafe";
+// import { url, frameworks, initSurvey, wrapVisualTest, takeElementScreenshot } from "../../helper";
 
-const title = "Complex Screenshot";
+// const title = "Complex Screenshot";
 
-fixture`${title}`.page`${url}`.beforeEach(async (t) => {
+// fixture`${title}`.page`${url}`.beforeEach(async (t) => {
 
-});
+// });
 
-const json = {
-  showQuestionNumbers: "off",
-  width: "900px",
-  questions: [
-    {
-      type: "Paneldynamic",
-      title: "My Order",
-      name: "order",
-      renderMode: "progressBottom",
-      templateTitle: "{panel.itemName}",
-      panelRemoveText: "Remove Item",
-      panelCount: 5,
-      minWidth: "708px",
-      maxWidth: "708px",
-      width: "708px",
-      templateElements: [
-        {
-          type: "text",
-          name: "itemName",
-          title: "Item Name",
-          defaultValue: "Converse Chuck Taylor"
-        },
-        {
-          type: "text",
-          name: "count",
-          title: "Count",
-          defaultValue: "1",
-          startWithNewLine: false,
-        },
-        {
-          type: "panel",
-          name: "shipping",
-          title: "Shipping",
-          elements: [
-            {
-              type: "radiogroup",
-              name: "delivery",
-              title: "Delivery",
-              choices: ["DHL", "Pony Express", "FedEx"],
-              defaultValue: "FedEx"
-            },
-            {
-              type: "boolean",
-              name: "fast",
-              title: "Would you like to get the order as fast as it possible?",
-              defaultValue: true
-            },
-            {
-              type: "matrix",
-              name: "best_delivery",
-              title: "What’s the best delivery time for you?",
-              "columns": ["Morning", "Afternoon", "Evening"],
-              "rows": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-              defaultValue: {
-                "Monday": "Afternoon",
-                "Tuesday": "Afternoon",
-                "Wednesday": "Morning",
-                "Thursday": "Morning",
-                "Friday": "Evening"
-              }
-            }
-          ]
-        },
-        {
-          type: "panel",
-          name: "blilling",
-          title: "Billing",
-          state: "collapsed",
-          elements: [{ "type": "text", name: "dummy" }]
-        }
-      ]
-    }
-  ]
-};
+// const json = {
+//   showQuestionNumbers: "off",
+//   width: "900px",
+//   questions: [
+//     {
+//       type: "Paneldynamic",
+//       title: "My Order",
+//       name: "order",
+//       renderMode: "progressBottom",
+//       templateTitle: "{panel.itemName}",
+//       panelRemoveText: "Remove Item",
+//       panelCount: 5,
+//       minWidth: "708px",
+//       maxWidth: "708px",
+//       width: "708px",
+//       templateElements: [
+//         {
+//           type: "text",
+//           name: "itemName",
+//           title: "Item Name",
+//           defaultValue: "Converse Chuck Taylor"
+//         },
+//         {
+//           type: "text",
+//           name: "count",
+//           title: "Count",
+//           defaultValue: "1",
+//           startWithNewLine: false,
+//         },
+//         {
+//           type: "panel",
+//           name: "shipping",
+//           title: "Shipping",
+//           elements: [
+//             {
+//               type: "radiogroup",
+//               name: "delivery",
+//               title: "Delivery",
+//               choices: ["DHL", "Pony Express", "FedEx"],
+//               defaultValue: "FedEx"
+//             },
+//             {
+//               type: "boolean",
+//               name: "fast",
+//               title: "Would you like to get the order as fast as it possible?",
+//               defaultValue: true
+//             },
+//             {
+//               type: "matrix",
+//               name: "best_delivery",
+//               title: "What’s the best delivery time for you?",
+//               "columns": ["Morning", "Afternoon", "Evening"],
+//               "rows": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+//               defaultValue: {
+//                 "Monday": "Afternoon",
+//                 "Tuesday": "Afternoon",
+//                 "Wednesday": "Morning",
+//                 "Thursday": "Morning",
+//                 "Friday": "Evening"
+//               }
+//             }
+//           ]
+//         },
+//         {
+//           type: "panel",
+//           name: "blilling",
+//           title: "Billing",
+//           state: "collapsed",
+//           elements: [{ "type": "text", name: "dummy" }]
+//         }
+//       ]
+//     }
+//   ]
+// };
 
-frameworks.forEach(framework => {
-  fixture`${framework} ${title}`
-    .page`${url}${framework}`.beforeEach(async t => {
-  });
-  test("Check complex question", async (t) => {
-    await wrapVisualTest(t, async (t, comparer) => {
-      await t.resizeWindow(1920, 1800);
-      await ClientFunction(() => { (window as any).Survey.surveyLocalization.locales.en.panelDynamicProgressText = "{0} of {1}"; })();
-      await initSurvey(framework, json);
-      await ClientFunction(() => {
-        const panel = (window as any).survey.getQuestionByName("order");
-        panel.currentIndex = 4;
-      })();
+// frameworks.forEach(framework => {
+//   fixture`${framework} ${title}`
+//     .page`${url}${framework}`.beforeEach(async t => {
+//   });
+//   test("Check complex question", async (t) => {
+//     await wrapVisualTest(t, async (t, comparer) => {
+//       await t.resizeWindow(1920, 1800);
+//       await ClientFunction(() => { (window as any).Survey.surveyLocalization.locales.en.panelDynamicProgressText = "{0} of {1}"; })();
+//       await initSurvey(framework, json);
+//       await ClientFunction(() => {
+//         const panel = (window as any).survey.getQuestionByName("order");
+//         panel.currentIndex = 4;
+//       })();
 
-      const questionRoot = Selector(".sd-question");
-      await takeElementScreenshot("complex-question.png", questionRoot, t, comparer);
-    });
-  });
-});
+//       const questionRoot = Selector(".sd-question");
+//       await takeElementScreenshot("complex-question.png", questionRoot, t, comparer);
+//     });
+//   });
+// });
 

2670c4289e4dcf280632832a2c0cec7a4dc84291
Dmitry Kurmanov <kurmanov.work@gmail.com>
2025-06-23 19:59:26 +0400

    PR: Move Ranking Tests to Playwright and Remove TestCafe (#10074)
    * work for the https://github.com/surveyjs/survey-library/issues/9958

--- e2e/helper.ts
+++ e2e/helper.ts
@@ -195,3 +195,15 @@ export const test = baseTest.extend<{page: void, skipJSErrors: boolean}>({
   }
 });
 export { expect };
+
+export async function doDrag({ page, element, target }: { page: Page, element: Locator, target: Locator }):Promise<void> {
+  await element.hover({ force: true });
+  await page.mouse.down();
+  const { x, y, width, height } = await <any>target.boundingBox();
+  await page.mouse.move(x + width / 2, y + height / 2, { steps: 20 });
+}
+
+export async function doDragDrop({ page, element, target }: { page: Page, element: Locator, target: Locator }):Promise<void> {
+  await doDrag({ page, element, target });
+  await page.mouse.up();
+}

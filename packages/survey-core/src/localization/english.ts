d09406fb374df7551dc1046db7bfa0c4397251e9
Andrew Telnov <andrew.telnov@gmail.com>
2025-08-02 17:17:08 +0300

    Remove comments from english file

--- packages/survey-core/src/localization/english.ts
+++ packages/survey-core/src/localization/english.ts
@@ -2,241 +2,123 @@
 // import { setupLocale } from "survey-core";
 
 export var englishStrings = {
-  // "Previous"
   pagePrevText: "Previous",
-  // "Next"
   pageNextText: "Next",
-  // "Complete"
   completeText: "Complete",
-  // "Preview"
   previewText: "Preview",
-  // "Edit"
   editText: "Edit",
-  // "Start"
   startSurveyText: "Start",
-  // "Please leave a comment"
   commentText: "Please leave a comment",
-  // "Other (describe)"
   otherItemText: "Other (describe)",
-  // "None"
   noneItemText: "None",
-  // "Refuse to answer"
   refuseItemText: "Refuse to answer",
-  // "Don't know"
   dontKnowItemText: "Don't know",
-  // "Select All"
   selectAllItemText: "Select All",
-  // "Deselect all"
   deselectAllItemText: "Deselect all",
-  // "Page {0} of {1}"
   progressText: "Page {0} of {1}",
-  // "{0} of {1}"
   indexText: "{0} of {1}",
-  // "{0} of {1}"
   panelDynamicProgressText: "{0} of {1}",
-  // "Panel {panelIndex}"
   panelDynamicTabTextFormat: "Panel {panelIndex}",
-  // "Answered {0}/{1} questions"
   questionsProgressText: "Answered {0}/{1} questions",
-  // "The survey doesn't contain any visible elements."
   emptySurvey: "The survey doesn't contain any visible elements.",
-  // "Thank you for completing the survey"
   completingSurvey: "Thank you for completing the survey",
-  // "You have already completed this survey."
   completingSurveyBefore: "You have already completed this survey.",
-  // "Loading Survey..."
   loadingSurvey: "Loading Survey...",
-  // "Select..."
   placeholder: "Select...",
-  // "Select..."
   ratingOptionsCaption: "Select...",
-  // "Select..."
   buttongroupOptionsCaption: "Select...",
-  // "value"
   value: "value",
-  // "Response required."
   requiredError: "Response required.",
-  // "Response required: answer at least one question."
   requiredErrorInPanel: "Response required: answer at least one question.",
-  // "Response required: answer questions in all rows."
   requiredInAllRowsError: "Response required: answer questions in all rows.",
-  // "Each row must have a unique value."
   eachRowUniqueError: "Each row must have a unique value.",
-  // "The value should be numeric."
   numericError: "The value should be numeric.",
-  // "The value should not be less than {0}"
   minError: "The value should not be less than {0}",
-  // "The value should not be greater than {0}"
   maxError: "The value should not be greater than {0}",
-  // "Numbers are not allowed."
   textNoDigitsAllow: "Numbers are not allowed.",
-  // "Please enter at least {0} character(s)."
   textMinLength: "Please enter at least {0} character(s).",
-  // "Please enter no more than {0} character(s)."
   textMaxLength: "Please enter no more than {0} character(s).",
-  // "Please enter at least {0} and no more than {1} characters."
   textMinMaxLength: "Please enter at least {0} and no more than {1} characters.",
-  // "Please fill in at least {0} row(s)."
   minRowCountError: "Please fill in at least {0} row(s).",
-  // "Please select at least {0} option(s)."
   minSelectError: "Please select at least {0} option(s).",
-  // "Please select no more than {0} option(s)."
   maxSelectError: "Please select no more than {0} option(s).",
-  // "The '{0}' should be at least {1} and at most {2}"
   numericMinMax: "The '{0}' should be at least {1} and at most {2}",
-  // "The '{0}' should be at least {1}"
   numericMin: "The '{0}' should be at least {1}",
-  // "The '{0}' should be at most {1}"
   numericMax: "The '{0}' should be at most {1}",
-  // "Please enter a valid e-mail address."
   invalidEmail: "Please enter a valid e-mail address.",
-  // "The expression: {0} should return 'true'."
   invalidExpression: "The expression: {0} should return 'true'.",
-  // "The request returned error '{0}'. {1}"
   urlRequestError: "The request returned error '{0}'. {1}",
-  // "The request returned empty data or the 'path' property is incorrect"
   urlGetChoicesError: "The request returned empty data or the 'path' property is incorrect",
-  // "The file size should not exceed {0}."
   exceedMaxSize: "The file size should not exceed {0}.",
-  // "Files cannot be uploaded. Please add a handler for the 'onUploadFiles' event."
   noUploadFilesHandler: "Files cannot be uploaded. Please add a handler for the 'onUploadFiles' event.",
-  // "Response required: enter another value."
   otherRequiredError: "Response required: enter another value.",
-  // "Your file is uploading. Please wait several seconds and try again."
   uploadingFile: "Your file is uploading. Please wait several seconds and try again.",
-  // "Loading..."
   loadingFile: "Loading...",
-  // "Choose file(s)..."
   chooseFile: "Choose file(s)...",
-  // "No file selected"
   noFileChosen: "No file selected",
-  // "Drag and drop a file here or click the button below to select a file to upload."
   filePlaceholder: "Drag and drop a file here or click the button below to select a file to upload.",
-  // "Are you sure you want to delete this record?"
   confirmDelete: "Are you sure you want to delete this record?",
-  // "This value should be unique."
   keyDuplicationError: "This value should be unique.",
-  // "Add Column"
   addColumn: "Add Column",
-  // "Add Row"
   addRow: "Add Row",
-  // "Remove"
   removeRow: "Remove",
-  // "There are no rows."
   noRowsText: "There are no rows.",
-  // "Row {rowIndex}"
   rowIndexTemplateTitle: "Row {rowIndex}",
-  // "{rowTitle}"
   rowNameTemplateTitle: "{rowTitle}",
-  // "Add new"
   addPanel: "Add new",
-  // "Remove"
   removePanel: "Remove",
-  // "Show Details"
   showDetails: "Show Details",
-  // "Hide Details"
   hideDetails: "Hide Details",
-  // "item"
   choices_Item: "item",
-  // "Choice option"
   choices_Choice: "Choice option",
-  // "Column"
   matrix_column: "Column",
-  // "Row"
   matrix_row: "Row",
-  // "text"
   multipletext_itemname: "text",
-  // "The results are being saved on the server..."
   savingData: "The results are being saved on the server...",
-  // "An error occurred and we could not save the results."
   savingDataError: "An error occurred and we could not save the results.",
-  // "The results were saved successfully!"
   savingDataSuccess: "The results were saved successfully!",
-  // "Your response exceeds 64KB. Please reduce the size of your file(s) and try again or contact the survey owner."
   savingExceedSize: "Your response exceeds 64KB. Please reduce the size of your file(s) and try again or contact the survey owner.",
-  // "Try again"
   saveAgainButton: "Try again",
-  // "min"
   timerMin: "min",
-  // "sec"
   timerSec: "sec",
-  // "You have spent {0} on this page and {1} in total."
   timerSpentAll: "You have spent {0} on this page and {1} in total.",
-  // "You have spent {0} on this page."
   timerSpentPage: "You have spent {0} on this page.",
-  // "You have spent {0} in total."
   timerSpentSurvey: "You have spent {0} in total.",
-  // "You have spent {0} of {1} on this page and {2} of {3} in total."
   timerLimitAll: "You have spent {0} of {1} on this page and {2} of {3} in total.",
-  // "You have spent {0} of {1} on this page."
   timerLimitPage: "You have spent {0} of {1} on this page.",
-  // "You have spent {0} of {1} in total."
   timerLimitSurvey: "You have spent {0} of {1} in total.",
-  // "Clear"
   clearCaption: "Clear",
-  // "Select"
   selectCaption: "Select",
-  // "Sign here"
   signaturePlaceHolder: "Sign here",
-  // "No signature"
   signaturePlaceHolderReadOnly: "No signature",
-  // "Select File"
   chooseFileCaption: "Select File",
-  // "Take Photo"
   takePhotoCaption: "Take Photo",
-  // "Click the button below to take a photo using the camera."
   photoPlaceholder: "Click the button below to take a photo using the camera.",
-  // "Drag and drop or select a file to upload or take a photo using the camera."
   fileOrPhotoPlaceholder: "Drag and drop or select a file to upload or take a photo using the camera.",
-  // "Replace file"
   replaceFileCaption: "Replace file",
-  // "Remove this file"
   removeFileCaption: "Remove this file",
-  // "Yes"
   booleanCheckedLabel: "Yes",
-  // "No"
   booleanUncheckedLabel: "No",
-  // "Are you sure that you want to remove this file: {0}?"
   confirmRemoveFile: "Are you sure that you want to remove this file: {0}?",
-  // "Are you sure that you want to remove all files?"
   confirmRemoveAllFiles: "Are you sure that you want to remove all files?",
-  // "Question Title"
   questionTitlePatternText: "Question Title",
-  // "Cancel"
   modalCancelButtonText: "Cancel",
-  // "Apply"
   modalApplyButtonText: "Apply",
-  // "Type to search..."
   filterStringPlaceholder: "Type to search...",
-  // "No data to display"
   emptyMessage: "No data to display",
-  // "Loading..."
   loadingPage: "Loading...",
-  // "Loading..."
   loadingData: "Loading...",
-  // "No entries yet.\nClick the button below to add a new entry."
   noEntriesText: "No entries yet.\nClick the button below to add a new entry.",
-  // "No entries"
   noEntriesReadonlyText: "No entries",
-  // "New Panel"
   tabTitlePlaceholder: "New Panel",
-  // "More"
   more: "More",
-  // "OK"
   tagboxDoneButtonCaption: "OK",
-  // "All choices are selected for ranking"
   selectToRankEmptyRankedAreaText: "All choices are selected for ranking",
-  // "Drag choices here to rank them"
   selectToRankEmptyUnrankedAreaText: "Drag choices here to rank them",
-  // "OK"
   ok: "OK",
-  // "Cancel"
   cancel: "Cancel",
-  // "Create \"{0}\" item..."
   createCustomItem: "Create \"{0}\" item...",
-  // "Table of contents"
   toc: "Table of contents",
-  // "Progress bar"
   progressbar: "Progress bar"
 };
 

54c4d85bda0174e7766cdec0c422907efbcc1916
RomanTsukanov <sergeich16@gmail.com>
2025-09-24 12:19:29 +0400

    Add a description for the `notificationType` property

--- packages/survey-core/src/validator.ts
+++ packages/survey-core/src/validator.ts
@@ -62,6 +62,19 @@ export class SurveyValidator extends Base {
       ? (<any>this.errorOwner).getSurvey()
       : null;
   }
+  /**
+   * Specifies the type of notification shown to users.
+   *
+   * Possible values:
+   *
+   * - `"error"`
+   * - `"warning"`
+   * - `"info"`
+   *
+   * Errors block survey progress until resolved. Warnings indicate potential issues but don't block respondents from continuing the survey. Informational notes provide guidance without restrictions.
+   *
+   * > If multiple notification types are eligible to be displayed for a question, only the strongest type is shown. Warnings appear only after all errors are resolved, and notes appear only when there are no errors or warnings.
+   */
   public get notificationType(): string { return this.getPropertyValue("notificationType"); }
   public set notificationType(val: string) { this.setPropertyValue("notificationType", val); }
   /**

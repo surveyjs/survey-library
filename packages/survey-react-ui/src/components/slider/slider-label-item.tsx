605e26ba3c3c7fc04c50d130018d954f174f869a
Dmitry Kurmanov <kurmanov.work@gmail.com>
2025-10-07 13:11:00 +0400

    PR: Library: Slider - Introduce an option to render value and text in custom labels (#10451)
    * Slider - Introduce an option to render value and text in custom labels #10399
    
    ---------
    
    Co-authored-by: RomanTsukanov <sergeich16@gmail.com>

--- packages/survey-react-ui/src/components/slider/slider-label-item.tsx
+++ packages/survey-react-ui/src/components/slider/slider-label-item.tsx
@@ -20,11 +20,24 @@ export class SliderLabelItem extends SurveyElementBase<any, any> {
   protected renderElement(): React.JSX.Element {
     const { cssClasses, handleLabelPointerUp, getLabelCss, getPercent } = this.question;
     const { value, locText } = this.item;
+    let labelText = null;
+    let labelTextSecondary = null;
+    if (this.item.showValue) {
+      labelText = <div className={cssClasses.labelText}>{this.item.value}</div>;
+      labelTextSecondary = <div className={cssClasses.labelTextSecondaryMode}>
+        {this.renderLocString(locText)}
+      </div>;
+    } else {
+      labelText = <div className={cssClasses.labelText}>
+        {this.renderLocString(locText)}
+      </div>;
+    }
     return <div key={this.item.id} className={getLabelCss(locText)}
       style={{ left: getPercent(value) + "%" }} onPointerUp={ (e)=>{ handleLabelPointerUp(e.nativeEvent, value); } }>
       <div className={cssClasses.labelTick}></div>
-      <div className={cssClasses.labelText}>
-        {this.renderLocString(locText)}
+      <div className={cssClasses.labelTextContainer}>
+        {labelText}
+        {labelTextSecondary}
       </div>
     </div>;
   }

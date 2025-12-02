c606cb05eac356d4e11c3b2d73c14ad5af4e616a
Dmitrii Gerasimov <dima@rimmon.dev>
2025-12-02 12:54:45 +0200

    Move logic to core, added responsiveness

--- packages/survey-core/src/question_imagemap.ts
+++ packages/survey-core/src/question_imagemap.ts
@@ -1,12 +1,159 @@
+import { DomDocumentHelper, DomWindowHelper } from "./global_variables_utils";
 import { ItemValue } from "./itemvalue";
-import { Serializer } from "./jsonobject";
+import { property, Serializer } from "./jsonobject";
 import { Question } from "./question";
 
 export class QuestionImageMapModel extends Question {
   constructor(name: string) { super(name); }
 
+  backgroundImage: HTMLImageElement;
+  selectedCanvas: HTMLCanvasElement;
+  hoverCanvas: HTMLCanvasElement;
+  imageMapMap: HTMLMapElement;
+
   public getType(): string {
     return "imagemap";
+  }
+
+  public supportResponsiveness(): boolean {
+    return true;
+  }
+
+  protected getObservedElementSelector(): string {
+    return "#imagemap-" + this.id + "-background";
+  }
+
+  public processResponsiveness() {
+    this.renderImageMap();
+  }
+
+  protected onValueChanged(): void {
+    super.onValueChanged();
+    this.renderSelectedCanvas();
+  }
+
+  public afterRenderQuestionElement(el: HTMLElement) {
+    if (DomWindowHelper.isAvailable()) {
+      if (!!el) {
+        if (!this.isDesignMode) {
+          this.initImageMap(el);
+        }
+        this.element = el;
+      }
+    }
+    super.afterRenderQuestionElement(el);
+  }
+
+  public initImageMap(el: HTMLElement): void {
+
+    if (!el) return;
+
+    this.backgroundImage = el.querySelector(`#imagemap-${this.id}-background`) as HTMLImageElement;
+    this.selectedCanvas = el.querySelector(`#imagemap-${this.id}-canvas-selected`) as HTMLCanvasElement;
+    this.hoverCanvas = el.querySelector(`#imagemap-${this.id}-canvas-hover`) as HTMLCanvasElement;
+    this.imageMapMap = el.querySelector("map") as HTMLMapElement;
+
+    this.imageMapMap.onclick = (event) => {
+      let target = event.target as HTMLElement;
+      let value = target.dataset.value;
+      let item = this.imageMap.find(i => i.value === value);
+      this.mapItemTooggle(item);
+    };
+
+    this.imageMapMap.onmouseover = (event) => {
+      let target = event.target as HTMLElement;
+      let value = target.dataset.value;
+      for (const item of this.imageMap.filter(i => i.value === value)) {
+        this.drawShape(this.hoverCanvas, item.shape, item.coords.split(",").map(Number), {
+          strokeColor: item.hoverStrokeColor,
+          strokeLineWidth: item.hoverStrokeSize,
+          fillColor: item.hoverFillColor,
+        });
+      }
+    };
+
+    this.imageMapMap.onmouseout = (event) => {
+      this.clearCanvas(this.hoverCanvas);
+    };
+
+    this.backgroundImage.onload = (event) => {
+      this.renderImageMap();
+      this.renderSelectedCanvas();
+      this.renderHoverCanvas();
+    };
+  }
+
+  public scaleCoords(coords: number[]): number[] {
+    let scale = this.backgroundImage.width / this.backgroundImage.naturalWidth;
+    return coords.map((coord) => coord * scale);
+  }
+
+  public drawShape(canvas: HTMLCanvasElement, shape: string, coords: number[], style: { strokeColor: string, fillColor: string, strokeLineWidth: number }): void {
+
+    let ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+
+    ctx.strokeStyle = style.strokeColor;
+    ctx.lineWidth = style.strokeLineWidth;
+
+    ctx.fillStyle = style.fillColor;
+
+    switch(shape) {
+      case "rect":
+        ctx.rect(coords[0], coords[1], coords[2] - coords[0], coords[3] - coords[1]);
+        break;
+      case "circle":
+        ctx.arc(coords[0], coords[1], coords[2], 0, 2 * Math.PI);
+        break;
+      case "poly":
+        for (let i = 0; i < coords.length; i += 2) {
+          ctx.lineTo(coords[i], coords[i + 1]);
+        }
+        ctx.closePath();
+        break;
+    }
+
+    ctx.stroke();
+    ctx.fill();
+  }
+
+  public clearCanvas(canvas: HTMLCanvasElement): void {
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  public renderImageMap(): void {
+    this.imageMapMap.innerHTML = "";
+    for (let item of this.imageMap) {
+      let area = DomDocumentHelper.createElement("area") as HTMLAreaElement;
+      area.shape = item.shape;
+      area.coords = this.scaleCoords(item.coords.split(",").map(Number)).join(",");
+      area.dataset["value"] = item.value;
+      this.imageMapMap.appendChild(area);
+    }
+  }
+
+  public renderSelectedCanvas(): void {
+    if (!this.selectedCanvas) return;
+    this.clearCanvas(this.selectedCanvas);
+    this.selectedCanvas.width = this.backgroundImage.naturalWidth;
+    this.selectedCanvas.height = this.backgroundImage.naturalHeight;
+    for (const item of this.imageMap) {
+      if (!this.isItemSelected(item)) continue;
+      this.drawShape(this.selectedCanvas, item.shape, item.coords.split(",").map(Number), {
+        strokeColor: item.selectedStrokeColor,
+        strokeLineWidth: item.selectedStrokeSize,
+        fillColor: item.selectedFillColor,
+      });
+    }
+  }
+
+  public renderHoverCanvas(): void {
+    if (!this.hoverCanvas) return;
+    this.clearCanvas(this.hoverCanvas);
+    this.hoverCanvas.width = this.backgroundImage.naturalWidth;
+    this.hoverCanvas.height = this.backgroundImage.naturalHeight;
   }
 
   public get imageLink() {
@@ -47,25 +194,30 @@ export class ImageMapItem extends ItemValue {
     return "imagemapitem";
   }
 
-  public get shape(): string {
-    return this.getPropertyValue("shape");
-  }
-  public set shape(val: string) {
-    this.setPropertyValue("shape", val);
-  }
+  @property() shape: string;
+  @property() coords: string;
 
-  public get coords(): string {
-    return this.getPropertyValue("coords");
-  }
-  public set coords(val: string) {
-    this.setPropertyValue("coords", val);
-  }
+  @property() hoverStrokeColor: string;
+  @property() hoverStrokeSize: number;
+  @property() hoverFillColor: string;
+
+  @property() selectedStrokeColor: string;
+  @property() selectedStrokeSize: number;
+  @property() selectedFillColor: string;
 }
 
 Serializer.addClass("imagemapitem",
   [
-    { name: "shape", choices: ["rect", "circle", "poly"], default: "poly" },
+    { name: "shape", choices: ["circle", "rect", "poly"], default: "poly" },
     { name: "coords:string" },
+
+    { name: "hoverStrokeColor:color", default: "rgba(255, 0, 0, 1)" },
+    { name: "hoverStrokeSize:number", default: 1 },
+    { name: "hoverFillColor:color", default: "rgba(255, 0, 0, 0.25)" },
+
+    { name: "selectedStrokeColor:color", default: "rgba(0, 0, 0, 1)" },
+    { name: "selectedStrokeSize:number", default: 1 },
+    { name: "selectedFillColor:color", default: "rgba(0, 0, 0, 0.25)" },
   ],
   () => new ImageMapItem(""),
   "itemvalue"
--- packages/survey-react-ui/src/reactquestion_imagemap.tsx
+++ packages/survey-react-ui/src/reactquestion_imagemap.tsx
@@ -22,89 +22,27 @@ export class SurveyQuestionImageMap extends SurveyQuestionElementBase {
     super.componentWillUnmount();
   }
 
-  backgroundImage: HTMLImageElement | null = null;
-  setBackgroundImage(e: HTMLImageElement): void {
-
-    if (!e) return;
-
-    // this.backgroundImage = e;
-
-    e.onload = () => {
-      const scale = e.width / e.naturalWidth;
-      this.setState({ scale });
-      this.setState({ naturalWidth: e.naturalWidth });
-      this.setState({ naturalHeight: e.naturalHeight });
-    };
-  }
-
-  selectedCanvas: HTMLCanvasElement | null = null;
-  setSelectedCanvas(e: HTMLCanvasElement): void {
-
-    this.selectedCanvas = e;
-    this.drawSelectedCanvas();
-  }
-  public drawSelectedCanvas(): void {
-
-    if (!this.selectedCanvas) return;
-
-    const ctx = this.selectedCanvas.getContext("2d");
-
-    ctx.clearRect(0, 0, this.selectedCanvas.width, this.selectedCanvas.height);
-
-    for (const item of this.question.imageMap) {
-
-      if (!this.question.isItemSelected(item)) continue;
-      let coords = item.coords.split(",").map((coord) => Number(coord));
-
-      ctx.beginPath();
-      switch(item.shape) {
-        case "rect":
-          ctx.rect(coords[0], coords[1], coords[2] - coords[0], coords[3] - coords[1]);
-          break;
-        case "circle":
-          ctx.arc(coords[0], coords[1], coords[2], 0, 2 * Math.PI);
-          break;
-        case "poly":
-          for (let i = 0; i < coords.length; i += 2) {
-            ctx.lineTo(coords[i], coords[i + 1]);
-          }
-          ctx.closePath();
-          break;
-      }
-      ctx.fill();
-    }
-  }
-
   public renderElement(): React.JSX.Element {
 
-    const imageMap = (this.state.scale && <map name={`imagemap-${ this.question.id }`}>
-      {this.question.imageMap.map((item, index) => {
+    return (
+      <div style={{ position: "relative", userSelect: "none" }} ref={(root) => (this.setControl(root))}>
 
-        let scale = this.state.scale;
-        let coords = item.coords.split(",").map((coord) => Number(coord) * scale).join(",");
+        <img id={`imagemap-${this.question.id}-background`} style={{ display: "block", width: "100%" }} src={this.question.imageLink} />
 
-        return <area key={index} shape={item.shape} coords={coords} href="#" alt={item.text}
-          onClick={(e) => { e.preventDefault(); this.question.mapItemTooggle(item); this.drawSelectedCanvas(); }} />;
-      })}
-    </map>);
+        <canvas id={`imagemap-${this.question.id}-canvas-selected`} style={{ position: "absolute", top: 0, left: 0, width: "100%" }}></canvas>
 
-    return (<>
-      <div style={{ position: "relative" }}>
+        <canvas id={`imagemap-${this.question.id}-canvas-hover`} style={{ position: "absolute", top: 0, left: 0, width: "100%" }}></canvas>
+
         <img
-          style={{ width: "100%" }}
+          style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", opacity: 0 }}
           src={this.question.imageLink}
           useMap={`#imagemap-${ this.question.id }`}
-          ref={(e) => this.setBackgroundImage(e)}
         />
-        <canvas
-          style={{ position: "absolute", top: 0, left: 0, width: "100%", pointerEvents: "none" }}
-          height={this.state.naturalHeight}
-          width={this.state.naturalWidth}
-          ref={(e) => { this.setSelectedCanvas(e); }}></canvas>
-      </div>
 
-      { imageMap }
-    </>);
+        <map name={`imagemap-${ this.question.id }`}></map>
+
+      </div>
+    );
   }
 }
 

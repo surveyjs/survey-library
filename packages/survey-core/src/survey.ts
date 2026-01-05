85fced6fc967baa00e5b9af415902da21a048336
Andrew <andrew.telnov@gmail.com>
2026-01-05 13:08:28 +0200

    Remove adding handler from survey.onPopupVisibleChanged event (#10756)

--- packages/survey-core/src/survey.ts
+++ packages/survey-core/src/survey.ts
@@ -1172,16 +1172,6 @@ export class SurveyModel extends SurveyElementCore
     }
     this.updateCss();
     this.setCalculatedWidthModeUpdater();
-
-    this.onPopupVisibleChanged.add((_, opt) => {
-      if (opt.visible) {
-        this.onScrollCallback = () => {
-          opt.popup.hide();
-        };
-      } else {
-        this.onScrollCallback = undefined;
-      }
-    });
   }
   protected onPropertyValueChanged(name: string, oldValue: any, newValue: any): void {
     super.onPropertyValueChanged(name, oldValue, newValue);
@@ -8631,7 +8621,15 @@ export class SurveyModel extends SurveyElementCore
     return containerLayoutElements;
   }
   public processPopupVisiblityChanged(question: Question, popup: PopupModel<any>, visible: boolean): void {
-    this.onPopupVisibleChanged.fire(this, { question, popup, visible });
+    if (visible) {
+      this.onScrollCallback = () => {
+        popup.hide();
+      };
+    } else {
+      this.onScrollCallback = undefined;
+    }
+
+    this.onPopupVisibleChanged.fire(this, { question: question, popup: popup, visible: visible });
   }
   public processOpenDropdownMenu(question: Question, options: IDropdownMenuOptions): void {
     const newOptions = Object.assign({ question }, options);

75219f5e2a98422a7d242d5939305d5bf007cdce
MarchTokenda <98102580+MarchTokenda@users.noreply.github.com>
2026-02-13 10:20:02 +0200

    Feature/rollup (#10891)
    * Refactor build process to use Rollup
    
    * Enhance Rollup configuration and add support for themes and icons
    
    * Refactor Rollup configuration to single pass
    
    * Forget to use emitMinified in css
    
    * umd esbuild option
    
    * Disable esbuild for themes, add d.ts for icons
    
    * Remove deprecated Rollup and Webpack configuration files
    
    * react-ui migrate to rollup
    
    * move rollup.helpers to root, update packages list

---
+++ packages/survey-core/rollup.i18n.config.mjs
@@ -0,0 +1,54 @@
+import { resolve, extname, basename, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { readdirSync } from "fs";
+import { createEsmConfig, createUmdConfig } from "../../rollup.helpers.mjs";
+import process from "process";
+import pkg from "./package.json" assert { type: "json" };
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const buildPath = resolve(__dirname, "build");
+
+function getEntries() {
+
+  const inputs = {
+    "survey.i18n": resolve(__dirname, "entries", "i18n.ts")
+  };
+
+  readdirSync(resolve(__dirname, "src", "localization")).forEach(file => {
+    var extension = extname(file);
+    if (extension.toLowerCase() === ".ts") {
+      inputs[`i18n/${basename(file, extension)}`] = resolve(__dirname, "src", "localization", file);
+    }
+  });
+  inputs["i18n/index"] = resolve(__dirname, "entries", "i18n.ts");
+
+  return inputs;
+}
+
+export default () => {
+
+  const inputs = getEntries();
+
+  const config = [
+    createEsmConfig({
+      input: inputs,
+      dir: resolve(buildPath, "fesm"),
+      external: ["survey-core"],
+      tsconfig: resolve(__dirname, "tsconfig.i18n.json"),
+      version: pkg.version
+    }),
+    ...Object.entries(inputs).map(([name, path]) => createUmdConfig({
+      input: { [name]: path },
+      tsconfig: resolve(__dirname, "tsconfig.i18n.json"),
+      external: ["survey-core"],
+      dir: resolve(buildPath),
+      emitMinified: process.env.emitMinified === "true",
+      globalName: name,
+      globals: { "survey-core": "Survey" },
+      useEsbuild: true,
+      version: pkg.version
+    }))
+  ];
+
+  return config;
+};

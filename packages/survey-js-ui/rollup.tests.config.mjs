e31d087dbcca79ee06d9ec499a5194e89a548943
Andrew Telnov <andrew.telnov@gmail.com>
2026-03-02 18:13:54 +0200

    Refactor: update package.json import syntax to use assert for JSON module

--- packages/survey-js-ui/rollup.tests.config.mjs
+++ packages/survey-js-ui/rollup.tests.config.mjs
@@ -5,7 +5,7 @@ import alias from "@rollup/plugin-alias";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import replace from "@rollup/plugin-replace";
-import pkg from "./package.json" with { type: "json" };
+import pkg from "./package.json" assert { type: "json" };
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const snapshotsDir = resolve(__dirname, "../../tests/markup/snapshots");

ed9a589e84e4289f0e04c05cb3f66d01c6fd2d70
Dmitrii Gerasimov <dima@rimmon.dev>
2026-03-03 12:27:58 +0200

    tidy

--- packages/survey-angular-ui/karma.conf.js
+++ packages/survey-angular-ui/karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function (config) {
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage"),
-      // require("karma-webpack"),
       require("@angular-devkit/build-angular/plugins/karma"),
       require("karma-html2js-preprocessor")
     ],

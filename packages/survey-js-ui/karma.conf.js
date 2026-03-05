c1d18c2c96ffdb5e707ed37c9517a4d905ee597c
Andrew <andrew.telnov@gmail.com>
2026-03-05 10:48:29 +0200

    Refactor/remove webpack (#10953)
    * Update testing configuration and add Rollup support for unit tests in survey-core
    
    * Refactor: remove Webpack configuration and related plugin, update localization documentation for Rollup
    
    * Refactor: update test watch script and add log functionality for Rollup watcher
    
    * Refactor: update package.json import to use dynamic import syntax
    
    * Refactor: migrate from Webpack to Rollup for testing configuration and add watch functionality
    
    * replace with type vs assert type
    
    * Refactor: migrate testing configuration from Webpack to Rollup, update test scripts, and add watch functionality
    
    * Refactor: remove Webpack configuration files in favor of Rollup
    
    * Use rollup in watch:dev command in react
    
    * Refactor: replace Webpack with http-server in start script and remove unused dependencies
    
    * Refactor: migrate from Webpack to Rollup for build and watch scripts, remove Webpack configuration, and add new Rollup config file
    
    * Refactor: update package.json import syntax to use assert for JSON module
    
    * Refactor: update package.json import syntax to use assert for JSON module
    
    * Fix package.json in survey-core
    
    * Refactor: remove karma-webpack dependency and comment out angular configuration
    
    * tidy
    
    * rollup.helper from creator, remove creation LICENSE.txt, optimize karma+rollup
    
    * Refactor: update rollup configuration to use aliases and improve module resolution
    
    * Create build directory before emit NonSourceFiles
    
    ---------
    
    Co-authored-by: Dmitrii Gerasimov <dima@rimmon.dev>

--- packages/survey-js-ui/karma.conf.js
+++ packages/survey-js-ui/karma.conf.js
@@ -1,16 +1,9 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const path = require("path");
-const packageJson = require("./package.json");
-const webpack = require("webpack");
 module.exports = function (config) {
   config.set({
     basePath: "",
-    files: ["tests/markup.ts"],
+    files: ["tests/bundle/test.js"],
     frameworks: ["qunit"],
     exclude: [],
-    mime: {
-      "text/x-typescript": ["ts", "tsx"]
-    },
     captureTimeout: 210000,
     browserDisconnectTimeout: 100000,
     browserDisconnectTolerance: 3,
@@ -18,48 +11,6 @@ module.exports = function (config) {
     junitReporter: {
       outputDir: "tmp/testresults/",
       outputFile: "test-results.xml"
-    },
-    preprocessors: {
-      "**/*.ts": ["webpack", "sourcemap"]
-    },
-    webpack: {
-      module: {
-        rules: [
-          {
-            test: /\.(ts|tsx)$/,
-            loader: "ts-loader",
-            options: {
-              transpileOnly: false,
-              configFile: path.resolve(__dirname, "./tsconfig.tests.json")
-            }
-          },
-          {
-            test: /\.html$/,
-            loader: "html-loader",
-            options: {
-              esModule: false
-            }
-          },
-        ]
-      },
-      resolve: {
-        extensions: [".ts", ".js", ".tsx", ".scss"],
-        alias: {
-          "react": path.resolve(__dirname, "./node_modules/preact/compat"),
-          "react-dom/test-utils": path.resolve(__dirname, "./node_modules/preact/test-utils"),
-          "react-dom": path.resolve(__dirname, "./node_modules/preact/compat"),
-          "react/jsx-runtime": path.resolve(__dirname, "./node_modules/preact/jsx-runtime"),
-          "survey-core": path.resolve(__dirname, "./node_modules/survey-core"),
-          "survey-core/icons/iconsV1": path.resolve(__dirname, "./node_modules/survey-core/icons/iconsV1"),
-          "survey-core/icons/iconsV2": path.resolve(__dirname, "./node_modules/survey-core/icons/iconsV2"),
-          "jquery": path.resolve(__dirname, "./node_modules/jquery"),
-        }
-      },
-      plugins: [
-        new webpack.DefinePlugin({
-          "process.env.VERSION": JSON.stringify(packageJson.version)
-        }),
-      ]
     },
     reporters: ["progress", "dots", "junit"],
     browsers: ["ChromeHeadlessNoSandbox"],
---
+++ packages/survey-js-ui/rollup.tests.config.mjs
@@ -0,0 +1,88 @@
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { readdirSync, readFileSync } from "node:fs";
+import alias from "@rollup/plugin-alias";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import replace from "@rollup/plugin-replace";
+import pkg from "./package.json" assert { type: "json" };
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const snapshotsDir = resolve(__dirname, "../../tests/markup/snapshots");
+
+function htmlSnapshotsPlugin() {
+  return {
+    name: "html-snapshots",
+    transform(code, id) {
+      if (!id.endsWith("tests/markup.ts") && !id.endsWith("tests\\markup.ts")) {
+        return null;
+      }
+      const files = readdirSync(snapshotsDir).filter(f => f.endsWith(".snap.html"));
+      const map = {};
+      for (const file of files) {
+        const name = file.replace(".snap.html", "");
+        map[name] = readFileSync(resolve(snapshotsDir, file), "utf8");
+      }
+      code = code.replace(
+        /require\("\.\.\/\.\.\/\.\.\/tests\/markup\/snapshots\/" \+ snapshot \+ "\.snap\.html"\)/,
+        "(__html_snapshots__[snapshot])"
+      );
+      code = `var __html_snapshots__ = ${JSON.stringify(map)};\n${code}`;
+      return { code, map: null };
+    },
+  };
+}
+
+function surveyCoreAlias() {
+  return {
+    name: "survey-core-alias",
+    resolveId(importee, importer) {
+      if (importee === "survey-core" && importer) {
+        return this.resolve(importee, resolve(__dirname, "tests/markup.ts"), { skipSelf: true });
+      }
+      return null;
+    },
+  };
+}
+
+export default {
+  input: resolve(__dirname, "tests/markup.ts"),
+  context: "this",
+  output: {
+    file: resolve(__dirname, "tests/bundle/test.js"),
+    format: "iife",
+    sourcemap: "inline",
+    name: "SurveyJsUITests",
+  },
+  plugins: [
+    surveyCoreAlias(),
+    alias({
+      entries: {
+        "react": resolve(__dirname, "node_modules/preact/compat"),
+        "react-dom/test-utils": resolve(__dirname, "node_modules/preact/test-utils"),
+        "react-dom": resolve(__dirname, "node_modules/preact/compat"),
+        "react/jsx-runtime": resolve(__dirname, "node_modules/preact/jsx-runtime"),
+      },
+    }),
+    nodeResolve({ extensions: [".ts", ".tsx", ".js", ".jsx"] }),
+    typescript({
+      tsconfig: resolve(__dirname, "tsconfig.tests.json"),
+      filterRoot: false,
+      compilerOptions: {
+        declaration: false,
+        declarationDir: null,
+        sourceMap: true,
+        inlineSources: true,
+      },
+    }),
+    htmlSnapshotsPlugin(),
+    replace({
+      preventAssignment: false,
+      values: {
+        "process.env.RELEASE_DATE": JSON.stringify(new Date().toISOString().slice(0, 10)),
+        "process.env.VERSION": JSON.stringify(pkg.version),
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      },
+    }),
+  ],
+};

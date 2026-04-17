280eb1c4da171f59cfabc03615a70a33561d7776
OlgaLarina <olga.larina.dev@gmail.com>
2026-04-17 11:12:04 +0300

    update for preact tests

--- e2e/helper.ts
+++ e2e/helper.ts
@@ -134,10 +134,6 @@ export const initSurvey = async (page: Page, framework: string, json: any, isDes
         ::before {
             box-sizing: border-box;
         }
-
-        .root-element {
-          height: 100%;
-        }
       `;
       shadowRoot.appendChild(styles);
       const surveyLink = document.createElement("link");

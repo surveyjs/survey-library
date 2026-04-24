0bfe253b40c991f2a073c58a207d2be199fae0bd
Aleksey Novikov <novikov82@gmail.com>
2026-04-24 13:58:19 +0300

    #11197 New tokens: boolean, boolean-item Fixes #11197

--- packages/survey-core/src/themes/default-light.ts
+++ packages/survey-core/src/themes/default-light.ts
@@ -886,6 +886,19 @@ export default {
     "--sjs2-border-effect-surface-focused": "0px 0px 0px var(--sjs2-border-width-x200) var(--sjs2-color-utility-shadow-surface-focused)",
     "--sjs2-border-effect-surface-disabled": "0px 0px 0px var(--sjs2-border-width-x100) var(--sjs2-color-utility-shadow-surface-disabled)",
     "--sjs2-border-effect-surface-dragging": "0px 6px 12px 4px var(--sjs2-color-utility-shadow-surface-dragging)",
-    "--sjs2-border-effect-floating-default": "0px 6px 20px 0px var(--sjs2-color-utility-shadow-floating-default)"
+    "--sjs2-border-effect-floating-default": "0px 6px 20px 0px var(--sjs2-color-utility-shadow-floating-default)",
+    "--sjs2-border-effect-component-boolean-default": "inset 0px 1px 2px 0px var(--sjs2-color-component-boolean-default-border)",
+    "--sjs2-border-effect-component-boolean-disabled": "inset 0px 0px 0px var(--sjs2-border-width-x100) var(--sjs2-color-component-boolean-disabled-border)",
+    "--sjs2-border-effect-component-boolean-invalid": "inset 0px 0px 0px var(--sjs2-border-width-x100) var(--sjs2-color-component-boolean-invalid-border)",
+    "--sjs2-border-effect-component-boolean-highlighted": "inset 0px 0px 0px var(--sjs2-border-width-x200) var(--sjs2-color-component-boolean-highlighted-border)",
+    "--sjs2-border-effect-component-boolean-readonly": "inset 0px 0px 0px var(--sjs2-border-width-x100) var(--sjs2-color-component-boolean-readonly-border)",
+    "--sjs2-border-effect-component-boolean-design": "inset 0px 1px 2px 0px var(--sjs2-color-component-boolean-design-border)",
+    "--sjs2-border-effect-component-boolean-item-false-default": "0px 0px 0px 0px var(--sjs2-color-component-boolean-item-false-default-border)",
+    "--sjs2-border-effect-component-boolean-item-false-disabled": "0px 0px 0px 0px var(--sjs2-color-component-boolean-item-false-disabled-border)",
+    "--sjs2-border-effect-component-boolean-item-false-readonly": "0px 0px 0px 0px var(--sjs2-color-component-boolean-item-false-readonly-border)",
+    "--sjs2-border-effect-component-boolean-item-false-hovered": "0px 0px 0px 0px var(--sjs2-color-component-boolean-item-false-hovered-border)",
+    "--sjs2-border-effect-component-boolean-item-true-default": "0px 1px 2px 0px var(--sjs2-color-component-boolean-item-true-default-border)",
+    "--sjs2-border-effect-component-boolean-item-true-disabled": "inset 0px 0px 0px var(--sjs2-border-width-x200) var(--sjs2-color-component-boolean-item-true-disabled-border)",
+    "--sjs2-border-effect-component-boolean-item-true-readonly": "inset 0px 0px 0px var(--sjs2-border-width-x200) var(--sjs2-color-component-boolean-item-true-readonly-border)"
   }
 };

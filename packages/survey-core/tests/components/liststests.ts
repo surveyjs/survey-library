17feecccaefcc9e97c03abc63af649e1e3166f4b
OlgaLarina <olga.larina.dev@gmail.com>
2026-05-06 14:42:45 +0300

    update test & etalon

--- packages/survey-core/tests/components/liststests.ts
+++ packages/survey-core/tests/components/liststests.ts
@@ -4,6 +4,7 @@ import { describe, test, expect } from "vitest";
 test("Check goToItemsMethod works correctly", () => {
   const list = new ListModel({ items: <Array<any>>[{ title: "item" }], onSelectionChanged: () => { }, allowSelection: false } as any);
   const element = document.createElement("div");
+  element.className = "sv-list__container";
   element.innerHTML = "<div><input/></div><div></div><ul><li tabindex='0'></li></ul>";
   const input = element.querySelector("input");
   const listElement = element.querySelector("li");

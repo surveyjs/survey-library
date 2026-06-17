4490bb8ecc4ad37e062494f7ccccb3d03e027300
Aleksey Novikov <novikov82@gmail.com>
2026-06-17 12:27:02 +0300

    fix build

--- packages/survey-core/src/interfaces/survey-interfaces.ts
+++ packages/survey-core/src/interfaces/survey-interfaces.ts
@@ -95,7 +95,6 @@ export interface ISurvey extends ITextProcessor, ISurveyErrorOwner,
   state: string;
   cancelPreviewByPage(panel: IPanel): any;
   locEditText: LocalizableString;
-  cssNavigationEdit: string;
   //#endregion
 
   //#region Question lookup

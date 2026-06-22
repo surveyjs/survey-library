ec812e167b2b4c3a38154de135fb25d518572d38
Aleksey Novikov <novikov82@gmail.com>
2026-06-22 19:08:25 +0300

    build scripts for theme adapters

--- packages/survey-core/src/themes/adapters/icons/mui.ts
+++ packages/survey-core/src/themes/adapters/icons/mui.ts
@@ -1,7 +1,7 @@
 import { SvgRegistry } from "survey-core";
-import iconCheck16x16 from "./icon-check-16x16.svg?raw";
-import iconRadio16x16 from "./icon-radio-16x16.svg?raw";
-import iconChevronDown24x24 from "./icon-chevrondown-24x24.svg?raw";
+import iconCheck16x16 from "./mui-icon-check-16x16.svg?raw";
+import iconRadio16x16 from "./mui-icon-radio-16x16.svg?raw";
+import iconChevronDown24x24 from "./mui-icon-chevrondown-24x24.svg?raw";
 
 SvgRegistry.registerIcon("icon-check-16x16", iconCheck16x16);
 SvgRegistry.registerIcon("icon-radio-16x16", iconRadio16x16);

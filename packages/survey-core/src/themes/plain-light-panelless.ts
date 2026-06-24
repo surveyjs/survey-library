af94b52834f17343a403dc3971b556bc99024bb4
OlgaLarina <olga.larina.dev@gmail.com>
2026-06-24 15:42:39 +0300

    update themes

--- packages/survey-core/src/themes/plain-light-panelless.ts
+++ packages/survey-core/src/themes/plain-light-panelless.ts
@@ -23,7 +23,7 @@ export default {
     "--sjs2-color-border-basic-secondary": "rgba(0, 0, 0, 0.15)",
     "--sjs2-color-border-basic-secondary-overlay": "rgba(0, 0, 0, 0.16)",
     "--sjs2-color-data-chart-track-color-0": "var(--sjs2-palette-gray-150)",
-    "--sjs2-color-component-panel-default-border": "var(--sjs2-color-utility-shadow-surface-default)",
+    "--sjs2-color-component-panel-nested-default-border": "var(--sjs2-color-utility-shadow-surface-default)",
     "--sjs2-border-effect-component-formbox-default": "0px 0px 0px 1px rgba(0, 0, 0, 0.15)",
     "--sjs2-border-effect-surface-default": "0px 0px 0px 1px rgba(0, 0, 0, 0.15)",
     "--sjs2-border-effect-floating-default": "0px 0px 0px 1px rgba(0, 0, 0, 0.1), 0px 8px 16px 0px rgba(0, 0, 0, 0.05)"

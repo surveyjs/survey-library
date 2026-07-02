47cc37751e2cbae17244fd8fe745077445a64779
tsv2013 <tsv2013@noreply.github.com>
2026-07-02 14:12:34 +0300

    Try to fix extra 1px diff in paneldynamic

--- packages/survey-core/src/question_paneldynamic.ts
+++ packages/survey-core/src/question_paneldynamic.ts
@@ -2473,6 +2473,7 @@ export class QuestionPanelDynamicModel extends Question implements IDynamicItemM
     const showTab = this.isRenderModeTab && !!this.visiblePanelCount;
     return new CssClassBuilder()
       .append(super.getCssHeader(this.cssClasses))
+      .append(this.cssClasses.root + "__header-" + this.displayMode)
       .append(this.cssClasses.headerTab, this.hasTitleOnTop && showTab)
       .toString();
   }

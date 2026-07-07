9227469306bc36d9cfb68a681cc126ee5716b33c
tsv2013 <tsv2013@noreply.github.com>
2026-07-07 14:08:33 +0300

    Trigger CI run with corresponding creator PR

--- e2e/questions/panel.spec.ts
+++ e2e/questions/panel.spec.ts
@@ -75,6 +75,7 @@ frameworks.forEach((framework) => {
       const getTitle3 = page.locator(".sd-element__title").filter({ hasText: "question3" });
       const getTitle4 = page.locator(".sd-element__title").filter({ hasText: "question4" });
 
+      // TODO: Check whether this test actual for new rendering
       const getPanelsCountByMargin = await page.evaluate(
         () => (window as any).survey.rootElement.getRootNode().querySelectorAll('div[style*="padding-inline-start"][style*="20px"]').length
       );

9b3f540e6bdada902d639181b4f15e41b1d21ece
dmitrykurmanov <kurmanov.work@gmail.com>
2026-07-17 20:18:59 +0400

    fixed tests (2)

--- .testcaferc.js
+++ .testcaferc.js
@@ -4,7 +4,6 @@ module.exports = {
   "browsers": [`chrome:${require("puppeteer").executablePath()}:headless`],
   "nativeAutomation": "true",
   "concurrency": 4,
-  "runExecutionTimeout": 2400000,
   "stopOnFirstFail": true,
   "quarantineMode": {
     "successThreshold": 1,

35b4f7fb5a275a729def3c638a4a8043fc8938d9
dmitrykurmanov <kurmanov.work@gmail.com>
2026-07-17 21:17:27 +0400

    fixed tests (3) - I hate TestCafe

--- .testcaferc.js
+++ .testcaferc.js
@@ -4,7 +4,6 @@ module.exports = {
   "browsers": [`chrome:${require("puppeteer").executablePath()}:headless`],
   "nativeAutomation": "true",
   "concurrency": 4,
-  "stopOnFirstFail": true,
   "quarantineMode": {
     "successThreshold": 1,
     "attemptLimit": 5

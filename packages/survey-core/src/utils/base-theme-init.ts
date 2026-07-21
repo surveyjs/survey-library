04288dad14831ec75f1daeef3181207cbd279eaa
Aleksey Novikov <novikov82@gmail.com>
2026-07-21 18:54:16 +0300

    increase CSS-variables chunk size

--- packages/survey-core/src/utils/base-theme-init.ts
+++ packages/survey-core/src/utils/base-theme-init.ts
@@ -4,7 +4,7 @@ import { DomDocumentHelper } from "../global_variables_utils";
 import { createBoxShadowReset } from "./shadow-effects";
 
 const STYLE_ELEMENT_ATTR = "data-survey-base-theme-variables";
-const VARIABLES_PER_RULE = 50;
+const VARIABLES_PER_RULE = 300;
 
 let cachedCss: string | undefined;
 

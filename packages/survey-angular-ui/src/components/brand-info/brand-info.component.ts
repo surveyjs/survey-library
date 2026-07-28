3f427eed3d2a9f8ca14e4e32619957e7c2720998
Dmitry Kuzin <dk981234@gmail.com>
2026-07-28 14:15:17 +0400

    Fix angular screenshot tests

--- packages/survey-angular-ui/src/components/brand-info/brand-info.component.ts
+++ packages/survey-angular-ui/src/components/brand-info/brand-info.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 import { AngularComponentFactory } from "../../component-factory";
+import { EmbeddedViewContentComponent } from "../../embedded-view-content.component";
 @Component({
   selector: "sv-brand-info",
-  templateUrl: "./brand-info.component.html"
+  templateUrl: "./brand-info.component.html",
+  styleUrls: ["../../hide-host.scss"]
 })
-export class BrandInfoComponent {}
+export class BrandInfoComponent extends EmbeddedViewContentComponent {}
 AngularComponentFactory.Instance.registerComponent("sv-brand-info", BrandInfoComponent);

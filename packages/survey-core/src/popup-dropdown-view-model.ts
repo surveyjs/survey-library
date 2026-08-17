274d783de93df8407bbbfb06fc11aac4909ceeca
OlgaLarina <olga.larina.developer@gmail.com>
2026-08-17 16:53:35 +0300

    resolve  #11729 Dropdown with lazy loading - A popup appears incorrectly on mobile (#11734) (#11736)
    (cherry picked from commit b6524c4b6e435d44f85cc3f9d4b021f9fc6772ed)
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- packages/survey-core/src/popup-dropdown-view-model.ts
+++ packages/survey-core/src/popup-dropdown-view-model.ts
@@ -20,13 +20,16 @@ export class PopupDropdownViewModel extends PopupBaseViewModel {
     }
     this.hidePopup();
   };
-  private resizeEventCallback = () => {
+  private visualViewportChangedCallback = () => {
     if (!DomWindowHelper.isAvailable()) return;
 
     const visualViewport = DomWindowHelper.getVisualViewport();
     const documentElement = DomDocumentHelper.getDocumentElement();
     if (!!documentElement && !!visualViewport) {
-      documentElement.style.setProperty("--sv-popup-overlay-height", `${visualViewport.height * visualViewport.scale}px`);
+      const scale = visualViewport.scale || 1;
+      documentElement.style.setProperty("--sv-popup-overlay-height", `${visualViewport.height * scale}px`);
+      const offsetTop = scale === 1 ? visualViewport.offsetTop : 0;
+      documentElement.style.setProperty("--sv-popup-overlay-top", `${offsetTop}px`);
     }
   };
   private resizeWindowCallback = () => {
@@ -256,12 +259,14 @@ export class PopupDropdownViewModel extends PopupBaseViewModel {
     this.switchFocus();
     DomWindowHelper.addEventListener("resize", this.resizeWindowCallback);
     if (this.shouldCreateResizeCallback) {
-      DomWindowHelper.getVisualViewport().addEventListener("resize", this.resizeEventCallback);
+      const visualViewport = DomWindowHelper.getVisualViewport();
+      visualViewport.addEventListener("resize", this.visualViewportChangedCallback);
+      visualViewport.addEventListener("scroll", this.visualViewportChangedCallback);
       if (this.container) {
         this.container.addEventListener("touchstart", this.touchStartEventCallback);
         this.container.addEventListener("touchmove", this.touchMoveEventCallback);
       }
-      this.resizeEventCallback();
+      this.visualViewportChangedCallback();
     }
     DomWindowHelper.addEventListener("scroll", this.scrollEventCallBack);
     this._isPositionSetValue = true;
@@ -288,7 +293,9 @@ export class PopupDropdownViewModel extends PopupBaseViewModel {
     super.updateOnHiding();
     DomWindowHelper.removeEventListener("resize", this.resizeWindowCallback);
     if (this.shouldCreateResizeCallback) {
-      DomWindowHelper.getVisualViewport().removeEventListener("resize", this.resizeEventCallback);
+      const visualViewport = DomWindowHelper.getVisualViewport();
+      visualViewport.removeEventListener("resize", this.visualViewportChangedCallback);
+      visualViewport.removeEventListener("scroll", this.visualViewportChangedCallback);
       if (this.container) {
         this.container.removeEventListener("touchstart", this.touchStartEventCallback);
         this.container.removeEventListener("touchmove", this.touchMoveEventCallback);
